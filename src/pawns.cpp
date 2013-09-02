50baa75eb9e1df900b062066cf37a13173f52749
Stefan Geschwentner <Stefan.Geschwentner@web.de>
2013-09-02 18:27:15 +0200

    minKPDistance: weight -12

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -259,7 +259,7 @@ Score Entry::update_safety(const Position& pos, Square ksq) {
   if (pawns)
       while (!(DistanceRingsBB[ksq][minKPdistance[Us]++] & pawns)) {}
 
-  const int kingPawnProximity = -8 * minKPdistance[Us];
+  const int kingPawnProximity = -12 * minKPdistance[Us];
   
   if (relative_rank(Us, ksq) > RANK_4)
       return kingSafety[Us] = make_score(0, kingPawnProximity);
