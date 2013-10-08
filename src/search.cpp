b4e267dbad34443dff5a6f36efc2af4d0d4c799c
Stefan Geschwentner <Stefan.Geschwentner@web.de>
2013-10-08 18:22:46 +0200

    LMR: decrease reduction one half ply if move extenden and moving piece attacks by threat move

--- src/search.cpp
+++ src/search.cpp
@@ -948,9 +948,10 @@ moves_loop: // When in check and at SpNode search starts from here
           else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
               ss->reduction += ONE_PLY / 2;
 
-          if (move == countermoves[0] || move == countermoves[1]
-              || (ext && threatMove != MOVE_NONE && from_sq(move) == to_sq(threatMove)))
+          if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
+          else if(ext && threatMove != MOVE_NONE && from_sq(move) == to_sq(threatMove))
+              ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY / 2);
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
