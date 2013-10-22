2ec9a2323bdc73648d59389807498a38f733db9f
Stefan Geschwentner <Stefan.Geschwentner@web.de>
2013-10-22 13:53:52 +0200

    LMR: reduce more if gives check and not extended

--- src/search.cpp
+++ src/search.cpp
@@ -951,8 +951,7 @@ moves_loop: // When in check and at SpNode search starts from here
           else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
               ss->reduction += ONE_PLY / 2;
               
-          else if (   PvNode && ss->staticEval != VALUE_NONE
-                   && ss->staticEval + futility_margin(depth, (ss-1)->futilityMoveCount) <= alpha)
+          else if (!ext && givesCheck)
               ss->reduction += ONE_PLY / 2;
 
           if (move == countermoves[0] || move == countermoves[1])
