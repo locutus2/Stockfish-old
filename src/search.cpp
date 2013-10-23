5f24ebdf85b00904f45b20bf2e187953620cb5e1
Stefan Geschwentner <Stefan.Geschwentner@web.de>
2013-10-23 18:10:54 +0200

    LMR: reduce one ply if PV node and static eval + margin <= alpha

--- src/search.cpp
+++ src/search.cpp
@@ -948,10 +948,12 @@ moves_loop: // When in check and at SpNode search starts from here
           if (!PvNode && cutNode)
               ss->reduction += ONE_PLY;
 
-          else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
-              ss->reduction += ONE_PLY / 2;
+          else if (   PvNode
+                   && ss->staticEval != VALUE_NONE
+                   && ss->staticEval + futility_margin(depth, moveCount) <= alpha)
+              ss->reduction += ONE_PLY;
               
-          else if (!ext && givesCheck)
+          else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
               ss->reduction += ONE_PLY / 2;
 
           if (move == countermoves[0] || move == countermoves[1])
