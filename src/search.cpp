d390c62d013a31aa85a712f84ebd117670d4f53b
Stefan Geschwentner <Stefan.Geschwentner@web.de>
2014-01-14 10:25:09 +0100

    use followupmoves in LMR

--- src/search.cpp
+++ src/search.cpp
@@ -889,7 +889,10 @@ moves_loop: // When in check and at SpNode search starts from here
           else if (History[pos.piece_on(to_sq(move))][to_sq(move)] < 0)
               ss->reduction += ONE_PLY / 2;
 
-          if (move == countermoves[0] || move == countermoves[1])
+          if (   move == countermoves[0]
+              || move == countermoves[1]
+              || move == followupmoves[0]
+              || move == followupmoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction - ONE_PLY);
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
