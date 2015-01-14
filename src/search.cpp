3d9c4095bb252537de29a7ab5decd5e7810ad539
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-01-14 23:41:34 +0100

    allow razoring at pv nodes

--- src/search.cpp
+++ src/search.cpp
@@ -615,8 +615,7 @@ namespace {
     }
 
     // Step 6. Razoring (skipped when in check)
-    if (   !PvNode
-        &&  depth < 4 * ONE_PLY
+    if (    depth < 4 * ONE_PLY
         &&  eval + razor_margin(depth) <= alpha
         &&  ttMove == MOVE_NONE
         && !pos.pawn_on_7th(pos.side_to_move()))
