a86efba58693bea9a4e1f314af53fa43b3bc0558
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-01-20 10:46:50 +0100

    move count pruning: don't prune counter moves at PV nodes

--- src/search.cpp
+++ src/search.cpp
@@ -834,12 +834,12 @@ moves_loop: // When in check and at SpNode search starts from here
       if (   !captureOrPromotion
           && !inCheck
           && !dangerous
-          &&  bestValue > VALUE_MATED_IN_MAX_PLY
-          && !(PvNode && (move == ss->killers[0] || move == ss->killers[1])))
+          &&  bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           // Move count based pruning
           if (   depth < 16 * ONE_PLY
-              && moveCount >= FutilityMoveCounts[improving][depth])
+              && moveCount >= FutilityMoveCounts[improving][depth]
+              && !(PvNode && (move == countermoves[0] || move == countermoves[1])))
           {
               if (SpNode)
                   splitPoint->mutex.lock();
