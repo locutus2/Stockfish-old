fb4d236db01c3596ee5ac31f96108cc176e8d848
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-02-06 22:46:29 +0100

    allow king as defender

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -553,7 +553,7 @@ namespace {
 
     b &=  ~pos.pieces()
         & ~ei.attackedBy[Them][PAWN]
-        & (ei.attackedBy[Us][PAWN] | ei.attackedBy[Us][KNIGHT] | ~ei.attackedBy[Them][ALL_PIECES]);
+        & (ei.attackedBy[Us][PAWN] | ei.attackedBy[Us][KING] | ~ei.attackedBy[Them][ALL_PIECES]);
 
     b =  (shift_bb<Left>(b) | shift_bb<Right>(b))
        &  pos.pieces(Them)
