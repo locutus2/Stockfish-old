5f47a78a3197804c020124eb09a61967ed924900
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2015-09-05 15:16:11 +0200

    Double flanking storm bonus

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -98,7 +98,7 @@ namespace {
   { Rank5BB & (FileEBB | FileHBB), Rank5BB & (FileDBB | FileABB), 0} };
 
   // Bonus for flanking storming pawns pair on the 4th rank (like e4,h4 against a white king on g1)
-  const Value FlankingStormingPawns = Value(32);
+  const Value FlankingStormingPawns = Value(128);
 
   // Max bonus for king safety. Corresponds to start position with all the pawns
   // in front of the king and no enemy pawn on the horizon.
