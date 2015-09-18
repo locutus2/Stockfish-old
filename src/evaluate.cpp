e8e35d59da281d8521cbba27113069a84f3c35ae
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-09-18 09:42:41 +0200

    Endgame pawn scaling. Use also passed pawns

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -798,9 +798,10 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Scale endgame by number of pawns
-  int p = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
+  int p    = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
+  int pp   = popcount<Max15>(ei.pi->passed_pawns(WHITE) | ei.pi->passed_pawns(BLACK));
   int v_eg = 1 + abs(int(eg_value(score)));
-  sf = ScaleFactor(std::max(sf / 2, sf - 7 * SCALE_FACTOR_NORMAL * (14 - p) / v_eg));
+  sf = ScaleFactor(std::max(sf / 2, sf - 7 * SCALE_FACTOR_NORMAL * (14 - p - 2 * pp) / v_eg));
 
   // Interpolate between a middlegame and a (scaled by 'sf') endgame score
   Value v =  mg_value(score) * int(me->game_phase())
