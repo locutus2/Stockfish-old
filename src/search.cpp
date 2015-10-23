b7fc16350f9a89e2f217a17a0da0204fd5c446d6
Stefan Geschwentner <stgeschwentner@gmail.com>
2015-10-23 13:20:44 +0200

    Reformat

--- src/search.cpp
+++ src/search.cpp
@@ -893,13 +893,13 @@ moves_loop: // When in check search starts from here
           if (   depth < 16 * ONE_PLY
               && moveCount >= FutilityMoveCounts[improving][depth])
               continue;
-              
-             // History Score Pruning
-		  if (depth <= 3 * ONE_PLY
-			  && thisThread->History[pos.moved_piece(move)][to_sq(move)] < VALUE_ZERO
-			  && CounterMovesHistory[pos.piece_on(prevMoveSq)][prevMoveSq]
-			  [pos.moved_piece(move)][to_sq(move)] < VALUE_ZERO)
-			  continue;
+
+          // History Score Pruning
+          if (   depth <= 3 * ONE_PLY
+              && thisThread->History[pos.moved_piece(move)][to_sq(move)] < VALUE_ZERO
+              && CounterMovesHistory[pos.piece_on(prevMoveSq)][prevMoveSq]
+                                    [pos.moved_piece(move)][to_sq(move)] < VALUE_ZERO)
+              continue;
 
           predictedDepth = newDepth - reduction<PvNode>(improving, depth, moveCount);
 
