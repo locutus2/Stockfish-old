7b404eaf0d96d88e6ea030d570d9fe1fc76821a6
Stefan Geschwentner <stgeschwentner@gmail.com>
2016-02-02 11:25:47 +0100

    try new formula with movecount

--- src/search.cpp
+++ src/search.cpp
@@ -958,7 +958,9 @@ moves_loop: // When in check search starts from here
           // Move count based pruning
           if (   depth < 16 * ONE_PLY
               && moveCount >= FutilityMoveCounts[improving][depth]
-              && cmh[pos.moved_piece(move)][to_sq(move)] < 13991 - 222 * depth / ONE_PLY)
+              &&        cmh[pos.moved_piece(move)][to_sq(move)]
+                +   3 * thisThread->history[pos.moved_piece(move)][to_sq(move)]
+                + 803 * moveCount <= 35227)
               continue;
 
           // History based pruning
