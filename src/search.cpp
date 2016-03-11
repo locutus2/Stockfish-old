0553b8fbc9f862d2022a5a89dcd8052aa6aef027
Stefan Geschwentner <stgeschwentner@gmail.com>
2016-03-11 14:04:01 +0100

    compensate by raising divisor

--- src/search.cpp
+++ src/search.cpp
@@ -1023,7 +1023,7 @@ moves_loop: // When in check search starts from here
               r += ONE_PLY;
 
           // Decrease/increase reduction for moves with a good/bad history
-          int rHist = (hValue + cmhValue + fmhValue) / 14980;
+          int rHist = (hValue + cmhValue + fmhValue) / 21727;
           r = std::max(DEPTH_ZERO, r - rHist * ONE_PLY);
 
           // Decrease reduction for moves that escape a capture. Filter out
