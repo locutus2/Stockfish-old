f380d343a6895f8df8b3c72863784f2eee3ee6aa
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2016-11-13 15:34:17 +0100

    simplify by replaceing new tweak by a constant

--- src/search.cpp
+++ src/search.cpp
@@ -946,8 +946,7 @@ moves_loop: // When in check search starts from here
           }
           else if (   depth < 7 * ONE_PLY
                    && !extension
-                   && !pos.see_ge(move,  Value(-35 * depth / ONE_PLY * depth / ONE_PLY)
-                                       + (ss->staticEval != VALUE_NONE ? ss->staticEval - alpha - 200 : VALUE_ZERO)))
+                   && !pos.see_ge(move, Value(-35 * depth / ONE_PLY * depth / ONE_PLY - 400)))
                   continue;
       }
 
