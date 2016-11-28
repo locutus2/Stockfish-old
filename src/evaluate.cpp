5187735176226b4d32e38c0a696d80d15cda29d5
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2016-11-28 15:30:04 +0100

    only midgame bonus

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -201,7 +201,7 @@ namespace {
   const Score PawnlessFlank       = S(20, 80);
   const Score HinderPassedPawn    = S( 7,  0);
   const Score ThreatByRank        = S(16,  3);
-  const Score DoubleThreat        = S(10, 10);
+  const Score DoubleThreat        = S(10,  0);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
