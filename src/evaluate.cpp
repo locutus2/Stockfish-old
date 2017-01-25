1b23c0bb13e60d84611ce630742920cdac53dc4d
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-01-25 10:23:19 +0100

    Bonus for threat of creation of a doubled pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -196,6 +196,7 @@ namespace {
   const Score ThreatByRank        = S(16,  3);
   const Score Hanging             = S(48, 27);
   const Score ThreatByPawnPush    = S(38, 22);
+  const Score ThreatDoubledPawn   = S( 9, 19);
   const Score HinderPassedPawn    = S( 7,  0);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
@@ -601,6 +602,22 @@ namespace {
        & ~ei.attackedBy[Us][PAWN];
 
     score += ThreatByPawnPush * popcount(b);
+
+    // Threat of creation of a doubled pawn
+    b = pos.pinned_pieces(Them);
+    if (b)
+    {
+        b &=  ei.attackedBy[Them][PAWN]
+           & ~ei.attackedBy2[Them]
+           & ~ei.attackedBy[Us][PAWN]
+           & (shift<NORTH>(pos.pieces(Them, PAWN)) | shift<SOUTH>(pos.pieces(Them, PAWN)))
+           & (  (pos.pieces(Them, PAWN)  & ei.attackedBy[Us][PAWN])
+              | (pos.pieces(Them, KNIGHT, BISHOP) & (ei.attackedBy[Us][KNIGHT] | ei.attackedBy[Us][BISHOP]))
+              | (pos.pieces(Them, ROOK)  & ei.attackedBy[Us][ROOK])
+              | (pos.pieces(Them, QUEEN) & ei.attackedBy[Us][QUEEN]));
+
+        score += ThreatDoubledPawn * popcount(b);
+    }
 
     if (DoTrace)
         Trace::add(THREAT, Us, score);
