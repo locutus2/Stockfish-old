8dfc4ea5ab36bf64fd871a19666f700797aae060
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-04-23 23:46:39 +0200

    More sorting depth > 17 weight 4250

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -242,7 +242,7 @@ Move MovePicker::next_move(bool skipQuiets) {
       endMoves = generate<QUIETS>(pos, cur);
       score<QUIETS>();
 
-      partial_insertion_sort(cur, endMoves, -4000 * depth / ONE_PLY);
+      partial_insertion_sort(cur, endMoves, 4250 - 4250 * depth / ONE_PLY);
       ++stage;
 
   case QUIET:
