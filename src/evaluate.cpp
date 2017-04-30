3945b72c872a8feb2bb22a02419bb9d5978b2ec7
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-04-30 15:57:11 +0200

    No MinorBehindPawn bonus if piece is behind an own passed pawn

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -316,9 +316,8 @@ namespace {
             }
 
             // Bonus when behind a pawn
-            if (     relative_rank(Us, s) < RANK_5
-                &&  (pos.pieces(PAWN) & (s + pawn_push(Us)))
-                && !(ei.attackedBy[Them][PAWN] & s))
+            if (    relative_rank(Us, s) < RANK_5
+                && ((pos.pieces(PAWN) ^ ei.pe->passed_pawns(Us)) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
 
             // Penalty for pawns on the same color square as the bishop
