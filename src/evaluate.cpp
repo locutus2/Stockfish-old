e73d47f169b496334bb6186bae4071373bf7d324
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-05-02 09:43:57 +0200

    less compensation. Bench 6337740

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -673,7 +673,7 @@ namespace {
             else if (pos.pieces(Us) & blockSq)
                 mbonus += rr + r * 2, ebonus += rr + r * 2;
             else
-                mbonus -= 6;
+                mbonus -= 3;
         } // rr != 0
 
         // Scale down bonus for candidate passers which need more than one
