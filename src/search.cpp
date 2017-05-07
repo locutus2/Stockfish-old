5d5524b4d02d0021100ffc5edfd7898ee7d6c53e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-05-07 15:08:31 +0200

    no nullmove pruning in knight endgames

--- src/search.cpp
+++ src/search.cpp
@@ -740,7 +740,7 @@ namespace {
     if (   !PvNode
         &&  eval >= beta
         && (ss->staticEval >= beta - 35 * (depth / ONE_PLY - 6) || depth >= 13 * ONE_PLY)
-        &&  pos.non_pawn_material(pos.side_to_move()))
+        &&  pos.non_pawn_material(pos.side_to_move()) > KnightValueMg)
     {
 
         assert(eval - beta >= 0);
