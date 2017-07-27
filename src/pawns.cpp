cf38c1dd2df80d9b4a686106b1665e14f81f8763
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-07-27 18:06:29 +0200

    remove rank condition for passed pawn sacrifices. bench 5921550

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -156,15 +156,15 @@ namespace {
         // Passed pawns will be properly scored in evaluation because we need
         // full attack info to evaluate them. Include also not passed pawns
         // which could become passed after one or two pawn pushes when are
-        // not attacked more times than defended.
+        // not attacked more times than defended ...
         if (   !(stoppers ^ lever ^ leverPush)
             && !(ourPawns & forward_file_bb(Us, s))
             && popcount(supported) >= popcount(lever)
             && popcount(phalanx)   >= popcount(leverPush))
             e->passedPawns[Us] |= s;
 
-        else if (   stoppers == SquareBB[s + Up]
-                 && relative_rank(Us, s) >= RANK_5)
+        // ... or become passed after sacrificing an adjacent pawn by pushing.
+        else if (stoppers == SquareBB[s + Up])
         {
             b = shift<Up>(supported) & ~theirPawns;
             while (b)
