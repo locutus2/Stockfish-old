092617d3482193582f2a32a22b42a9ed52e937e0
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-07-29 02:03:36 +0200

    Use distance to blocking and promotion square. bench 5306348

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -653,7 +653,8 @@ namespace {
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
-            ebonus +=  distance(pos.square<KING>(Them), promotionSq) * 5 * rr
+            ebonus +=  distance(pos.square<KING>(Them), promotionSq) * 2 * rr
+                     + distance(pos.square<KING>(Them), blockSq    ) * 3 * rr
                      - distance(pos.square<KING>(  Us), blockSq    ) * 2 * rr;
 
             // If blockSq is not the queening square then consider also a second push
