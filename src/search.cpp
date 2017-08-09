990f15ca83f40b323d6515f14767a3608ed61a79
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-08-09 11:01:41 +0200

    variant with less reductions. bench 7012767

--- src/search.cpp
+++ src/search.cpp
@@ -165,7 +165,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double r = log(d + 0.22) * log(mc - 0.17) / 1.95;
+              double r = log(d + 0.22) * (log(mc) - 0.17) / 1.95;
 
               Reductions[NonPV][imp][d][mc] = int(std::round(r));
               Reductions[PV][imp][d][mc] = std::max(Reductions[NonPV][imp][d][mc] - 1, 0);
