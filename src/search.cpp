fcefe5de7d42486a5d199c4818e04acdec8ce4f3
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-09-05 10:16:23 +0200

    With compensation. bench 5772994

--- src/search.cpp
+++ src/search.cpp
@@ -976,7 +976,7 @@ moves_loop: // When in check search starts from here
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              + (*pbmHistory)[movedPiece][to_sq(move)]
-                             - 4000;
+                             - 2000;
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->statScore > 0 && (ss-1)->statScore < 0)
