4315e04b206a230c4bfe475c829a618cd193981e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-10-01 22:58:43 +0200

    One and a half ply ttCapture reduction. bench 5197341

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,7 @@ moves_loop: // When in check search starts from here
 
               // Increase reduction if ttMove is a capture
               if (ttCapture)
-                  r += ONE_PLY / 2;
+                  r += ONE_PLY * 3 / 2;
 
               // Increase reduction for cut nodes
               if (cutNode)
