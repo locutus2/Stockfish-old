3c21889f912a4598861362612ee2b2a85fadac6b
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-10-28 15:35:25 +0200

    Two plies less reduction. bench 5084947

--- src/search.cpp
+++ src/search.cpp
@@ -950,9 +950,8 @@ moves_loop: // When in check search starts from here
           Depth r = reduction<PvNode>(improving, depth, moveCount);
 
           // Decrease reduction if at a PV node the best value is draw
-          if (PvNode && bestValue == DrawValue[~pos.side_to_move()]
-                     && bestValue >= alpha)
-              r -= ONE_PLY;
+          if (PvNode && bestMove && bestValue == DrawValue[~pos.side_to_move()])
+              r -= 2 * ONE_PLY;
 
           if (captureOrPromotion)
               r -= ONE_PLY;
