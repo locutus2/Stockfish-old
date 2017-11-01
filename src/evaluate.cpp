3c7d03226e0719f47e2b6400f4161baa82322eea
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-11-01 14:01:39 +0100

    Use king safety differenc. bench 4866889

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -768,14 +768,14 @@ namespace {
 
     Value mg = mg_value(score);
     Value eg = eg_value(score);
-    Color weakSideMg = mg < VALUE_DRAW ? WHITE : BLACK;
+    Color strongSideMg = mg > VALUE_DRAW ? WHITE : BLACK;
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
     int initiative_eg = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
-    int initiative_mg = -mg_value(kingSafety[weakSideMg]) / 8;
+    int initiative_mg = (mg_value(kingSafety[strongSideMg] - kingSafety[~strongSideMg])) / 8;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
