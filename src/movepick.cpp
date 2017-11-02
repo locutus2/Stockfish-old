6d6f67c987c16db13cfa7a7c77248e5d3e085b02
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-11-02 12:38:18 +0100

    Add also rank term from master to catpure move sorting. bench 4951176. Set capture history update weight back to 1.

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -132,6 +132,7 @@ void MovePicker::score() {
   for (auto& m : *this)
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
+                   - Value(200 * relative_rank(pos.side_to_move(), to_sq(m)))
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
