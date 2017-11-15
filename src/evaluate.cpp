ddf11f4d4fba446260bc4256cdc6ee88c65c144e
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-11-15 16:00:37 +0100

    Initiative bonus of one side has queens and the other not. bench 5293354

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,7 +754,7 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
-    bool imbalance =  !pos.pieces(WHITE, KNIGHT) != !pos.pieces(BLACK, KNIGHT);
+    bool imbalance =  !pos.pieces(WHITE, QUEEN) != !pos.pieces(BLACK, QUEEN);
 
     // Compute the initiative bonus for the attacking side
     int initiative =   8 * (pe->pawn_asymmetry() + kingDistance + imbalance - 18)
