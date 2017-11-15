206b5f2434482daff45b599d9eccfb511d9663cd
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-11-15 07:30:33 +0100

    Initiative bonus of one side has bishops and the other not. bench 5624408

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -754,9 +754,13 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
+    bool imbalance =   (!pos.pieces(WHITE, BISHOP) && pos.pieces(BLACK, BISHOP))
+                    || (!pos.pieces(BLACK, BISHOP) && pos.pieces(WHITE, BISHOP));
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (pe->pawn_asymmetry() + kingDistance - 17) + 12 * pos.count<PAWN>() + 16 * bothFlanks;
+    int initiative =   8 * (pe->pawn_asymmetry() + kingDistance - 17)
+                    + 12 *  pos.count<PAWN>()
+                    + 16 * (bothFlanks + imbalance);
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
