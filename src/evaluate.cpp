671f930306838edee6c8a8c056438b558a2c1336
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-11-20 11:16:01 +0100

    Try the opposite. bench 5459761

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -893,7 +893,7 @@ namespace {
 
 Value Eval::tempo(const Position& pos)
 {
-   return Value(28 - pos.count<PAWN>() / 2);
+   return Value(20 + pos.count<PAWN>() / 2);
 }
 
 /// evaluate() is the evaluator for the outer world. It returns a static evaluation
