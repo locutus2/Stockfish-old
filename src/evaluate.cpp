66f41c126c3ae0d65b3af2e63b063ee44b1e787b
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-11-22 11:10:52 +0100

    More queen mobility also in endgame. bench 5397338

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -327,8 +327,7 @@ namespace {
 
         mobility[Us] += MobilityBonus[Pt - 2][mob];
         if (Pt == QUEEN && !pos.pieces(Them, QUEEN))
-            mobility[Us] += make_score(   mg_value(MobilityBonus[Pt - 2][mob])
-                                       * (pos.count<KNIGHT>(Them) + pos.count<BISHOP>(Them)) / 3, 0);
+            mobility[Us] += MobilityBonus[Pt - 2][mob] * (pos.count<KNIGHT>(Them) + pos.count<BISHOP>(Them)) / 3;
 
         // Bonus for this piece as a king protector
         score += KingProtector[Pt - 2] * distance(s, pos.square<KING>(Us));
