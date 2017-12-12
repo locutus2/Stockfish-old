7713fa065281915ec2014d8aca4394bb54907847
Stefan Geschwentner <stgeschwentner@gmail.com>
2017-12-12 18:19:12 +0100

    Greater stats bonus if best move found at expected ALL node. bench 5626797

--- src/search.cpp
+++ src/search.cpp
@@ -1077,9 +1077,9 @@ moves_loop: // When in check search starts from here
     {
         // Quiet best move: update move sorting heuristics
         if (!pos.capture_or_promotion(bestMove))
-            update_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
+            update_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth + (!PvNode && !cutNode) * ONE_PLY));
         else
-            update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth));
+            update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth + (!PvNode && !cutNode) * ONE_PLY));
 
         // Extra penalty for a quiet TT move in previous ply when it gets refuted
         if ((ss-1)->moveCount == 1 && !pos.captured_piece())
