5c7c482742deeedf4f619d568e5ab66e7bb5e95e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-17 14:48:49 +0100

    Even less slope (7/8) for stat bonus. bench 5225075

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : (15 * d * d + 50 * d - 34) / 16;
+    return d > 17 ? 0 : (7 * d * d + 34 * d - 26) / 8;
   }
 
   // Skill structure is used to implement strength limit
