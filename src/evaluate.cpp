3833662d7a4ff2fbb0cbbf6baf80075e793866cb
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-22 18:36:22 +0100

    Exclude pieces around the opponent king as blockers. bench 5840162

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -310,10 +310,8 @@ namespace {
     while ((s = *pl++) != SQ_NONE)
     {
         // Find attacked squares, including x-ray attacks for bishops and rooks
-        b = Pt == BISHOP ? attacks_bb<BISHOP>(s,   pos.pieces() ^ pos.pieces(QUEEN)
-                                                ^ (pos.pieces(Them, KNIGHT, ROOK) & attackedBy[Us][PAWN]))
-          : Pt ==   ROOK ? attacks_bb<  ROOK>(s,   pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK)
-                                                ^ (pos.pieces(Them, KNIGHT, BISHOP) & attackedBy[Us][PAWN]))
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, ~attackedBy[Them][KING] & (pos.pieces() ^ pos.pieces(QUEEN)))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, ~attackedBy[Them][KING] & (pos.pieces() ^ pos.pieces(QUEEN) ^ pos.pieces(Us, ROOK)))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.pinned_pieces(Us) & s)
