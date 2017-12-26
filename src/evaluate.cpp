947994a9de5c7583fc8b1e0aa93e33a803c95693
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-26 15:27:22 +0100

    Add to mobilityArea own pawns attacked by pawn. bench 5864875

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -263,7 +263,8 @@ namespace {
 
     // Squares occupied by those pawns, by our king, or controlled by enemy pawns
     // are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them));
+    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them))
+                      | (pos.pieces(Us, PAWN) & pe->pawn_attacks(Them));
 
     // Initialise the attack bitboards with the king and pawn information
     b = attackedBy[Us][KING] = pos.attacks_from<KING>(pos.square<KING>(Us));
