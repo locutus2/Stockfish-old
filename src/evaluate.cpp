01691490b6135cb2f0ecb0ee70aaeca17a57e661
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-26 18:50:42 +0100

    Add opponent pinned pieces. bench 5507766

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -263,7 +263,8 @@ namespace {
 
     // Squares occupied by those pawns, by our king, or controlled by enemy pawns
     // are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them) | pos.pinned_pieces(Us));
+    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them))
+                      |  pos.pinned_pieces(Them);
 
     // Initialise the attack bitboards with the king and pawn information
     b = attackedBy[Us][KING] = pos.attacks_from<KING>(pos.square<KING>(Us));
