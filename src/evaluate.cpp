2a724d69988152763386b0193d9c9d4fdcf8f128
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-26 19:51:02 +0100

    Add opponent majors. bench 5601876

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -259,11 +259,12 @@ namespace {
     const Bitboard LowRanks = (Us == WHITE ? Rank2BB | Rank3BB: Rank7BB | Rank6BB);
 
     // Find our pawns on the first two ranks, and those which are blocked
-    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces(Us, PAWN) | pos.pieces(Them)) | LowRanks);
+    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
 
     // Squares occupied by those pawns, by our king, or controlled by enemy pawns
     // are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them));
+    mobilityArea[Us] = ~(b | pos.square<KING>(Us) | pe->pawn_attacks(Them))
+                      |  pos.pieces(Them, ROOK, QUEEN);
 
     // Initialise the attack bitboards with the king and pawn information
     b = attackedBy[Us][KING] = pos.attacks_from<KING>(pos.square<KING>(Us));
