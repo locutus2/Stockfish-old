2f99e30d3eabcd6c7c91098b68eb65413719bd6d
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2017-12-30 18:51:20 +0100

    Avoid overwriting tt entries with depth > 2. bench 5544445

--- src/search.cpp
+++ src/search.cpp
@@ -643,8 +643,7 @@ namespace {
         eval = ss->staticEval =
         (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
                                          : -(ss-1)->staticEval + 2 * Eval::Tempo;
-
-        if (tte->depth() <= ONE_PLY)
+        if (tte->depth() <= 2 * ONE_PLY)
             tte->save(posKey, VALUE_NONE, BOUND_NONE, DEPTH_NONE, MOVE_NONE,
                       ss->staticEval, TT.generation());
     }
