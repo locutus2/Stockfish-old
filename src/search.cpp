e680143f86bd09b31eedf8030fa676dda8dd64fe
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-01-09 14:44:40 +0100

    Decrease stat bonus by one. bench 5407749

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : d * d + 2 * d - 1;
+    return d > 17 ? 0 : d * d + 2 * d - 3;
   }
 
   // Skill structure is used to implement strength limit
