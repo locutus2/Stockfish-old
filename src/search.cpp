e79504d06dcf81b8d39f10bd23a78dc6f0e0b1e6
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-01-19 11:22:45 +0100

    variant 1. bench 5489313

--- src/search.cpp
+++ src/search.cpp
@@ -676,7 +676,7 @@ namespace {
     if (   !PvNode
         &&  eval >= beta
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
-        && (cutNode || relative_rank(pos.side_to_move(), pos.square<KING>(~pos.side_to_move())) > RANK_4)
+        && (cutNode || relative_rank(pos.side_to_move(), pos.square<KING>(~pos.side_to_move())) < RANK_5)
         && (ss->ply >= thisThread->nmp_ply || ss->ply % 2 != thisThread->nmp_odd))
     {
 
