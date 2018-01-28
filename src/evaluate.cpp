907dda1da2d9b305444946afb160db3cbf3cc52e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-01-28 19:50:56 +0100

    Half scaling. bench 5361460

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -495,7 +495,7 @@ namespace {
         {
             int mobilityDanger = mg_value(mobility[Them] - mobility[Us]);
             kingDanger = std::max(0, kingDanger + mobilityDanger);
-            kingDanger = kingDanger * (kingAttackersCount[Them] + 60) / 64;
+            kingDanger = kingDanger * (kingAttackersCount[Them] + 125) / 128;
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
         }
     }
