dba16aa984f98f64aab64be2c7a2cd5c68489059
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-01-28 13:46:14 +0100

    Scale king danger with number of attackers. bench 5535428

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -491,6 +491,7 @@ namespace {
         {
             int mobilityDanger = mg_value(mobility[Them] - mobility[Us]);
             kingDanger = std::max(0, kingDanger + mobilityDanger);
+            kingDanger = kingDanger * (kingAttackersCount[Them] + 62) / 64;
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
         }
     }
