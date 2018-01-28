07cc1aa9f1d374b7a99aebba31aded7e6698867b
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-01-28 16:15:16 +0100

    break even for one attacker. bench 5181738

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -495,7 +495,7 @@ namespace {
         {
             int mobilityDanger = mg_value(mobility[Them] - mobility[Us]);
             kingDanger = std::max(0, kingDanger + mobilityDanger);
-            kingDanger = kingDanger * (kingAttackersCount[Them] + 62) / 64;
+            kingDanger = kingDanger * (kingAttackersCount[Them] + 63) / 64;
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
         }
     }
