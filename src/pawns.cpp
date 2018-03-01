f7a6983157ab41a3ebc1c9222bdf211c72d5990d
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-03-01 11:27:37 +0100

    Tweak king safety calculations of castles. bench 6149361

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -290,16 +290,17 @@ Score Entry::do_king_safety(const Position& pos, Square ksq) {
   if (pawns)
       while (!(DistanceRingBB[ksq][minKingPawnDistance++] & pawns)) {}
 
-  Value bonus = shelter_storm<Us>(pos, ksq);
+  Value bonus1, bonus2;
+  bonus1 = bonus2 = shelter_storm<Us>(pos, ksq);
 
   // If we can castle use the bonus after the castling if it is bigger
   if (pos.can_castle(MakeCastling<Us, KING_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
+      bonus2 = std::max(bonus2, shelter_storm<Us>(pos, relative_square(Us, SQ_G1)));
 
   if (pos.can_castle(MakeCastling<Us, QUEEN_SIDE>::right))
-      bonus = std::max(bonus, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
+      bonus2 = std::max(bonus2, shelter_storm<Us>(pos, relative_square(Us, SQ_C1)));
 
-  return make_score(bonus, -16 * minKingPawnDistance);
+  return make_score((bonus1 + bonus2) / 2, -16 * minKingPawnDistance);
 }
 
 // Explicit template instantiation
