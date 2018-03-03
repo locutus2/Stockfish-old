e99875107c96eb2913528323161f7f7c84ebbe98
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-03-03 11:12:14 +0100

    less pruning offset. bench 5904591

--- src/search.cpp
+++ src/search.cpp
@@ -758,7 +758,7 @@ namespace {
         Value rbeta = std::min(beta + 200, VALUE_INFINITE);
         MovePicker mp(pos, ttMove, rbeta - ss->staticEval, &thisThread->captureHistory);
 		int mc = 0;
-        while ((move = mp.next_move()) != MOVE_NONE && mc < depth / ONE_PLY - 4)
+        while ((move = mp.next_move()) != MOVE_NONE && mc < depth / ONE_PLY - 2)
             if (pos.legal(move))
             {
 				mc++;
