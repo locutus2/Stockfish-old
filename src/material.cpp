ff298a4c38842004a15ea619f47529050bf7dc66
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-03-03 22:49:27 +0100

    Scale game phase down according to number of pawns (max 50%). bench 5643625

--- src/material.cpp
+++ src/material.cpp
@@ -136,6 +136,8 @@ Entry* probe(const Position& pos) {
 
   // Map total non-pawn material into [PHASE_ENDGAME, PHASE_MIDGAME]
   e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit - EndgameLimit));
+  // Scale game phase down according to number of pawns
+  e->gamePhase = Phase(e->gamePhase * (16 + pos.count<PAWN>()) / 32);
 
   // Let's look if we have a specialized evaluation function for this particular
   // material configuration. Firstly we look for a fixed configuration one, then
