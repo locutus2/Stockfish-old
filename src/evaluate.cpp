7577e654384aea5de7eb516e1933777edeb419b8
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-03-08 00:23:25 +0100

    Remove pinned pieces from threat squares. bench 5544886

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -602,6 +602,7 @@ namespace {
         b =   pos.attacks_from<KNIGHT>(pos.square<QUEEN>(Them))
            &  attackedBy[Us][KNIGHT]
            & ~pos.pieces(Us, PAWN, KING)
+           & ~(pos.blockers_for_king(Us) & pos.pieces(Us))
            & ~stronglyProtected;
 
         score += KnightOnQueen * popcount(b);
