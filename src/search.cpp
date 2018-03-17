04bedd8fd09b24f4f26e29c3f1fb41052ef2e6db
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-03-17 18:48:52 +0100

    use lower move count in lmr formula for follow move. bench 5482782

--- src/search.cpp
+++ src/search.cpp
@@ -961,7 +961,7 @@ moves_loop: // When in check, search starts from here
           &&  moveCount > 1
           && (!captureOrPromotion || moveCountPruning))
       {
-          Depth r = reduction<PvNode>(improving, depth, moveCount);
+          Depth r = reduction<PvNode>(improving, depth, moveCount - (move == followmove));
 
           if (captureOrPromotion)
               r -= r ? ONE_PLY : DEPTH_ZERO;
@@ -973,10 +973,6 @@ moves_loop: // When in check, search starts from here
 
               // Decrease reduction for exact PV nodes
               if (pvExact)
-                  r -= ONE_PLY;
-
-              // Decrease reduction for late follow move
-              if (move == followmove && moveCount > 15)
                   r -= ONE_PLY;
 
               // Increase reduction if ttMove is a capture
