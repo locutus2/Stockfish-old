f36edf6a6ba4c469bde9768dce8869c118a584cd
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-04-07 13:45:33 +0200

    Try resolution/3. bench 5268372

--- src/search.cpp
+++ src/search.cpp
@@ -861,7 +861,7 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY] + thisThread->resolutions / 2;
+                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY] + thisThread->resolutions / 3;
 
       // Step 13. Extensions (~70 Elo)
 
