3da7fe5acf9fef512ef6e5ddb3eb01db835f1ef6
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-04-11 20:03:39 +0200

    Try now half bonus. bench 5010882

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -683,7 +683,7 @@ namespace {
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
-                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : popcount(squaresToQueen ^ unsafeSquares);
+                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : (1 + popcount(squaresToQueen ^ unsafeSquares)) / 2;
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
