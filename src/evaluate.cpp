95c0c0e6a322a572897ed6b38d02a150905713a6
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-04-11 20:55:03 +0200

    Less bonus for defended promotion path. bench 4980703

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -683,12 +683,12 @@ namespace {
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
                 // assign a smaller bonus if the block square isn't attacked.
-                int k = !unsafeSquares ? 22 : !(unsafeSquares & blockSq) ? 9 : 0;
+                int k = !unsafeSquares ? 20 : !(unsafeSquares & blockSq) ? 9 : 0;
 
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
                 if (defendedSquares == squaresToQueen)
-                    k += 6;
+                    k += 5;
 
                 else if (defendedSquares & blockSq)
                     k += 4;
