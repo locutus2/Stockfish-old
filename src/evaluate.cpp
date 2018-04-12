fd45316b2ec84dc32a1c930f20808edf0b50d235
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-04-12 21:21:31 +0200

    Scale midgame value with 9/8. bench 4754618

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -693,7 +693,7 @@ namespace {
                 else if (defendedSquares & blockSq)
                     k += 4;
 
-                bonus += make_score(k * w * 7 / 8, k * w);
+                bonus += make_score(k * w * 9 / 8, k * w);
             }
             else if (pos.pieces(Us) & blockSq)
                 bonus += make_score(w + r * 2, w + r * 2);
