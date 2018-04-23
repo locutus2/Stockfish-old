fc356befe9cc81a961a50c624cb7e93be22c502b
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-04-23 14:14:10 +0200

    depth < plies + 15. bench 5164290

--- src/search.cpp
+++ src/search.cpp
@@ -791,7 +791,7 @@ namespace {
         int probCutCount = 0;
 
         while (   (move = mp.next_move()) != MOVE_NONE
-               && (probCutCount < 3 || (rbeta < ss->staticEval && depth < 16 * ONE_PLY)))
+               && (probCutCount < 3 || (rbeta < ss->staticEval && depth < (ss->ply + 15) * ONE_PLY)))
             if (pos.legal(move))
             {
                 probCutCount++;
