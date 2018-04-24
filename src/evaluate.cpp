b356c4c53399fe4a619955e902ff7c7db1115d08
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-04-24 07:41:23 +0200

    Exclude pawns from weight calculation. bench 5540781

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,9 +747,9 @@ namespace {
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
     int bonus = popcount(safe) + popcount(behind & safe);
-    int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
+    int weight = pos.count<ALL_PIECES>(Us) - pos.count<PAWN>(Us) - pe->open_files();
 
-    Score score = make_score(bonus * weight * weight / 16, 0);
+    Score score = make_score(bonus * weight * weight / 4, 0);
 
     if (T)
         Trace::add(SPACE, Us, score);
