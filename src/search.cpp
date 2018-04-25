7a26cf05f5e2e5377fe68a70a734e1110c04a608
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-04-25 12:18:14 +0200

    Use also depth more offset generation. bench 4636344

--- src/search.cpp
+++ src/search.cpp
@@ -1019,7 +1019,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              statOffset = ((posKey + move) & 0x03FF) - 512;
+              statOffset = ((posKey + move + depth / ONE_PLY) & 0x03FF) - 512;
               r = std::max(DEPTH_ZERO, (r / ONE_PLY - (ss->statScore + statOffset) / 20000) * ONE_PLY);
           }
 
