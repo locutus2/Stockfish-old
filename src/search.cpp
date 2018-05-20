e74f35320cf55797b66063069ab016d67ddd7aa6
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-05-20 23:53:54 +0200

    capture history < 10100. bench 4951410

--- src/search.cpp
+++ src/search.cpp
@@ -748,7 +748,7 @@ namespace {
         && (ss-1)->statScore < 22500
         &&  eval >= beta
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
-        && (!pos.captured_piece() || thisThread->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] < 10000)
+        && (!pos.captured_piece() || thisThread->captureHistory[pos.piece_on(prevSq)][prevSq][type_of(pos.captured_piece())] < 10100)
         && !excludedMove
         &&  pos.non_pawn_material(pos.side_to_move())
         && (ss->ply >= thisThread->nmp_ply || ss->ply % 2 != thisThread->nmp_odd))
