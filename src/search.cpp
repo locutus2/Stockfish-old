ebe684d95ee8cae1bb974e115b6f2c2f13611f9c
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-05-25 18:17:29 +0200

    (ss-1)->statScore > -35000. bench 5204320

--- src/search.cpp
+++ src/search.cpp
@@ -934,7 +934,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
-              if (moveCountPruning && (ss-1)->statScore > -40000)
+              if (moveCountPruning && (ss-1)->statScore > -35000)
               {
                   skipQuiets = true;
                   continue;
