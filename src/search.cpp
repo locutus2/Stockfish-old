13689ba71fe9738f563b0a98cdc7c01a47ac46f4
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-05-25 08:22:43 +0200

    No move count pruning if last opponent move has good stat score. bench 4747020

--- src/search.cpp
+++ src/search.cpp
@@ -927,7 +927,6 @@ moves_loop: // When in check, search starts from here
       // Step 14. Pruning at shallow depth (~170 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
-          && (ss-1)->statScore < 30000
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
           if (   !captureOrPromotion
@@ -935,7 +934,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
-              if (moveCountPruning)
+              if (moveCountPruning && (ss-1)->statScore < 30000)
               {
                   skipQuiets = true;
                   continue;
