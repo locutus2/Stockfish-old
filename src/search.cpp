cc6f86e903bcc6affe2151a7e5f6a5c435b51d3a
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-05-25 15:10:30 +0200

    Do move count pruning only if (ss-1)->statScore > -40000. bench 4749155

--- src/search.cpp
+++ src/search.cpp
@@ -890,7 +890,7 @@ moves_loop: // When in check, search starts from here
       givesCheck = gives_check(pos, move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY] - ((ss-1)->statScore + 16000) / 40000;
+                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
 
       // Step 13. Extensions (~70 Elo)
 
@@ -934,7 +934,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
-              if (moveCountPruning)
+              if (moveCountPruning && (ss-1)->statScore > -40000)
               {
                   skipQuiets = true;
                   continue;
