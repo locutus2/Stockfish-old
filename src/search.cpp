6642bbde1977fdf53e5c54629f20309fa11f343e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-05-26 19:06:45 +0200

    Rewrite null move reduction formula for smoother eval effect. bench 4947000

--- src/search.cpp
+++ src/search.cpp
@@ -756,7 +756,7 @@ namespace {
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and value
-        Depth R = ((823 + 67 * depth / ONE_PLY) / 256 + std::min((eval - beta) / PawnValueMg, 3)) * ONE_PLY;
+        Depth R = ((1646 + depth / ONE_PLY * 134 + std::min(int(eval - beta), 512) * 3) / 512) * ONE_PLY;
 
         ss->currentMove = MOVE_NULL;
         ss->contHistory = thisThread->contHistory[NO_PIECE][0].get();
