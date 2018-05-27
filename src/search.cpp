7a37f72ae1ac27cc5f8c40b15360c85243d966db
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-05-27 21:51:28 +0200

    Scale now too match quiet stat scores range. bench 4779355

--- src/search.cpp
+++ src/search.cpp
@@ -996,7 +996,7 @@ moves_loop: // When in check, search starts from here
 
           if (captureOrPromotion) // (~5 Elo)
           {
-              ss->statScore = thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())];
+              ss->statScore = (7 * thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 66536) / 2;
 
               //Increase reduction by comparing opponent's stat score
               if (  (ss-1)->statScore >= 0
