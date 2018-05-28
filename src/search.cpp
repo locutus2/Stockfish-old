3424dc0852fb515e74bf5355b82a603a61090e78
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-05-28 18:35:17 +0200

    Momentum factor 1/36. bench 5047495

--- src/search.cpp
+++ src/search.cpp
@@ -444,7 +444,7 @@ void Thread::search() {
               estimatedValue[PVIdx] = bestValue;
           else
           {
-              estimatedValueChange[PVIdx] += (bestValue - estimatedValue[PVIdx]) / 28;
+              estimatedValueChange[PVIdx] += (bestValue - estimatedValue[PVIdx]) / 36;
               estimatedValue[PVIdx] = bestValue + estimatedValueChange[PVIdx];
           }
 
