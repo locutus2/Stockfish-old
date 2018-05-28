4236136db118023daf4f2d9d8013617889044ef1
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-05-28 16:31:26 +0200

    Exclude supported pawns for connected bonus when the support pawn is not safe. bench 5390460

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -92,6 +92,8 @@ namespace {
     e->pawnsOnSquares[Us][BLACK] = popcount(ourPawns & DarkSquares);
     e->pawnsOnSquares[Us][WHITE] = pos.count<PAWN>(Us) - e->pawnsOnSquares[Us][BLACK];
 
+    Bitboard safe = e->pawnAttacks[Us] | ~pawn_attacks_bb<Them>(theirPawns);
+
     // Loop through all pawns of the current color and score each pawn
     while ((s = *pl++) != SQ_NONE)
     {
@@ -137,7 +139,7 @@ namespace {
         }
 
         // Score this pawn
-        if (supported | phalanx)
+        if ((supported & safe) | phalanx)
             score += Connected[opposed][bool(phalanx)][popcount(supported)][relative_rank(Us, s)];
 
         else if (!neighbours)
