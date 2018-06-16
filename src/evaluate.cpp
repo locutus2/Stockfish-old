99573995a583eabdde3aedd2dba6c5ed5bd62300
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-06-16 09:10:57 +0200

    Factor 2. bench 4458999

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -782,7 +782,7 @@ namespace {
                     + 12 * pos.count<PAWN>()
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    +      pawnMobility
+                    +  2 * pawnMobility
                     -136 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
