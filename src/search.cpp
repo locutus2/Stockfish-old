191134b05c0bd0b02db6a8377f9d913cb704d54f
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-06-16 12:37:18 +0200

    No check extension for tt move. bench 4272756

--- src/search.cpp
+++ src/search.cpp
@@ -909,6 +909,7 @@ moves_loop: // When in check, search starts from here
       }
       else if (    givesCheck // Check extension (~2 Elo)
                && !moveCountPruning
+               &&  move != ttMove
                &&  pos.see_ge(move))
           extension = ONE_PLY;
 
