5d09fdcda5161f68309136c237708830d9bc0f45
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-06-16 11:23:33 +0200

    double effect. bench 4528276

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -775,11 +775,11 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * outflanking
                     +  8 * pe->pawn_asymmetry()
-                    + 14 * pos.count<PAWN>( strongSide)
+                    + 16 * pos.count<PAWN>( strongSide)
                     + 12 * pos.count<PAWN>(~strongSide)
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -140;
+                    -144;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
