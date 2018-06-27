bea6006d176fd6f9a305e21914660db4d95a4e74
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-06-27 12:19:11 +0200

    Second quiet stat update. bench 4577273

--- src/search.cpp
+++ src/search.cpp
@@ -1153,8 +1153,12 @@ moves_loop: // When in check, search starts from here
     {
         // Quiet best move: update move sorting heuristics
         if (!pos.capture_or_promotion(bestMove))
+        {
+            update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount,
+                               stat_bonus((depth + (bestValue > beta + PawnValueMg ? ONE_PLY : DEPTH_ZERO)) / 2));
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount,
                                stat_bonus(depth + (bestValue > beta + PawnValueMg ? ONE_PLY : DEPTH_ZERO)));
+        }
         else
             update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth + ONE_PLY));
 
