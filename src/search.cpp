25c62e2fa0dd46204f804d9a386d32d7c734da9c
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-24 15:11:16 +0200

    Use two move history for stat score. bench 4413368

--- src/search.cpp
+++ src/search.cpp
@@ -1027,6 +1027,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
+                             + (*(ss-1)->twoMoveHistory)[movedPiece][to_sq(move)]
                              - 4000;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
