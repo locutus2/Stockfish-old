243fd1a2d0044c2484e6c39032cec212e3f793f0
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-27 16:52:11 +0200

    Use counter move only for pvDistance < 8. bench 4766254

--- src/search.cpp
+++ src/search.cpp
@@ -1288,7 +1288,7 @@ moves_loop: // When in check, search starts from here
 
     const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory, nullptr, (ss-4)->continuationHistory };
     Square prevSq = to_sq((ss-1)->currentMove);
-    Move countermove = ss->pvDistance < 4 ? thisThread->counterMoves[pos.piece_on(prevSq)][prevSq] : MOVE_NONE;
+    Move countermove = ss->pvDistance < 8 ? thisThread->counterMoves[pos.piece_on(prevSq)][prevSq] : MOVE_NONE;
 
     // Initialize a MovePicker object for the current position, and prepare
     // to search the moves. Because the depth is <= 0 here, only captures,
