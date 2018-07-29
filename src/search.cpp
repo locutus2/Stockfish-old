a87cca4964a5a2f7be0f0b484a92504285b44404
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-29 14:16:03 +0200

    Quadratic factor 25 (keep maximum and minimum the same). bench 4573602

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 41 * d * d - 78 * d + 70;
+    return d > 17 ? 0 : 25 * d * d + 210 * d - 202;
   }
 
   // Skill structure is used to implement strength limit
