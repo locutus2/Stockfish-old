df3053508a7e9e3fcdf82280b30fa06117d3ce91
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-07-29 13:29:05 +0200

    In addition keep minimum the same. bench 4834779

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 36 * d * d + 11 * d + 2;
+    return d > 17 ? 0 : 36 * d * d + 12 * d - 15;
   }
 
   // Skill structure is used to implement strength limit
