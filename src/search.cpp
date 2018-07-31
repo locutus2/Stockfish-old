b1b010bcc3fae151e66cbb69b96a84cbd7fc43e3
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-07-31 20:50:55 +0200

    Try cmh3 in quiet move ordering. bench 4359143

--- src/search.cpp
+++ src/search.cpp
@@ -835,7 +835,8 @@ namespace {
 
 moves_loop: // When in check, search starts from here
 
-    const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory, nullptr, (ss-4)->continuationHistory };
+    const PieceToHistory* contHist[] = { (ss-1)->continuationHistory, (ss-2)->continuationHistory,
+                                         (ss-3)->continuationHistory, (ss-4)->continuationHistory };
     Move countermove = thisThread->counterMoves[pos.piece_on(prevSq)][prevSq];
 
     MovePicker mp(pos, ttMove, depth, &thisThread->mainHistory,
