1081f948ee0d0350f95b8b770a2f15b8210b1acf
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-05 02:28:35 +0200

    More reduction if quiet non-pawn move end on a square which is attacked by an opponent pawn. bench 4656854

--- src/search.cpp
+++ src/search.cpp
@@ -1003,6 +1003,11 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r += ONE_PLY;
 
+              // Increase reduction if non-pawn moves to a pawn attacked square
+              if (   type_of(movedPiece) != PAWN
+                  && (PawnAttacks[us][to_sq(move)] & pos.pieces(~us, PAWN)))
+                  r += ONE_PLY;
+
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
                   r += 2 * ONE_PLY;
