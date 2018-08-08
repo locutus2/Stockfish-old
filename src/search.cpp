9c5a9d596f1244882cc7128df8a66ead9194dc24
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-08 23:12:27 +0200

    Bugfix. Less reduction at cut nodes if stat score > 0. bench 4620577

--- src/search.cpp
+++ src/search.cpp
@@ -1026,7 +1026,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += (ss->statScore > 0 ? 3 : 2) * ONE_PLY;
+                  r += (ss->statScore > 0 ? 1 : 2) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
