b409d76a96f493523204870cd0431ceef6ffc72a
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-09 08:18:34 +0200

    Quadratic interpolation of two last test results give an maximum of around 0.54 less reductions. Add this in stat score reductions. bench 4799963

--- src/search.cpp
+++ src/search.cpp
@@ -1026,7 +1026,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += (ss->statScore > 20000 ? 1 : 2) * ONE_PLY;
+                  r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
@@ -1043,7 +1043,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= ss->statScore / 20000 * ONE_PLY;
+              r -= (ss->statScore + 10912 * (cutNode && ss->statScore > 0)) / 20000 * ONE_PLY;
           }
 
           Depth d = std::max(newDepth - std::max(r, DEPTH_ZERO), ONE_PLY);
