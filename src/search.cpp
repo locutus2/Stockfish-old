2c51cb3675cd1077a6beac5668e7c1d3f2146214
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-08-09 16:13:52 +0200

    Only one ply less reduction for escape moves if reductions < 6 plies. bench 4262786

--- src/search.cpp
+++ src/search.cpp
@@ -1021,7 +1021,7 @@ moves_loop: // When in check, search starts from here
               // hence break make_move(). (~5 Elo)
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
-                  r -= ONE_PLY;
+                  r -= (r < 6 * ONE_PLY ? 1 : 2) * ONE_PLY;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
