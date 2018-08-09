bab3ed359f39696e504c947a5cb8979d8b4f6897
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-08-09 21:09:17 +0200

    Compensate by decreasing mn depth to 6. bench 2241047

--- src/search.cpp
+++ src/search.cpp
@@ -905,7 +905,7 @@ moves_loop: // When in check, search starts from here
       // then that move is singular and should be extended. To verify this we do
       // a reduced search on on all the other moves but the ttMove and if the
       // result is lower than ttValue minus a margin then we will extend the ttMove.
-      if (    depth >= 8 * ONE_PLY
+      if (    depth >= 6 * ONE_PLY
           &&  move == ttMove
           && !rootNode
           && !excludedMove // Recursive singular search is not allowed
