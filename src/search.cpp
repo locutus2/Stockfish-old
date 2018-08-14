d875dd5bb1a291b04fe2cde5d6f0101aa2bfdbc7
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-08-14 11:24:18 +0200

    No move count pruning if best value > static eval at PV nodes. (park with prio -1). bench 4746164

--- src/search.cpp
+++ src/search.cpp
@@ -1104,6 +1104,7 @@ moves_loop: // When in check, search starts from here
       if (value > bestValue)
       {
           bestValue = value;
+          bestMoveIsOptimistic = PvNode && value > ss->staticEval;
 
           if (value > alpha)
           {
@@ -1113,10 +1114,7 @@ moves_loop: // When in check, search starts from here
                   update_pv(ss->pv, move, (ss+1)->pv);
 
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
-              {
                   alpha = value;
-                  bestMoveIsOptimistic = value > ss->staticEval;
-              }
               else
               {
                   assert(value >= beta); // Fail high
