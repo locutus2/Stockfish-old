7d102e831b904c6a5d03af72a39f0d2f7a6920fb
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-08-20 19:12:41 +0200

    After retrieval of eval from TT apply also the stat score penalty. bench 4380697

--- src/search.cpp
+++ src/search.cpp
@@ -712,8 +712,10 @@ namespace {
     else if (ttHit)
     {
         // Never assume anything on values stored in TT
-        if ((ss->staticEval = pureStaticEval = eval = tte->eval()) == VALUE_NONE)
-            eval = ss->staticEval = (pureStaticEval = evaluate(pos)) - 10 * ((ss-1)->statScore > 0);
+        if ((pureStaticEval = tte->eval()) == VALUE_NONE)
+            pureStaticEval = evaluate(pos);
+
+        eval = ss->staticEval = pureStaticEval - 10 * ((ss-1)->statScore > 0);
 
         // Can ttValue be used as a better position evaluation?
         if (    ttValue != VALUE_NONE
