85b1b4d220e8327b8ca3ed3a0bef054547886fde
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-08-21 22:36:40 +0200

    Use king danger/4 +compensation in initiative. bench 4108860

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -774,8 +774,8 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    +      kingDanger / 2
-                    -198;
+                    +      kingDanger / 4
+                    -167;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
