aee9a7e915bd451fc21058b48305e1c9f793e45e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-22 02:54:02 +0200

    Weight=24. bench 4900497

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,7 +765,7 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
-                    + 26 * pawns
+                    + 24 * pawns
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
