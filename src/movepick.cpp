51e88d44019dca8ef535e0d9356d3b9b27a3486d
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-26 22:37:26 +0200

    Swap the order of first and second second killer for move ordering. bench 5017696

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -62,7 +62,7 @@ namespace {
 MovePicker::MovePicker(const Position& p, Move ttm, Depth d, const ButterflyHistory* mh,
                        const CapturePieceToHistory* cph, const PieceToHistory** ch, Move cm, Move* killers)
            : pos(p), mainHistory(mh), captureHistory(cph), continuationHistory(ch),
-             refutations{{killers[0], 0}, {killers[1], 0}, {cm, 0}}, depth(d) {
+             refutations{{killers[1], 0}, {killers[0], 0}, {cm, 0}}, depth(d) {
 
   assert(d > DEPTH_ZERO);
 
