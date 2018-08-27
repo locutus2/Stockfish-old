4a3ddf376f09af8632095b9e27a5c5f8445a4566
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-08-27 13:15:21 +0200

    Bugfix: if excluded move set ignore tt hit + simplify position key. bench 4190882

--- src/search.cpp
+++ src/search.cpp
@@ -618,8 +618,12 @@ namespace {
     // search to overwrite a previous full search TT value, so we use a different
     // position key in case of an excluded move.
     excludedMove = ss->excludedMove;
-    posKey = pos.key() ^ Key(excludedMove << 16); // Isn't a very good hash
+    posKey = pos.key();
     tte = TT.probe(posKey, ttHit);
+
+    if (excludedMove)
+        ttHit = false;
+
     ttValue = ttHit ? value_from_tt(tte->value(), ss->ply) : VALUE_NONE;
     ttMove =  rootNode ? thisThread->rootMoves[thisThread->pvIdx].pv[0]
             : ttHit    ? tte->move() : MOVE_NONE;
