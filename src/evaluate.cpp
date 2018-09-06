3d2c07d5ef3dea7023f02348c6923e8a3ca35b92
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-09-07 00:55:53 +0200

    Now use only negative trend. bench 4568915

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -771,7 +771,7 @@ namespace {
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    +      trend / 8
+                    +      std::min(trend / 8, 0)
                     -118;
 
     // Now apply the bonus: note that we find the attacking side by extracting
