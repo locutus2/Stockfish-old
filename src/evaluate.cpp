367f231aeffa2966c5ad4cc9283294e16bbfa595
Stefan Geschwentner <stgeschwentner@gmail.com>
2018-09-10 17:40:24 +0200

    Weighted average. current : next = 7 : 1. bench 4322511

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,7 @@ namespace {
         Square s = pop_lsb(&b);
 
         if (relative_rank(Us, s) != RANK_7 && pos.empty(s + Up))
-            score += (single_passed<Us>(s) * 3 + single_passed<Us>(s + Up)) / 4;
+            score += (single_passed<Us>(s) * 7 + single_passed<Us>(s + Up)) / 8;
         else
             score += single_passed<Us>(s);
     }
