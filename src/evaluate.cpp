7a669127dfdc04f59dca1ba8a2aa5cbf4f28d2a3
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-09-12 00:04:29 +0200

    Weight 12. bench 3998143

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,12 +765,12 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   8 * pe->pawn_asymmetry()
-                    +  8 * pawnImbalance
+                    + 12 * pawnImbalance
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
-                    -124 ;
+                    -118 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
