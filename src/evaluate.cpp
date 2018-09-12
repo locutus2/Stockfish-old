b1d9a262355073158d413e8c049afe30e2236919
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-09-12 22:10:43 +0200

    Weight pawnsOnBothFlanks by reverse pawn count (50%). bench 4352621

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -765,7 +765,7 @@ namespace {
     int complexity =   8 * pe->pawn_asymmetry()
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
-                    +      pawnsOnBothFlanks * (40 - 5 * pos.count<PAWN>() / 2)
+                    +      pawnsOnBothFlanks * (28 - 5 * pos.count<PAWN>() / 4)
                     + 48 * !pos.non_pawn_material()
                     -118;
 
