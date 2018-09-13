1e9d4fd75f5699d942384c1939a6354c131cde70
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-09-13 21:53:29 +0200

    Use also compensation. bench 4368515

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -768,7 +768,7 @@ namespace {
                     + 16 * pawnsOnBothFlanks
                     + 16 * (pos.count<QUEEN>() > 0)
                     + 48 * !pos.non_pawn_material()
-                    -118 ;
+                    -128;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
