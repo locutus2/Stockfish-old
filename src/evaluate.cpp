49ccc6d7ca9b4c34e5bc31a1efc8132607173e5e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2018-09-13 22:18:13 +0200

    Double weight to 32. bench 4768144

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -766,9 +766,9 @@ namespace {
                     + 12 * pos.count<PAWN>()
                     + 12 * outflanking
                     + 16 * pawnsOnBothFlanks
-                    + 16 * (pos.count<QUEEN>() > 0)
+                    + 32 * (pos.count<QUEEN>() > 0)
                     + 48 * !pos.non_pawn_material()
-                    -128;
+                    -118;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
