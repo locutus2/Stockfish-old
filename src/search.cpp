1a1572ca1db03bc5b01d8c238d60a246b22e3232
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-06 11:39:27 +0200

    Decrease quadratic factor further and keep bonus for depth 1 the same. Bench: 3437075

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 28 * d * d + 155 * d - 150;
+    return d > 17 ? 0 : 27 * d * d + 172 * d - 166;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
