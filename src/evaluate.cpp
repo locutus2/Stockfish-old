dc6162883ee1238d6d3d41d6d62feaa74cda9ff3
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-11 23:41:51 +0200

    Decrease king proximity Us bonus by 1/8 and increase king proximity Them by 1/8. Bench: 3662556

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -634,7 +634,7 @@ namespace {
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
-            bonus += make_score(0, (  king_proximity(Them, blockSq) * 35
+            bonus += make_score(0, (  king_proximity(Them, blockSq) * 45
                                     - king_proximity(Us,   blockSq) * 14) * w / 8);
 
             // If blockSq is not the queening square then consider also a second push
