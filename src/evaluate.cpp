6c3578e853ceee6cdefb9c4a4c298b39a66f3800
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-12 13:13:37 +0200

    weight+1. Bench: 3433918

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -630,7 +630,7 @@ namespace {
 
         if (r > RANK_3)
         {
-            int w = r * (9 * r - 38) / 8 + 6;
+            int w = (r-2) * (r-2) + 3;
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
