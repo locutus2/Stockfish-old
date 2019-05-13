39cf82fa93691d584ef33fed5baebf41f6868cfc
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-13 19:05:38 +0200

    Use average of ttValue and beta as base margin. Bench: 3160231

--- src/search.cpp
+++ src/search.cpp
@@ -904,7 +904,7 @@ moves_loop: // When in check, search starts from here
           &&  tte->depth() >= depth - 3 * ONE_PLY
           &&  pos.legal(move))
       {
-          Value singularBeta = ttValue - 2 * depth / ONE_PLY;
+          Value singularBeta = (ttValue + beta) / 2 - 2 * depth / ONE_PLY;
           Depth halfDepth = depth / (2 * ONE_PLY) * ONE_PLY; // ONE_PLY invariant
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, halfDepth, cutNode);
