9ec40b670c7ad8a439085a6777c5c1e9072e2f31
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-15 17:37:43 +0200

    Higher bonus 100. Bench: 3784509

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -480,7 +480,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +  40 * outpostsKingAttack[Them]
+                 + 100 * outpostsKingAttack[Them]
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
