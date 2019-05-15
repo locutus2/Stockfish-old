7aee83b257f2ec3ec6d7c3a3af9f05bf9f111690
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-15 16:43:52 +0200

    Lower bonus 40. Bench: 3725717

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -480,7 +480,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +  70 * outpostsKingAttack[Them]
+                 +  40 * outpostsKingAttack[Them]
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
