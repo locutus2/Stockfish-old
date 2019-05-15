d21e88eeb08ecd61149bb8e419db1c91d1526f97
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-15 18:36:30 +0200

    Add pawns to phase calculation (Inspired by miguel's test). Bench: 3294749

--- src/material.cpp
+++ src/material.cpp
@@ -117,6 +117,8 @@ namespace Material {
 
 Entry* probe(const Position& pos) {
 
+  constexpr Value PawnValuePhase = PawnValueMg;
+
   Key key = pos.material_key();
   Entry* e = pos.this_thread()->materialTable[key];
 
@@ -129,10 +131,10 @@ Entry* probe(const Position& pos) {
 
   Value npm_w = pos.non_pawn_material(WHITE);
   Value npm_b = pos.non_pawn_material(BLACK);
-  Value npm   = clamp(npm_w + npm_b, EndgameLimit, MidgameLimit);
+  Value npm   = clamp(npm_w + npm_b + pos.count<PAWN>() * PawnValuePhase, EndgameLimit, MidgameLimit + 16 * PawnValuePhase);
 
   // Map total non-pawn material into [PHASE_ENDGAME, PHASE_MIDGAME]
-  e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit - EndgameLimit));
+  e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit + 16 * PawnValuePhase - EndgameLimit));
 
   // Let's look if we have a specialized evaluation function for this particular
   // material configuration. Firstly we look for a fixed configuration one, then
