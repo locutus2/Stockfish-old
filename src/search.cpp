2ecbe0ac31bd9808dd06b446715ac26c44e8be9d
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-16 16:22:03 +0200

    More reduction for higher depth at non-PV nodes. reduction = depth/12. Bench: 3789336

--- src/search.cpp
+++ src/search.cpp
@@ -1022,6 +1022,11 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
+
+          // Increase reduction for higher depth at non-PV nodes
+          if (!PvNode)
+              r += depth / (12 * ONE_PLY) * ONE_PLY;
+
           // Decrease reduction if move has been singularly extended
           r -= singularExtensionLMRmultiplier * ONE_PLY;
 
