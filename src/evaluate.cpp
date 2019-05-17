9c4e6bbe81e3c7c30c5a89cb4227ad3ff90a8cac
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-17 17:59:09 +0200

    Allow rook attacks through non-pawns. Bench: 3427851

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -649,12 +649,12 @@ namespace {
                 // in the pawn's path attacked or occupied by the enemy.
                 defendedSquares = unsafeSquares = squaresToQueen = forward_file_bb(Us, s);
 
-                bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
+                bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN) & attacks_bb<ROOK>(s, pos.pieces(PAWN));
 
-                if (!(pos.pieces(Us) & bb & attacks_bb<ROOK>(s, pos.pieces(Them))))
+                if (!(pos.pieces(Us) & bb))
                     defendedSquares &= attackedBy[Us][ALL_PIECES];
 
-                if (!(pos.pieces(Them) & bb & attacks_bb<ROOK>(s, pos.pieces(Us))))
+                if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
 
                 // If there aren't any enemy attacks, assign a big bonus. Otherwise
