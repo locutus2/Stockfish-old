9f69d4f4f140dd1dc9be42f6e9ce5cf55fc12c78
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-18 11:48:00 +0200

    w = r*(r-5)+9. Bench: 3576095

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -630,7 +630,7 @@ namespace {
 
         if (r > RANK_3)
         {
-            int w = r * (r-5) + 10;
+            int w = r * (r-5) + 9;
             Square blockSq = s + Up;
 
             // Adjust bonus based on the king's proximity
