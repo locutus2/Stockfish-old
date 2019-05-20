7e4cb2ca62b40ad55aee7abd4b667e19e10ec943
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-20 14:09:26 +0200

    Non-linear phase interpolation with more weight on the midgame score. Bench: 3317836

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -838,10 +838,10 @@ namespace {
 
     // Interpolate between a middlegame and a (scaled by 'sf') endgame score
     ScaleFactor sf = scale_factor(eg_value(score));
-    v =  mg_value(score) * int(me->game_phase())
-       + eg_value(score) * int(PHASE_MIDGAME - me->game_phase()) * sf / SCALE_FACTOR_NORMAL;
+    v =  mg_value(score) * int(me->game_phase()) * int(2 * PHASE_MIDGAME - me->game_phase())
+       + eg_value(score) * int(PHASE_MIDGAME - me->game_phase()) * int(PHASE_MIDGAME - me->game_phase()) * sf / SCALE_FACTOR_NORMAL;
 
-    v /= PHASE_MIDGAME;
+    v /= PHASE_MIDGAME * PHASE_MIDGAME;
 
     // In case of tracing add all remaining individual evaluation terms
     if (T)
