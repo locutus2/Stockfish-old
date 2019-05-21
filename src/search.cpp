4554ae59f06d10a251124d18be90997460ee6ff1
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-05-21 19:30:23 +0200

    Do more singular extension LMR if capture or promotion. Bench: 3598571

--- src/search.cpp
+++ src/search.cpp
@@ -1022,8 +1022,10 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
+
           // Decrease reduction if move has been singularly extended
-          r -= singularExtensionLMRmultiplier * ONE_PLY;
+          if (singularExtensionLMRmultiplier)
+              r -= (singularExtensionLMRmultiplier - captureOrPromotion) * ONE_PLY;
 
           if (!captureOrPromotion)
           {
