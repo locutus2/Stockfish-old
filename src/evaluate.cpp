763439a19b3da383918c4b03501dd0834a89737e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-25 05:37:29 +0200

    Use rook attacks only for unsafe squares. Bench: 3511475

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -651,11 +651,10 @@ namespace {
 
                 bb = forward_file_bb(Them, s) & pos.pieces(ROOK, QUEEN);
 
-                if (r <= RANK_5)
-                    bb &= pos.attacks_from<ROOK>(s);
-
                 if (!(pos.pieces(Us) & bb))
                     defendedSquares &= attackedBy[Us][ALL_PIECES];
+
+                bb &= pos.attacks_from<ROOK>(s);
 
                 if (!(pos.pieces(Them) & bb))
                     unsafeSquares &= attackedBy[Them][ALL_PIECES] | pos.pieces(Them);
