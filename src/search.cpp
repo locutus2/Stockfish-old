3e17e8c616f736d85a47fa89dccec089625ea0d1
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-25 18:11:58 +0200

    Do now also for captures less reduction if counter move and move count pruning active. Bench: 3894100

--- src/search.cpp
+++ src/search.cpp
@@ -1022,6 +1022,11 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if opponent's move count is high (~10 Elo)
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
+
+          // Decrease reduction if counter move and move count pruning active
+          if (move == countermove && moveCountPruning)
+              r -= ONE_PLY;
+
           // Decrease reduction if move has been singularly extended
           r -= singularExtensionLMRmultiplier * ONE_PLY;
 
