2097527acdbcbe23cb36dc3470e92f738ab03050
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-26 09:58:11 +0200

    compensation of +2. Bench: 3219149

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -667,7 +667,7 @@ namespace {
                 // If the path to the queen is fully defended, assign a big bonus.
                 // Otherwise assign a smaller bonus if the block square is defended.
                 if (defendedSquares == squaresToQueen)
-                    k += 9;
+                    k += 8;
 
                 else if (defendedSquares & blockSq)
                     k += 4;
