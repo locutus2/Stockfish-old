a5743722aa0d6bed82c54bbe245bfcb5e1dc7f61
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-05-31 12:29:13 +0200

    Decrease safe rook checks byonly 50. Bench: 3226269

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -82,7 +82,7 @@ namespace {
 
   // Penalties for enemy's safe checks
   constexpr int QueenSafeCheck  = 780;
-  constexpr int RookSafeCheck   = 880;
+  constexpr int RookSafeCheck   = 1030;
   constexpr int BishopSafeCheck = 635;
   constexpr int KnightSafeCheck = 790;
 
