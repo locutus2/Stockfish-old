098185cfa6ff0218250c872c3dbadbf543a0dc9d
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-06-03 21:41:02 +0200

    Increase endgame spread by 8 cp for bishop mobility. Bench: 3187099

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -94,7 +94,7 @@ constexpr int spreadSNeg = 0, fixedSNeg = 0;
 #define SN(mg, eg) make_score(mg+spreadSNmg*(mg-fixedSNmg)/(33+62), eg+spreadSNeg*(eg-fixedSNeg)/(33+81))
 
 constexpr int spreadSBmg = 0, fixedSBmg = 0;
-constexpr int spreadSBeg = 0, fixedSBeg = 0;
+constexpr int spreadSBeg = 17, fixedSBeg = 42;
 
 #define SB(mg, eg) make_score(mg+spreadSBmg*(mg-fixedSBmg)/(98+48), eg+spreadSBeg*(eg-fixedSBeg)/(97+59))
 
