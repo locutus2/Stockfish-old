c57a4c856ea148023d2b07633710cdfc28aea7fd
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-06-04 17:10:41 +0200

    Less reduction if after pawn move opponent king safety < -150. Bench: 3546766

--- src/search.cpp
+++ src/search.cpp
@@ -1029,6 +1029,10 @@ moves_loop: // When in check, search starts from here
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
 
+          // Decrease reduction of a pawn move if opponents king safety < -150
+          if(type_of(movedPiece) == PAWN && kingSafety(~us) < -150)
+              r -= ONE_PLY;
+
           // Decrease reduction if move has been singularly extended
           r -= singularExtensionLMRmultiplier * ONE_PLY;
 
@@ -1065,10 +1069,6 @@ moves_loop: // When in check, search starts from here
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 20000 * ONE_PLY;
           }
-
-          // Decrease reduction if opponents king safety < -100
-          else if(kingSafety(~us) < -100)
-              r -= ONE_PLY;
 
           Depth d = std::max(newDepth - std::max(r, DEPTH_ZERO), ONE_PLY);
 
