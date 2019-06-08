55000e0363f498b5e4efc8bc32b7e3873182ae19
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-06-08 16:14:30 +0200

    No LMR at root if move gives check. Bench: 3663253

--- src/search.cpp
+++ src/search.cpp
@@ -1009,7 +1009,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3 * ONE_PLY
           &&  moveCount > 1 + 3 * rootNode
-          && (!rootNode || !inCheck)
+          && (!rootNode || !givesCheck)
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha))
