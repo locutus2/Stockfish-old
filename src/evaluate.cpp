30cf1e066ba2bf69fa3fb1e9edaadbdde14ad67c
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-06-14 17:03:52 +0200

    Double effect. Bench: 3554860

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -468,7 +468,7 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +       eg_value(passedScore[Them]) / 8
+                 +       eg_value(passedScore[Them])
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
