40eaf5516e4637fa2dd88cc1df2c4cd81094edca
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-06-14 12:05:00 +0200

    Use difference of opponent and own threat scores for king danger. Bench: 3778953

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -467,9 +467,9 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
+                 +       mg_value(threatScore[Them] - threatScore[Us]) / 2
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +       mg_value(threatScore[Them]) / 8
-                 -  21;
+                 -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
