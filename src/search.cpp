e4da5a3aa53fe943197739909c2e05db8a4646ba
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-06-28 18:09:30 +0200

    Use twoMoveHistory for quiet stat scores in LMR. Bench: 3173299

--- src/search.cpp
+++ src/search.cpp
@@ -1060,6 +1060,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
+                             + (*ss->twoMoveHistory)[movedPiece][to_sq(move)]
                              - 4000;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
