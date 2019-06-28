4e0ce310b5c926882e9211b2f15d97c666432a92
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-06-28 18:22:11 +0200

    Add compensation to stat score. Bench: 3668647

--- src/search.cpp
+++ src/search.cpp
@@ -1061,7 +1061,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
                              + (*ss->twoMoveHistory)[movedPiece][to_sq(move)]
-                             - 4000;
+                             - 3570;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)
