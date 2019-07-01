5648805013978d4033bcf986e4b690279a57433b
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-01 15:29:06 +0200

    Exclude counter moves with main history >= 10000 from cmh based pruning. Bench: 3950987

--- src/search.cpp
+++ src/search.cpp
@@ -1024,7 +1024,8 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                  && (thisThread->mainHistory[us][from_to(move)] < 10000 || countermove != move))
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
