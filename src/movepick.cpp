0cac3f319a17e2f13490c7084a60d8fdd48304d3
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-10 22:48:00 +0200

    Now increase the SEE threshold for good captures by around 55%. Test against VoyagerOne's passed capScoreT4 patch. Bench: 3841512

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,7 +174,7 @@ top:
 
   case GOOD_CAPTURE:
       if (select<Best>([&](){
-                       return pos.see_ge(*cur, Value(-cur->value / 14)) ?
+                       return pos.see_ge(*cur, Value(-cur->value / 12)) ?
                               // Move losing capture to endBadCaptures to be tried later
                               true : (*endBadCaptures++ = *cur, false); }))
           return *(cur - 1);
