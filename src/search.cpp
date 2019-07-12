5a630a9aea4cb31e4481caa2b62a27fd06d7db57
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-12 09:02:30 +0200

    Use depth instead of newDepth for LMR stat update. Test against my passed lmr_update2_PR patch. Bench: 3429358

--- src/search.cpp
+++ src/search.cpp
@@ -1134,7 +1134,7 @@ moves_loop: // When in check, search starts from here
 
           if (doLMR && !captureOrPromotion)
           {
-              int bonus = stat_bonus(newDepth);
+              int bonus = stat_bonus(depth) / 2;
               if (value > alpha)
                   update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
               else
