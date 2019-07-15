e1034ec098d039ff679aedfcd0110e14399a5bed
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-15 17:05:15 +0200

    No null move pruning if own pinned knight exists (the knights have no mobility then). Bench: 3552041

--- src/search.cpp
+++ src/search.cpp
@@ -794,7 +794,7 @@ namespace {
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
         && !excludedMove
         &&  pos.non_pawn_material(us)
-        && !pos.blockers_for_king(us)
+        && !(pos.blockers_for_king(us) & pos.pieces(us, KNIGHT))
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
     {
         assert(eval - beta >= 0);
