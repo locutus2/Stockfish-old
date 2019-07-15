699eca1a53bce38107a8b4e9be3136081d12b694
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-15 20:52:00 +0200

    No probcut pruning if own pinned non-pawn exists. Bench: 3495997

--- src/search.cpp
+++ src/search.cpp
@@ -783,7 +783,6 @@ namespace {
     if (   !PvNode
         &&  depth < 7 * ONE_PLY
         &&  eval - futility_margin(depth, improving) >= beta
-        && !(pos.blockers_for_king(us) & (pos.pieces(us) ^ pos.pieces(us, PAWN)))
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
@@ -841,6 +840,7 @@ namespace {
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth >= 5 * ONE_PLY
+        && !(pos.blockers_for_king(us) & (pos.pieces(us) ^ pos.pieces(us, PAWN)))
         &&  abs(beta) < VALUE_MATE_IN_MAX_PLY)
     {
         Value raisedBeta = std::min(beta + 216 - 48 * improving, VALUE_INFINITE);
