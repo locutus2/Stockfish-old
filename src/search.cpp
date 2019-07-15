220e26339d349d566999c6a7ef0d9f7d37b3b879
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-15 12:35:34 +0200

    Try the other direction and half stat bonus in null move update. Bench: 3231604

--- src/search.cpp
+++ src/search.cpp
@@ -833,7 +833,7 @@ namespace {
             if (v >= beta)
             {
                 if (!pos.captured_piece())
-                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + ONE_PLY));
+                    update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth) / 2);
 
                 return nullValue;
             }
