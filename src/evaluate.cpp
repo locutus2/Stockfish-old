4f2aabdb0b3bf6ee4c31b5451dae223869f6611f
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-17 20:19:07 +0200

    Doesn't exclude pawns blocked by own piece from mobility area. Bench: 3477157

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -229,12 +229,11 @@ namespace {
     Bitboard dblAttackByPawn = pawn_double_attacks_bb<Us>(pos.pieces(Us, PAWN));
 
     // Find our pawns that are blocked or on the first two ranks
-    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces()) | LowRanks);
+    Bitboard b = pos.pieces(Us, PAWN) & (shift<Down>(pos.pieces(Them)) | LowRanks);
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
-    mobilityArea[Us] = ~(  b | pos.pieces(Us, KING) | pe->pawn_attacks(Them)
-                         | (pos.pieces(Us, QUEEN) & ~(FileABB | FileHBB | Rank1BB | Rank8BB)));
+    mobilityArea[Us] = ~(b | pos.pieces(Us, KING, QUEEN) | pe->pawn_attacks(Them));
 
     // Initialize attackedBy[] for king and pawns
     attackedBy[Us][KING] = pos.attacks_from<KING>(ksq);
