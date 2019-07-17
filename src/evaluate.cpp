1c37b14a962df20b4b027c2d5080d4d0636741fb
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-17 20:13:13 +0200

    Doesn't exclude queen on the board edge from mobility area. Bench: 3472017

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -233,7 +233,8 @@ namespace {
 
     // Squares occupied by those pawns, by our king or queen or controlled by
     // enemy pawns are excluded from the mobility area.
-    mobilityArea[Us] = ~(b | pos.pieces(Us, KING, QUEEN) | (pe->pawn_attacks(Them) & ~pos.pieces(Us, PAWN)));
+    mobilityArea[Us] = ~(  b | pos.pieces(Us, KING) | pe->pawn_attacks(Them)
+                         | (pos.pieces(Us, QUEEN) & ~(FileABB | FileHBB | Rank1BB | Rank8BB)));
 
     // Initialize attackedBy[] for king and pawns
     attackedBy[Us][KING] = pos.attacks_from<KING>(ksq);
