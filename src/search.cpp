3a80136bee3e47295ea7b7104437d7588bbaeef2
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-26 08:57:27 +0200

    Prune only if main history < 12000 - 256 * lmrDepth. Bench: 3532661

--- src/search.cpp
+++ src/search.cpp
@@ -1048,7 +1048,7 @@ moves_loop: // When in check, search starts from here
                   continue;
 
               // Prune moves with negative SEE and not too good main history (~10 Elo)
-              if (   thisThread->mainHistory[us][from_to(move)] < 10000 - 500 * lmrDepth
+              if (   thisThread->mainHistory[us][from_to(move)] < 12000 - 256 * lmrDepth
                   && !pos.see_ge(move, Value(-(31 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
