43c8397c380fd98ebc372d04c61c6cbbb0dff81f
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-29 20:57:07 +0200

    Do no move count pruning if eval > alpha and a queen moves. Bench: 3626198

--- src/search.cpp
+++ src/search.cpp
@@ -1028,7 +1028,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning
-              if (moveCountPruning)
+              if (moveCountPruning && (eval < alpha || type_of(movedPiece) != QUEEN))
                   continue;
 
               // Reduced depth of the next LMR search
