2a96c351c32c939a04dfe32ef79121ef61064b9e
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-07-31 13:42:38 +0200

    Increase quadratic factor only from 22 to 23 but compensate to maintain depth=1 bonus (max stat bonus is now 9073). Bench: 4517603

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 25 * d * d + 151 * d - 140;
+    return d > 17 ? 0 : 23 * d * d + 151 * d - 141;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
