54e0dda532a7e2323b37f7aeb6c8c813c6e3981b
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-08-01 19:43:55 +0200

    Increase from my 67K yellow test further the quadratic factor to 24 and compensate (max stat bonus is now 9361). Bench: 3988544

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
   // History and stats update bonus, based on depth
   int stat_bonus(Depth depth) {
     int d = depth / ONE_PLY;
-    return d > 17 ? 0 : 15 * d * d + 158 * d - 140;
+    return d > 17 ? 0 : 24 * d * d + 151 * d - 142;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
