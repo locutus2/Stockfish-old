ae8733c2d0cc234a79c80427b37509308117af15
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-09 00:21:34 +0200

    threshold 8000. Bench: 4735482

--- src/search.cpp
+++ src/search.cpp
@@ -1038,7 +1038,7 @@ moves_loop: // When in check, search starts from here
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*(ss-1)->sectorHistory)[movedPiece][to_sq(move)] < 10000)
+                  && (*(ss-1)->sectorHistory)[movedPiece][to_sq(move)] < 8000)
                   continue;
 
               // Futility pruning: parent node (~2 Elo)
