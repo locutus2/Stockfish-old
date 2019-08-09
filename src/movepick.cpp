ca6e408fc5e9b9a6deffde151e8d9ce9b5044b0f
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-09 16:45:01 +0200

    use an eigth sector history in quiet move ordering. Bench: 3985637

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -117,7 +117,7 @@ void MovePicker::score() {
                    + (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    + (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)] / 2
-                   + (*sectorHistory[0])[pos.moved_piece(m)][to_sq(m)] / 4;
+                   + (*sectorHistory[0])[pos.moved_piece(m)][to_sq(m)] / 8;
 
       else // Type == EVASIONS
       {
