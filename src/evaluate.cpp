8a6f18e11bdca8457ef6f880484efefc623f20b8
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-11 00:40:53 +0200

    Give 100 units more weight for blockers. Bench: 4495988

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,8 +457,8 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 + 127 * popcount(unsafeChecks)
-                 + 177 * popcount(pos.blockers_for_king(Us))
+                 + 116 * popcount(unsafeChecks)
+                 + 216 * popcount(pos.blockers_for_king(Us))
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
