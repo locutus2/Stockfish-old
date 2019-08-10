7723a292b4c252d5b350f4cdced4e5f135f1ba5e
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-10 15:00:36 +0200

    try quadratic formula. Bench: 3699512

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -462,9 +462,8 @@ namespace {
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  +   5 * kingFlankAttacks * kingFlankAttacks / 16
-                 +   5 * relative_rank(Us, ksq)
+                 +       relative_rank(Us, ksq) * relative_rank(Us, ksq)
                  -  12;
-
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
