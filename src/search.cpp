83d1e0a0a28c31ad1fbbf0b85cb920f533316808
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-10 16:29:33 +0200

    set improving = eval trend > 0. Bench 4368479

--- src/search.cpp
+++ src/search.cpp
@@ -787,9 +787,7 @@ namespace {
         &&  eval <= alpha - RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
-    improving =   ss->evalTrend > 0
-               && (   ss->staticEval >= (ss-2)->staticEval
-                   || (ss-2)->staticEval == VALUE_NONE);
+    improving = ss->evalTrend > 0;
 
     // Step 8. Futility pruning: child node (~30 Elo)
     if (   !PvNode
