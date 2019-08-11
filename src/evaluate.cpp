cf2f270115123c939a43007399e48252034bd7f9
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-11 04:01:38 +0200

    New test indicates that value have to be even less so try difference=-75. Bench: 4281667

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -457,8 +457,8 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
-                 + 141 * popcount(unsafeChecks)
-                 + 126 * popcount(pos.blockers_for_king(Us))
+                 + 153 * popcount(unsafeChecks)
+                 +  78 * popcount(pos.blockers_for_king(Us))
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
