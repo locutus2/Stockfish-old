34ebbde548b532b292d4aa9bce0eaf4217be0a26
Stefan Geschwentner <Stefan-Geschwentner@web.de>
2019-08-16 11:07:07 +0200

    Multiple knight checks: bonus=107. Bench: 4391926

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -85,7 +85,7 @@ namespace {
   constexpr int RookSafeCheck   = 1080;
   constexpr int BishopSafeCheck = 635;
   constexpr int KnightSafeCheck = 790;
-  constexpr int MultipleKnightSafeCheck = 55;
+  constexpr int MultipleKnightSafeCheck = 107;
 
 #define S(mg, eg) make_score(mg, eg)
 
