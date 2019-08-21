7bcfc36ec8d33cf8568a34f4c84236555d66d5ca
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-08-21 17:18:06 +0200

    Do LMR for captures also if capture history <= 0. Bench: 4000907

--- src/search.cpp
+++ src/search.cpp
@@ -1075,7 +1075,8 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || (cutNode && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] <= 0)))
+              || cutNode
+              || thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] <= 0))
       {
           Depth r = reduction(improving, depth, moveCount);
 
