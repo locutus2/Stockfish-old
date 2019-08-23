8dac42d85c75de252837b9d33923902059043686
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-08-23 22:26:44 +0200

    Cut node reduction: one ply additional delayed reduction. Bench: 3849217

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += ONE_PLY, (ss+1)->delayedReduction += ONE_PLY;
+                  r += 2 * ONE_PLY, (ss+1)->delayedReduction += ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
