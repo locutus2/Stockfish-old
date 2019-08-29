bb402c1ea09da7bd5dcc76e3e7559419f35a9de3
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-08-29 20:20:18 +0200

    Double effect without compensation. Bench: 4133618

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -728,8 +728,8 @@ namespace {
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 20 * bool(pos.pieces(weakSide, QUEEN))
-                    - 95 ;
+                    - 40 * bool(pos.pieces(weakSide, QUEEN))
+                    -103 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
