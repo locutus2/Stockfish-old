1404e4a1e3c86971ce8df6bfb0fb0c5ff2d6e1e2
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-01 22:20:18 +0200

    No move count pruning if extension at PV node. Bench: 3568155

--- src/search.cpp
+++ src/search.cpp
@@ -1027,7 +1027,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material(~us) > BishopValueMg))
           {
               // Move count based pruning
-              if (moveCountPruning && (depth >= 2 * ONE_PLY || !extension))
+              if (moveCountPruning && (!PvNode || !extension))
                   continue;
 
               // Reduced depth of the next LMR search
