7631be6887f834f545353908e4b8aac55fb1ab63
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-02 22:56:21 +0200

    Consider square in front of passed pawn also not as blocked if attacked by an own pawn. For compensation skip k = 5 bonus if not free. Bench: 3809847

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -644,7 +644,7 @@ namespace {
                                                              0 ;
 
                 // Assign a larger bonus if the block square is defended
-                if ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq))
+                if (pos.empty(blockSq) && ((pos.pieces(Us) & bb) || (attackedBy[Us][ALL_PIECES] & blockSq)))
                     k += 5;
 
                 bonus += make_score(k * w, k * w);
