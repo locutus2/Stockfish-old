bd760f900a7ad1e2d04e91b83f0f12463b15c694
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-03 22:40:39 +0200

    ONE_PLY=256: use fractional plies for stat score reduction. Bench: 3282909

--- src/search.cpp
+++ src/search.cpp
@@ -1137,10 +1137,7 @@ moves_loop: // When in check, search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              if (ss->statScore > 0)
-                 r -= ss->statScore * ONE_PLY / 16384;
-              else
-                 r -= ss->statScore / 16384 * ONE_PLY;
+              r -= ss->statScore * ONE_PLY / 16384;
           }
 
           Depth d = clamp(newDepth - r, ONE_PLY, newDepth);
