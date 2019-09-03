6a0ef9cd5aa37f7284151e1a9ed09059a5c7ea9a
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-03 19:39:20 +0200

    Only endgame bonus. Bench: 3847758

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -147,7 +147,7 @@ namespace {
   constexpr Score ThreatBySafePawn   = S(173, 94);
   constexpr Score TrappedRook        = S( 47,  4);
   constexpr Score WeakQueen          = S( 49, 15);
-  constexpr Score OnlyPawnDefender   = S( 20, 20);
+  constexpr Score OnlyPawnDefender   = S(  0, 20);
 
 #undef S
 
