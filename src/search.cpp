8377003b96542b0e3dcdc9dc4dfe8dea1af929c0
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-04 11:01:08 +0200

    ONE_PLY=8. 3/8 ply more reduction at cut nodes. Based on my fractional_base branch. Bench: 3884586

--- src/search.cpp
+++ src/search.cpp
@@ -1106,7 +1106,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 * ONE_PLY + ONE_PLY / 8;
+                  r += 2 * ONE_PLY + ONE_PLY * 3 / 8;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
