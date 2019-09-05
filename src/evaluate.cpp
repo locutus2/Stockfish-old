ef2cd40fa7341517bd214ffc520d965914dfa7c4
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-05 18:29:44 +0200

    Increase imbalance score by 1/8. Bench: 4066433

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -783,11 +783,11 @@ namespace {
     // Initialize score by reading the incrementally updated scores included in
     // the position object (material + piece square tables) and the material
     // imbalance. Score is computed internally from the white point of view.
-    Score score = pos.psq_score() + me->imbalance() + pos.this_thread()->contempt;
+    Score score = pos.psq_score() + me->imbalance() * 9 / 8 + pos.this_thread()->contempt;
 
     // Probe the pawn hash table
     pe = Pawns::probe(pos);
-    score += (pe->pawn_score(WHITE) - pe->pawn_score(BLACK)) * 7 / 8;
+    score += pe->pawn_score(WHITE) - pe->pawn_score(BLACK);
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
