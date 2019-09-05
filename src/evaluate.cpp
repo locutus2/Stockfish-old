f9b83c255ae86f0fc525e14e4c7d34b2d0fe40f8
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-05 18:36:27 +0200

    Increase bishop score by 1/8. Bench: 3897813

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -800,8 +800,8 @@ namespace {
     initialize<BLACK>();
 
     // Pieces should be evaluated first (populate attack tables)
-    score +=  (pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>()) * 7 / 8
-            + pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()
+    score +=  pieces<WHITE, KNIGHT>() - pieces<BLACK, KNIGHT>()
+            + (pieces<WHITE, BISHOP>() - pieces<BLACK, BISHOP>()) * 9 / 8
             + pieces<WHITE, ROOK  >() - pieces<BLACK, ROOK  >()
             + pieces<WHITE, QUEEN >() - pieces<BLACK, QUEEN >();
 
