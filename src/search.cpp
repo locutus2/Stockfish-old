2ac816eb248ffc7aed7c997ac6e76b8d8658c40a
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 19:48:50 +0200

    Do more tt capture reduction if moveCount >= 30. Bench: 3583711

--- src/search.cpp
+++ src/search.cpp
@@ -1103,11 +1103,11 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
-                  r += ONE_PLY;
+                  r += moveCount < 30 ? ONE_PLY : 2 * ONE_PLY;
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += moveCount > 20 ? 3 * ONE_PLY : 2 * ONE_PLY;
+                  r += 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
