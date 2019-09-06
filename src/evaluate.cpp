72550fa20d127a74f12871b8da92b17a0ff6d41f
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 14:18:39 +0200

    Half effect (with compensation). Bench: 3543876

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -732,7 +732,7 @@ namespace {
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     +      evalTrend / 2
-                    -103 ;
+                    - 91 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
