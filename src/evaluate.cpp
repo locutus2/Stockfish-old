2014c297d16ac97fe7d20657983d1a15b10a29d0
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 16:42:27 +0200

    Sligthly decrease outflanking weight. Bench: 3665262

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -723,10 +723,10 @@ namespace {
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
-                    + 10 * outflanking
+                    +  8 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    -100 ;
+                    -106 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
