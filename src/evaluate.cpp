a364d4d679ed9184bdb4131689883e860c8b4a5e
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 16:41:33 +0200

    Sligthly increase outflanking weight. Bench: 4125550

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,11 +722,11 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    + 10 * pos.count<PAWN>()
-                    +  9 * outflanking
+                    + 11 * pos.count<PAWN>()
+                    + 10 * outflanking
                     + 18 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
-                    - 95 ;
+                    -100 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
