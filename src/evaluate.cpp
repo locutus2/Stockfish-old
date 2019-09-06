37c534987d7d8dfaef2c076893bb38848f4e7eda
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 18:01:53 +0200

    Sligthly more increase passed_count weight. Bench: 3508715

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -721,12 +721,12 @@ namespace {
                             && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int complexity =   9 * pe->passed_count()
+    int complexity =  11 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 18 * pawnsOnBothFlanks
-                    + 44 * !pos.non_pawn_material()
-                    -103 ;
+                    + 49 * !pos.non_pawn_material()
+                    -105 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
