44c7314c4eda7c744e4e655f03b7072288e7208a
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-06 19:31:59 +0200

    Do more cut node reduction if moveCOunt > 20. Bench: 3874302

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += moveCount > 20 ? ONE_PLY : 2 * ONE_PLY;
+                  r += moveCount > 20 ? 3 * ONE_PLY : 2 * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
