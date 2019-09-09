96da2a1cef08add351f1d612ac6fcfc026939ff7
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-09 22:10:49 +0200

    For quiet moves update stats of reverse best move with negative bonus. Only main history. Bench: 3773783

--- src/search.cpp
+++ src/search.cpp
@@ -1603,7 +1603,6 @@ moves_loop: // When in check, search starts from here
     thisThread->mainHistory[us][from_to(move)] << bonus;
     thisThread->mainHistory[us][from_to(make_move(to_sq(move), from_sq(move)))] << -bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
-    update_continuation_histories(ss, pos.moved_piece(move), from_sq(move), -bonus);
 
     if (is_ok((ss-1)->currentMove))
     {
