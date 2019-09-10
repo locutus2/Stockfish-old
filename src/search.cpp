4600c7a110151777bf3360e17938c4207d001df8
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-10 18:59:56 +0200

    Give reverse move bonus only if not a pawn move and moves backward. Bench: 3648040

--- src/search.cpp
+++ src/search.cpp
@@ -1603,7 +1603,7 @@ moves_loop: // When in check, search starts from here
     thisThread->mainHistory[us][from_to(move)] << bonus;
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
-    if (type_of(pos.moved_piece(move)) != PAWN && type_of(pos.moved_piece(move)) != KING)
+    if (type_of(pos.moved_piece(move)) != PAWN && (forward_ranks_bb(us, from_sq(move)) & to_sq(move)))
         thisThread->mainHistory[us][from_to(make_move(to_sq(move), from_sq(move)))] << -bonus;
 
     if (is_ok((ss-1)->currentMove))
