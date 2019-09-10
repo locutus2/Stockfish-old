b2ad48926d8e7b1533f11626626d3d347259fbb6
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-10 14:15:19 +0200

    Give for reverse move now only half bonus only if not a pawn move. Bench: 3699463

--- src/search.cpp
+++ src/search.cpp
@@ -1604,7 +1604,7 @@ moves_loop: // When in check, search starts from here
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
     if (type_of(pos.moved_piece(move)) != PAWN)
-        thisThread->mainHistory[us][from_to(make_move(to_sq(move), from_sq(move)))] << -bonus;
+        thisThread->mainHistory[us][from_to(make_move(to_sq(move), from_sq(move)))] << -bonus / 2;
 
     if (is_ok((ss-1)->currentMove))
     {
