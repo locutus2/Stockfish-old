a1e6175ca8e017dbf29dbbd77d42490d78f1025b
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-11 18:49:16 +0200

    If a king move use only half reverse bonus. Bench: 3568946

--- src/search.cpp
+++ src/search.cpp
@@ -1604,7 +1604,10 @@ moves_loop: // When in check, search starts from here
     update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
 
     if (type_of(pos.moved_piece(move)) != PAWN)
-        thisThread->mainHistory[us][from_to(reverse_move(move))] << -bonus;
+    {
+        int reverseBonus = type_of(pos.moved_piece(move)) == KING ? -bonus/2 : -bonus;
+        thisThread->mainHistory[us][from_to(reverse_move(move))] << reverseBonus;
+    }
 
     if (is_ok((ss-1)->currentMove))
     {
