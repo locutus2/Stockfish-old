511dc3f1f43c1317ba32cbde71599498856f6bdd
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-09-14 19:45:06 +0200

    Give 50% more pawns on both flanks bonus if pawn outside of center files exists. Weight=9. Bench: 3852979

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -722,8 +722,8 @@ namespace {
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                      - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
 
-    bool pawnsOnBothFlanks =   (pos.pieces(PAWN) & QueenSide)
-                            && (pos.pieces(PAWN) & KingSide);
+    int pawnsOnBothFlanks =  ((pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide))
+                           * (2 + bool(pos.pieces(PAWN) & ~CenterFiles));
 
     bool almostUnwinnable =   !pe->passed_count()
                            &&  outflanking < 0
@@ -733,7 +733,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    + 18 * (pawnsOnBothFlanks && (pos.pieces(PAWN) & ~CenterFiles))
+                    +  9 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
                     -103 ;
