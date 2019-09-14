a225d688469aa64135b73cf4bd2438f8bc0de05d
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-09-14 18:02:34 +0200

    Give 50% more pawns on both flanks bonus if a material imbalance exists. Weight=6. Bench: 3908734

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -733,7 +733,7 @@ namespace {
     int complexity =   9 * pe->passed_count()
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
-                    +  9 * pawnsOnBothFlanks
+                    +  6 * pawnsOnBothFlanks
                     + 49 * !pos.non_pawn_material()
                     - 36 * almostUnwinnable
                     -103 ;
