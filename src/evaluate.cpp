fbe41dff939782a6e73c314771b47ea3c828dbda
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-09-15 11:53:30 +0200

    Subtract contempt from midgame score in initiative. Bench: 3869526

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -719,7 +719,7 @@ namespace {
   template<Tracing T>
   Score Evaluation<T>::initiative(Score score) const {
 
-    Value mg = mg_value(score);
+    Value mg = mg_value(score - pos.this_thread()->contempt);
     Value eg = eg_value(score);
 
     int outflanking =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
@@ -744,7 +744,7 @@ namespace {
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
     // so that the midgame and endgame scores do not change sign after the bonus.
-    int u = ((eg > 0) - (eg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
+    int u = ((mg > 0) - (mg < 0)) * std::max(std::min(complexity + 50, 0), -abs(mg));
     int v = ((eg > 0) - (eg < 0)) * std::max(complexity, -abs(eg));
 
     if (T)
