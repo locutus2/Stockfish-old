00abc4561d9b4abe71af4ea5a2c71be076757080
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-20 12:07:08 +0200

    Increase best move counter at root also for each second fail high (starting at the second one). Bench: 4182633

--- src/search.cpp
+++ src/search.cpp
@@ -469,8 +469,8 @@ void Thread::search() {
               else if (bestValue >= beta)
               {
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
-                  ++failedHighCnt;
                   rootMoves[pvIdx].bestMoveCount += failedHighCnt % 2;
+                  ++failedHighCnt;
               }
               else
               {
