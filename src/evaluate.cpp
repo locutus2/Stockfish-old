b34229782b0f5d2712246d3254d95a265a148843
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-26 18:39:45 +0200

    Give more king flank attacks if a defender's piece is attacked by a pawn. Bench: 4056097

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -449,7 +449,7 @@ namespace {
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
     b1 = attackedBy[Them][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp;
-    b2 = b1 & (attackedBy2[Them] | (pos.pieces(Us, PAWN) & attackedBy[Them][PAWN]));
+    b2 = b1 & (attackedBy2[Them] | (pos.pieces(Us) & attackedBy[Them][PAWN]));
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
