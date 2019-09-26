8bcf9074ff4be4dc35cc2c79689564e6dd67f701
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-09-26 17:50:05 +0200

    Give more king flank attacks if square in front of storming pawn is defended and not blocked. Bench: 3887713

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -450,7 +450,7 @@ namespace {
     // Find the squares that opponent attacks in our king flank, and the squares
     // which are attacked twice in that flank.
     b1 = attackedBy[Them][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp;
-    b2 = b1 & (attackedBy2[Them] | shift<Down>(pos.pieces(Them, PAWN)));
+    b2 = b1 & (attackedBy2[Them] | (shift<Down>(pos.pieces(Them, PAWN)) & ~pos.pieces()));
 
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
