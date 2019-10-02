6e5cc330fd453fc016d3cc163b2c59fa61072d6e
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-02 22:17:39 +0200

    Add undefended squares also to king flank. Compensate by halfing. Bench: 3746824

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -458,7 +458,7 @@ namespace {
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
-                 +   5 * kingFlankAttacks * kingFlankAttacks / 16
+                 +   5 * kingFlankAttacks * kingFlankAttacks / 32
                  -   7;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
