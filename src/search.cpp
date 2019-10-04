65d082b3c8417af7fc61efb9fe42b512ebdd8ee0
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-05 01:08:40 +0200

    Scale contempt with dynamic part. Bench: 3738768

--- src/search.cpp
+++ src/search.cpp
@@ -417,7 +417,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 86 * previousScore / (abs(previousScore) + 176);
+              int dct = ct * (86 + 86 * previousScore / (abs(previousScore) + 176)) / 86;
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
