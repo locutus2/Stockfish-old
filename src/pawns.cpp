20055078067d94a523cb73a2e54c9ed047b94414
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-07 18:46:58 +0200

    Connected bonus: -25% of phalanx effect. Bench: 4037701

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (8 + 5 * bool(phalanx) - 4 * bool(opposed)) / 4
+            int v =  Connected[r] * (8 + 3 * bool(phalanx) - 4 * bool(opposed)) / 4
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
