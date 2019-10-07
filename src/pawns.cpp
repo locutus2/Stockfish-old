d6726dbcf7f50d52cebc8ffc20e4c389f5c66d7c
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-07 19:57:51 +0200

    Connected bonus: +75% of opposed effect. Bench: 4027443

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - 2 * bool(opposed))
+            int v =  Connected[r] * (8 + 4 * bool(phalanx) - 7 * bool(opposed)) / 4
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
