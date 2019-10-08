58995fe9c26c718c55e657fcce3f2c65e971d93a
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-08 11:45:22 +0200

    Stephane pointed out a nice rewrite of my 70K yellow test. Based on this try now -50% opposed effect. Bench: 3884512

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) + (phalanx && !opposed))
+            int v =  Connected[r] * (1 + bool(phalanx)) * (2 + !opposed) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
