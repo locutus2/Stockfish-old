dd0b5e437fc45b35d9a859fca054a5a7d3920f47
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-08 19:03:36 +0200

    Based on new formula: -1/2 phalanx effect. Bench: 3837602

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + 3 * bool(phalanx)) * (1 + !opposed) / 2
+            int v =  Connected[r] * (2 + bool(phalanx)) * (1 + !opposed) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
