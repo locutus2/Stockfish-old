e967cb3632433b5e555c7437a4156d21e7f6e4cf
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-08 11:46:27 +0200

    Based on new formula: +50% opposed effect. Bench: 3702292

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (1 + bool(phalanx)) * (2 + !opposed) / 2
+            int v =  Connected[r] * (1 + bool(phalanx)) * (2 + 3 * !opposed) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
