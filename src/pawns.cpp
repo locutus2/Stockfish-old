b00bc59d72c8a51b5710b8258243393f5fab0b26
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-08 15:10:29 +0200

    Connected bonus: decrease if not phalanx and opposed. Bench: 4151533

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -132,7 +132,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) + (phalanx && !opposed))
+            int v =  Connected[r] * (2 + bool(phalanx) - bool(opposed) - (!phalanx && opposed))
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
