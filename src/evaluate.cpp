c0ed705342d406b9d6e8659c308ef3bd58ed81c7
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-10 13:51:22 +0200

    Use difference of strong and weak side king safety for midgame initiaitve (divisor 8) and compensate. Bench: 3880173

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -729,8 +729,8 @@ namespace {
     Color strongSide = mg > VALUE_ZERO ? WHITE : BLACK;
 
     int complexity_mg =  complexity_eg
-                       + mg_value(kingSafety[strongSide]) / 8
-                       + 68;
+                       + mg_value(kingSafety[strongSide] - kingSafety[~strongSide]) / 8
+                       + 25;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
