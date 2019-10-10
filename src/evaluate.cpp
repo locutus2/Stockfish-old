eed3e0f40334eeae0c2959c09baddcae4adb8f24
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-10 12:38:45 +0200

    Use only weak side king safety for midgame initiaitve and compensate. Bench: 4096797

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -726,9 +726,11 @@ namespace {
                        - 36 * almostUnwinnable
                        -103 ;
 
+    Color weakSide = mg < VALUE_ZERO ? WHITE : BLACK;
+
     int complexity_mg =  complexity_eg
-                       + mg_value(kingSafety[WHITE] + kingSafety[BLACK]) / 4
-                       + 170;
+                       + mg_value(kingSafety[weakSide]) / 8
+                       + 92;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
