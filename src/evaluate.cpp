3170b0a8d44441fb5e87c84ef15dd1df91558314
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-24 11:41:12 +0200

    ComplexityPawnsOnBothFlanks = (0,18). base on PR #2354. Bench: 4244222

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -151,9 +151,9 @@ namespace {
   constexpr Score ComplexityPasser            = S(9, 9);
   constexpr Score ComplexityPawnCount         = S(11, 11);
   constexpr Score ComplexityOutflanking       = S(9, 9);
-  constexpr Score ComplexityPawnsOnBothFlanks = S(18, 18);
+  constexpr Score ComplexityPawnsOnBothFlanks = S(0, 18);
   constexpr Score ComplexityNoPieces          = S(49, 49);
-  constexpr Score ComplexityUnwinnable        = S(72, 36);
+  constexpr Score ComplexityUnwinnable        = S(36, 36);
   constexpr Score ComplexityConstant          = S(53, 103);
 
 #undef S
