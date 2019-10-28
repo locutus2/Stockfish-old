0664e9d738e68cbccb080ecd24eeb2d8a15b826f
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-28 16:08:33 +0100

    Count own pawns attacked by the bishop 50% more for BishopPawns penalty. Bench: 4670139

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BishopPawns        = S( 12, 28);
+  constexpr Score BishopPawns        = S(  3,  7);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
@@ -316,8 +316,8 @@ namespace {
                 // bishop, bigger when the center files are blocked with pawns.
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
-                score -= BishopPawns * (pos.pawns_on_same_color_squares(Us, s) + popcount(pos.pieces(Us,PAWN) & b))
-                                     * (1 + popcount(blocked & CenterFiles)) / 5;
+                score -= BishopPawns * (2 * pos.pawns_on_same_color_squares(Us, s) + popcount(pos.pieces(Us,PAWN) & b))
+                                     * (1 + popcount(blocked & CenterFiles)) / 2;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
