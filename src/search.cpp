5c1bc884f4e27d1f3a5d64a3058e92032db00b2c
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-28 19:38:19 +0100

    Half effect: Change dynamic contempt in the same direction then static contempt. For defaut contempt C=24 this is equivalent to master. So test it for C=0 against master with C=0. Bench: 5115841

--- src/search.cpp
+++ src/search.cpp
@@ -418,7 +418,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + (35 + ct) * previousScore / (abs(previousScore) + 176);
+              int dct = ct + (61 + ct / 2) * previousScore / (abs(previousScore) + 176);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
