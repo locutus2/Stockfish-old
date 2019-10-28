d41cc1bf03d5708bbf538ed13cf986dd5f6a2d3d
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-28 15:41:02 +0100

    Count own pawns attacked by the bishop as doubled for BishopPawns penalty. For compensation scale with 4/5. Bench: 4536560

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -127,7 +127,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties
-  constexpr Score BishopPawns        = S(  3,  7);
+  constexpr Score BishopPawns        = S( 12, 28);
   constexpr Score CorneredBishop     = S( 50, 50);
   constexpr Score FlankAttacks       = S(  8,  0);
   constexpr Score Hanging            = S( 69, 36);
@@ -317,7 +317,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * (pos.pawns_on_same_color_squares(Us, s) + popcount(pos.pieces(Us,PAWN) & b))
-                                     * (1 + popcount(blocked & CenterFiles));
+                                     * (1 + popcount(blocked & CenterFiles)) / 5;
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
