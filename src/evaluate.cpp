a3d3210b8e42053bcbc3c89ff98e1db88776f706
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-10-28 15:15:24 +0100

    Give BishopPawns penalty only if more than one pawn is on the same color as the bishop. Bench: 4497404

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,10 +314,13 @@ namespace {
             {
                 // Penalty according to number of pawns on the same color square as the
                 // bishop, bigger when the center files are blocked with pawns.
-                Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+                int bishopPawns = pos.pawns_on_same_color_squares(Us, s);
 
-                score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (1 + popcount(blocked & CenterFiles));
+                if (bishopPawns > 1)
+                {
+                    Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
+                    score -= BishopPawns * bishopPawns * (1 + popcount(blocked & CenterFiles));
+                }
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & Center))
