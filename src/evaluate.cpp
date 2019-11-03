c77bab32ddb8e921f8aa02749e221337542613f6
Stefan Geschwentner <locutus2@users.noreply.github.com>
2019-11-03 21:55:49 +0100

    Consider only strong side passed pawns. Double weight and compensate. Bench: 4349080

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -718,7 +718,7 @@ namespace {
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 95 ;
+                    -105 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
