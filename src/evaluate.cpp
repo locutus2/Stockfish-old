e78d24174d196b43b23779663cfcf618c7b6d984
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-04 17:01:24 +0100

    Use weighted difference of strong and weak side passed pawns instead of total passed pawn count (with compensation). Bench: 4538083

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -711,16 +711,15 @@ namespace {
 
     Color strongSide = eg > 0 ? WHITE : BLACK;
 
-    int passedPawns = pe->passed_count(strongSide) - pe->passed_count(~strongSide);
-
     // Compute the initiative bonus for the attacking side
-    int complexity =  12 * passedPawns
+    int complexity =  13 * pe->passed_count( strongSide)
+                    -  6 * pe->passed_count(~strongSide)
                     + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 51 * !pos.non_pawn_material()
                     - 43 * almostUnwinnable
-                    - 89 ;
+                    - 92 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting the
     // sign of the midgame or endgame values, and that we carefully cap the bonus
