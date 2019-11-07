115bea7d77f57c9b6f0c794fe08e28173d3113d7
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-07 19:27:01 +0100

    From the king danger terms popcount(kingRing[Us] & weak) and kingAttacksCount[Them] have the highest correlation (r=0.69). So try to simplify by replacing first term with a linear fit of second term. Bench: 4481825

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -442,17 +442,16 @@ namespace {
     int kingFlankAttacks = popcount(b1) + popcount(b2);
 
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
-                 + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
-                 +  69 * kingAttacksCount[Them]
+                 + 189 * kingAttacksCount[Them]
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -  35 * bool(attackedBy[Us][BISHOP] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
-                 -   7;
+                 +  66;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
