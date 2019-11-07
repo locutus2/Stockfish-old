4896543812e68a1a9da946972355b1148fc1a6a6
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-07 21:00:03 +0100

    To reduce the correlation of the both terms now add multiplicative term (quarter effect). Bench: 4587334

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -445,7 +445,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
-                 +       kingAttacksCount[Them] * (69 - 8 * popcount(kingRing[Us] & weak))
+                 +       kingAttacksCount[Them] * (69 - 4 * popcount(kingRing[Us] & weak))
                  +   3 * kingFlankAttacks * kingFlankAttacks / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
