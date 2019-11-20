6b60d88ccbb34c999d7a51feed545e9e728761bd
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-20 09:35:36 +0100

    Do only one ply less reduction if singularLMR if move was already extended. Bench: 5263035

--- src/search.cpp
+++ src/search.cpp
@@ -1101,12 +1101,12 @@ moves_loop: // When in check, search starts from here
               r -= 2;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
-          if (!extension && (ss-1)->moveCount > 15)
+          if ((ss-1)->moveCount > 15)
               r--;
 
           // Decrease reduction if ttMove has been singularly extended
           if (singularLMR)
-              r -= 2;
+              r -= 2 - extension;
 
           if (!captureOrPromotion)
           {
