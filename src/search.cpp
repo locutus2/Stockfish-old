2b28135dffd541768c5da9c996b0f56671fcefa0
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-20 20:56:32 +0100

    Give for second stat score rule more reduction if ttCapture. Bench: 4727121

--- src/search.cpp
+++ src/search.cpp
@@ -1112,7 +1112,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
-                  r += 2 - cutNode;
+                  r++;
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
@@ -1143,7 +1143,7 @@ moves_loop: // When in check, search starts from here
                   r--;
 
               else if ((ss-1)->statScore >= -117 && ss->statScore < -144)
-                  r++;
+                  r += 1 + ttCapture;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
               r -= ss->statScore / 16384;
