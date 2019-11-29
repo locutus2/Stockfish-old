e62a092e6ea66214d0e1f8b8d0356112408040e5
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-29 12:56:51 +0100

    Change OCB scaling with difference of passed pawns (consider only passed pawns for which the promotion square is not on the color of opponent bishop): 18 + 4 * pp_diff. Bench: 5742031

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,7 +747,12 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 22 ;
+        {
+            Bitboard undefendedPromotions = pos.pieces(strongSide, BISHOP) & DarkSquares ? FileBBB | FileDBB | FileFBB | FileHBB
+                                                                                         : FileABB | FileCBB | FileEBB | FileGBB;
+            sf = 18 + 4 * (  popcount(pe->passed_pawns( strongSide) & undefendedPromotions)
+                           - popcount(pe->passed_pawns(~strongSide) & undefendedPromotions));
+        }
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
