ceb34d78c9219c280b9bcd54b8e189d6c43677b0
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-11-29 13:03:23 +0100

    Change OCB scaling with difference of passed pawns: 14 + 8 * pp_diff (double effect). Bench: 5128616

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,7 +747,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 18 + 4 * (pe->passed_count(strongSide) - pe->passed_count(~strongSide));
+            sf = 14 + 8 * (pe->passed_count(strongSide) - pe->passed_count(~strongSide));
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
