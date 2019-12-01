89cf30386e10474abaf2fef999b8cba8dd76b237
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-12-01 18:26:59 +0100

    less ocb scaling if strong side has no passed pawns (pp): 22 - 16 * !pp. Bench: 5086177

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -747,7 +747,7 @@ namespace {
     {
         if (   pos.opposite_bishops()
             && pos.non_pawn_material() == 2 * BishopValueMg)
-            sf = 30 - 8 * !(pe->passed_pawns(strongSide));
+            sf = 22 - 16 * !pe->passed_pawns(strongSide);
         else
             sf = std::min(sf, 36 + (pos.opposite_bishops() ? 2 : 7) * pos.count<PAWN>(strongSide));
 
