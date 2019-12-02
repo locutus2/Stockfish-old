c00b981f985846e6e60e739d1c515f05c8644aa6
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-12-02 18:36:39 +0100

    Do a second lazy check which subtract king safety of better side from lazy threshold. Bench: 5453881

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -786,10 +786,13 @@ namespace {
 
     // Early exit if score is high
     Value v = (mg_value(score) + eg_value(score)) / 2;
-    Score safety = v > 0 ? pe->king_safety<WHITE>(pos)
-                         : pe->king_safety<BLACK>(pos);
-    if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64 - mg_value(safety))
-       return pos.side_to_move() == WHITE ? v : -v;
+    if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64)
+    {
+       Score safety = v > 0 ? pe->king_safety<WHITE>(pos)
+                            : pe->king_safety<BLACK>(pos);
+       if (abs(v) > LazyThreshold + pos.non_pawn_material() / 64 - mg_value(safety))
+           return pos.side_to_move() == WHITE ? v : -v;
+    }
 
     // Main evaluation begins here
 
