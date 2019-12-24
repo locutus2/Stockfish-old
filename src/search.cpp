883350366a3d903f4e4ec7fa83d6e0ab48463cb1
Stefan Geschwentner <stgeschwentner@gmail.com>
2019-12-24 20:21:22 +0100

    Less reduction for quiet moves at cut nodes if static eval > alpha. Bench: 4925778

--- src/search.cpp
+++ src/search.cpp
@@ -1136,7 +1136,7 @@ moves_loop: // When in check, search starts from here
 
               // Increase reduction for cut nodes (~5 Elo)
               if (cutNode)
-                  r += 2 + (ss->staticEval + 200 <= alpha);
+                  r += 2 - (ss->staticEval > alpha);
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
