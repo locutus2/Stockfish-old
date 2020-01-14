a22b6c4cac80f239f6357ad7a6670ae13725d806
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-14 08:43:05 +0100

    Less reduction for counter move if depth < 6. Bench: 4637593

--- src/search.cpp
+++ src/search.cpp
@@ -1147,6 +1147,10 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
+              // Decrease reduction for counter move at low depth
+              if (move == countermove && depth < 6)
+                  r--;
+
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
                   r++;
