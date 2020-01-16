d113a527156faff1958b992b109b7496a2a37dae
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-16 20:56:48 +0100

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1132,7 +1132,8 @@ moves_loop: // When in check, search starts from here
           //C = givesCheck;
           //C = inCheck;
           //C = !captureOrPromotion && move == ss->killers[0];
-          C = !captureOrPromotion && move == countermove;
+          //C = !captureOrPromotion && move == countermove;
+          C = cutNode;
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 500 * ttHitAverageResolution * ttHitAverageWindow / 1024)
               r--;
