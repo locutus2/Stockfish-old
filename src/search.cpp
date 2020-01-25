e7f20d479c9d712f0ad915a2afb18d8cc0156325
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-26 00:27:02 +0100

    Update for extended failed captures history twice. Bench: 4794550

--- src/search.cpp
+++ src/search.cpp
@@ -1297,14 +1297,15 @@ moves_loop: // When in check, search starts from here
 
       if (move != bestMove)
       {
-          for (int i = 0; i <= extension; ++i)
+          if (captureOrPromotion && captureCount < 32)
           {
-              if (captureOrPromotion && captureCount < 32)
+              capturesSearched[captureCount++] = move;
+              if (extension && captureCount < 32)
                   capturesSearched[captureCount++] = move;
-
-              else if (!captureOrPromotion && quietCount < 64)
-                  quietsSearched[quietCount++] = move;
           }
+
+          else if (!captureOrPromotion && quietCount < 64)
+              quietsSearched[quietCount++] = move;
       }
     }
 
