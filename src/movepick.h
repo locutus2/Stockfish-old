37b85c14196f5f691b5838396341d3b0726a7b41
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-26 19:37:17 +0100

    Smooth histories: period=2. Bench: 5255170

--- src/movepick.h
+++ src/movepick.h
@@ -33,9 +33,11 @@
 /// be a move or even a nested history. We use a class instead of naked value
 /// to directly call history update operator<<() on the entry so to use stats
 /// tables at caller sites as simple multi-dim arrays.
-template<typename T, int D>
-class StatsEntry {
 
+template<typename T, int D>
+class StatsEntryBase {
+
+protected:
   T entry;
 
 public:
@@ -54,54 +56,82 @@ public:
   }
 };
 
+template<typename T, int D, int Smoothing>
+class StatsEntry : public StatsEntryBase<T,D> {
+
+protected:
+  T average;
+
+public:
+  void operator=(const T& v) { StatsEntryBase<T,D>::operator=(v); average = v; }
+  T* operator&() { return &average; }
+  T* operator->() { return &average; }
+  operator const T&() const { return average; }
+
+  void operator<<(int bonus) {
+
+    StatsEntryBase<T,D>::operator<<(bonus);
+
+    average = ((Smoothing - 1) * average + StatsEntryBase<T,D>::entry) / Smoothing;
+  }
+};
+
+template<typename T, int D>
+class StatsEntry<T,D,0> : public StatsEntryBase<T,D> {
+
+public:
+  void operator=(const T& v) { StatsEntryBase<T,D>::operator=(v); }
+};
+
 /// Stats is a generic N-dimensional array used to store various statistics.
 /// The first template parameter T is the base type of the array, the second
 /// template parameter D limits the range of updates in [-D, D] when we update
 /// values with the << operator, while the last parameters (Size and Sizes)
 /// encode the dimensions of the array.
-template <typename T, int D, int Size, int... Sizes>
-struct Stats : public std::array<Stats<T, D, Sizes...>, Size>
+template <typename T, int D, int Smoothing, int Size, int... Sizes>
+struct Stats : public std::array<Stats<T, D, Smoothing, Sizes...>, Size>
 {
-  typedef Stats<T, D, Size, Sizes...> stats;
+  typedef Stats<T, D, Smoothing, Size, Sizes...> stats;
 
   void fill(const T& v) {
 
     // For standard-layout 'this' points to first struct member
     assert(std::is_standard_layout<stats>::value);
 
-    typedef StatsEntry<T, D> entry;
+    typedef StatsEntry<T, D, Smoothing> entry;
     entry* p = reinterpret_cast<entry*>(this);
     std::fill(p, p + sizeof(*this) / sizeof(entry), v);
   }
 };
 
-template <typename T, int D, int Size>
-struct Stats<T, D, Size> : public std::array<StatsEntry<T, D>, Size> {};
+template <typename T, int D, int Smoothing, int Size>
+struct Stats<T, D, Smoothing, Size> : public std::array<StatsEntry<T, D, Smoothing>, Size> {};
 
 /// In stats table, D=0 means that the template parameter is not used
 enum StatsParams { NOT_USED = 0 };
-enum StatsType { NoCaptures, Captures };
+enum StatsType   { NoCaptures, Captures };
+enum StatsSmooth { NO_SMOOTHING = 0, DEFAULT_SMOOTHING_PERIOD = 2 };
 
 /// ButterflyHistory records how often quiet moves have been successful or
 /// unsuccessful during the current search, and is used for reduction and move
 /// ordering decisions. It uses 2 tables (one for each color) indexed by
 /// the move's from and to squares, see www.chessprogramming.org/Butterfly_Boards
-typedef Stats<int16_t, 10692, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)> ButterflyHistory;
+typedef Stats<int16_t, 10692, DEFAULT_SMOOTHING_PERIOD, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)> ButterflyHistory;
 
 /// CounterMoveHistory stores counter moves indexed by [piece][to] of the previous
 /// move, see www.chessprogramming.org/Countermove_Heuristic
-typedef Stats<Move, NOT_USED, PIECE_NB, SQUARE_NB> CounterMoveHistory;
+typedef Stats<Move, NOT_USED, NO_SMOOTHING, PIECE_NB, SQUARE_NB> CounterMoveHistory;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-typedef Stats<int16_t, 10692, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
+typedef Stats<int16_t, 10692, DEFAULT_SMOOTHING_PERIOD, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
-typedef Stats<int16_t, 29952, PIECE_NB, SQUARE_NB> PieceToHistory;
+typedef Stats<int16_t, 29952, DEFAULT_SMOOTHING_PERIOD, PIECE_NB, SQUARE_NB> PieceToHistory;
 
 /// ContinuationHistory is the combined history of a given pair of moves, usually
 /// the current one given a previous one. The nested history table is based on
 /// PieceToHistory instead of ButterflyBoards.
-typedef Stats<PieceToHistory, NOT_USED, PIECE_NB, SQUARE_NB> ContinuationHistory;
+typedef Stats<PieceToHistory, NOT_USED, DEFAULT_SMOOTHING_PERIOD, PIECE_NB, SQUARE_NB> ContinuationHistory;
 
 
 /// MovePicker class is used to pick one pseudo legal move at a time from the
