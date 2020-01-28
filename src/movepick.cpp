46bca7c1375c06438db3bc0028e6f8d83a2fc740
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-28 09:08:00 +0100

    Use different history weights for quiet move ordering. Bench: 4826391

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,11 +111,11 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
-          m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                   + 2 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   + 2 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
+          m.value =  16 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+                   + 14 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                   + 18 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
+                   + 19 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
+                   + 13 * (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
 
       else // Type == EVASIONS
       {
@@ -206,7 +206,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -30000 * depth);
       }
 
       ++stage;
