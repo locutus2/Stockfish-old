97099e5800604340b77799a1d4765ab965665fe9
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-01-28 16:02:05 +0100

    Use different history weights for quiet move ordering. Double change. Test against pb's passed updStats_ttPv patch (rebased to current master). Bench: 4976889

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -111,11 +111,11 @@ void MovePicker::score() {
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if (Type == QUIETS)
-          m.value =  19 * (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 11 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
-                   + 17 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
+          m.value =  22 * (*mainHistory)[pos.side_to_move()][from_to(m)]
+                   +  8 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                   + 16 * (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    + 18 * (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
-                   + 15 * (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
+                   + 16 * (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)];
 
       else // Type == EVASIONS
       {
