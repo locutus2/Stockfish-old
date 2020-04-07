3ca61c1576e2c3ea2a3e6b3972bf2db53616b563
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-07 19:04:12 +0200

    Do lmr stat update also for captures if not gives check. Bench: 4422760

--- src/search.cpp
+++ src/search.cpp
@@ -1243,7 +1243,7 @@ moves_loop: // When in check, search starts from here
 
                   update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
               }
-              else if (value <= alpha)
+              else if (!givesCheck)
                   captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] << bonus;
           }
       }
