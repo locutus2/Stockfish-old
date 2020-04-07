d91f45e1fc9de03aba0ceafbea3b0be98174aab4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-07 18:43:28 +0200

    Do lmr stat update also for captures if value > alpha. Bench: 4876661

--- src/search.cpp
+++ src/search.cpp
@@ -1236,16 +1236,15 @@ moves_loop: // When in check, search starts from here
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
-              if (captureOrPromotion)
-                  captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] << bonus / 2;
-
-              else
+              if (!captureOrPromotion)
               {
                   if (move == ss->killers[0])
                       bonus += bonus / 4;
 
                   update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
               }
+              else if (value > alpha)
+                  captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] << bonus;
           }
       }
 
