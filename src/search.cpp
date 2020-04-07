d93385b8edbee5b81f8afab6d7dbe03abb430a77
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-07 22:52:39 +0200

    More reduction for captures if capture history < -7000. Bench: 4661788

--- src/search.cpp
+++ src/search.cpp
@@ -1201,14 +1201,17 @@ moves_loop: // When in check, search starts from here
           }
           else
           {
-            // Increase reduction for captures/promotions if low depth and late move or bad history
-            if (    depth < 8
-                && (moveCount > 2 || captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 0))
+            // Increase reduction for captures/promotions if late move and at low depth
+            if (depth < 8 && moveCount > 2)
                 r++;
 
             // Unless giving check, this capture is likely bad
             if (   !givesCheck
                 && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 200 * depth <= alpha)
+                r++;
+
+            // Increase reduction for captures/promotions if capture history is bad
+            if (captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < -7000)
                 r++;
           }
 
