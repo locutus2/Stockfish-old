447fa2dd6c26367c94e96636ab9818eb02a72dce
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-08 13:13:24 +0200

    Increase passed pawn score (+50%) if opponent endgame mobility is very low. Bench: 4772113

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -641,6 +641,9 @@ namespace {
         score += bonus - PassedFile * edge_distance(file_of(s));
     }
 
+    if (eg_value(mobility[Them]) <= 0)
+        score += score / 2;
+
     if (T)
         Trace::add(PASSED, Us, score);
 
