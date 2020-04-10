915872d7a30e1a70d24097370758a55f3b862a79
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-10 13:07:11 +0200

    Use blocked attacks in king danger (double effect). Bench: 4787193

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -465,7 +465,7 @@ namespace {
                  + 185 * popcount(kingRing[Us] & weak)
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
-                 +  50 * popcount(blockedAttacks)
+                 + 100 * popcount(blockedAttacks)
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
