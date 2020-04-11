95cc9c3a676c595abfb28faf7cad60abdc566a94
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-11 21:23:22 +0200

    In almost unwinnable rook endgames increase weight by 16 for initiative (Thanks to Viz for this idea!). Bench: 4812987

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -712,12 +712,12 @@ namespace {
 
     // Compute the initiative bonus for the attacking side
     int complexity =   9 * pe->passed_count()
-                    +      pos.count<PAWN>() * (11 - 8 * rookEndgame)
+                    + 11 * pos.count<PAWN>()
                     +  9 * outflanking
                     + 21 * pawnsOnBothFlanks
                     + 24 * infiltration
                     + 51 * !pos.non_pawn_material()
-                    - 43 * almostUnwinnable
+                    -      almostUnwinnable * (43 + 16 * rookEndgame)
                     -110 ;
 
     Value mg = mg_value(score);
