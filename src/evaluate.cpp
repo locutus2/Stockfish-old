9b34ed31d3985224ff0e556844e9337824a39848
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-14 20:53:49 +0200

    Allow unsafe queen checks if its captures a queen. Bench: 4830182

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -414,6 +414,8 @@ namespace {
 
     if (queenChecks)
         kingDanger += QueenSafeCheck;
+    else
+        unsafeChecks |= (b1 | b2) & attackedBy[Them][QUEEN] & pos.pieces(Us, QUEEN);
 
     // Enemy bishops checks: we count them only if they are from squares from
     // which we can't give a queen check, because queen checks are more valuable.
