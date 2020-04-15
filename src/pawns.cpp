43546cc6e41a959c0b0019d56411ad840db4472c
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-15 12:24:25 +0200

    Count in connected bonus also pawns for which the square in front are attacked twice (if not phalanx) as blocked. Bench: 5046502

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -138,7 +138,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked || more_than_one(leverPush))) / 2
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - (blocked || (!phalanx && more_than_one(leverPush)))) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
