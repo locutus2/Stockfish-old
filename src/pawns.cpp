e2ac44478de97333356653ab9bd9cd772b52e813
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-16 17:46:07 +0200

    Try now the opposite and use a even lower bonus (3,3) for each pawn chain (multiple bonus for chains longer than 3) on the files B to G. Bench: 4820671

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -36,7 +36,7 @@ namespace {
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
-  constexpr Score PawnChain     = S(10, 10);
+  constexpr Score PawnChain     = S( 3,  3);
   constexpr Score WeakLever     = S( 0, 56);
   constexpr Score WeakUnopposed = S(13, 27);
 
