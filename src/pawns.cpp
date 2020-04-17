75196b46cbe07886c6876943dbdebfb2635a63ec
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-17 20:44:29 +0200

    Give connected bonus if unblocked support or phalanx. Bench: 4772627

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -139,8 +139,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (  4 + 2 * bool(phalanx) + (!phalanx && unblockedSupport)
-                                         - 2 * bool(opposed) - bool(blocked)) / 2
+            int v =  Connected[r] * (4 + 2 * bool(phalanx | unblockedSupport) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
