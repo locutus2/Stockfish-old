37c88f2b6377eb8c11f9c44920d7c09823decb17
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-21 19:31:51 +0200

    50% more eval history effect for rooks. Test against eval_history_master. Bench: 4721591

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -309,8 +309,8 @@ namespace {
         }
 
         if (nhist)
-            score += make_score(hist / (nhist * (Pt == QUEEN ? 512 : 1024)),
-                                hist / (nhist * (Pt == QUEEN ? 512 : 1024)));
+            score += make_score(hist / (nhist * (Pt == ROOK ? 683 : 1024)),
+                                hist / (nhist * (Pt == ROOK ? 683 : 1024)));
 
         if (Pt == BISHOP || Pt == KNIGHT)
         {
