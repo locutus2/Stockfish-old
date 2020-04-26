f1c9d3dbe2c25c846efe8c25cad1bf14b4262ada
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-26 10:00:15 +0200

    More connected bonus for passed pawns (double effect). Bench: 4757494

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -138,7 +138,7 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (4 + 2 * bool(phalanx) + passed - 2 * bool(opposed) - bool(blocked)) / 2
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) + 2 * passed - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
             score += make_score(v, v * (r - 2) / 4);
