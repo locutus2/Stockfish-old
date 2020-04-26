54773c77a964b76a781d8e6d9f2986933871e9be
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-26 12:35:54 +0200

    Increase connected endgame bonus factor: (r-2+passed)/(4+passed). Bench: 4468487

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -141,7 +141,7 @@ namespace {
             int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
-            score += make_score(v, v * (r - 2 + passed) / 4);
+            score += make_score(v, v * (r - 2 + passed) / (4 + passed));
         }
 
         else if (!neighbours)
