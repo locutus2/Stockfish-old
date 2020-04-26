f981c7a109d58c4a7f19a8716cefd6127debd159
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-26 12:12:23 +0200

    Tweak connected endgame bonus factor: (r-1)/5. Bench: 4655728

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -141,7 +141,7 @@ namespace {
             int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
-            score += make_score(v, v * (r - 2) / 4);
+            score += make_score(v, v * (r - 1) / 5);
         }
 
         else if (!neighbours)
