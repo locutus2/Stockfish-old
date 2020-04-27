a4e3c00d69ea3b7c3745d4d591d5364efbe34317
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-27 22:50:54 +0200

    Tweak some pawns values. Bench: 4679797

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,15 +32,15 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
-  constexpr Score Backward      = S( 9, 24);
+  constexpr Score Backward      = S(12, 18);
   constexpr Score BlockedStorm  = S(82, 82);
-  constexpr Score Doubled       = S(11, 56);
-  constexpr Score Isolated      = S( 5, 15);
-  constexpr Score WeakLever     = S( 0, 56);
-  constexpr Score WeakUnopposed = S(13, 27);
+  constexpr Score Doubled       = S(18, 60);
+  constexpr Score Isolated      = S( 6, 18);
+  constexpr Score WeakLever     = S( 2, 56);
+  constexpr Score WeakUnopposed = S(17, 25);
 
   // Connected pawn bonus
-  constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
+  constexpr int Connected[RANK_NB] = { 0, 2, 3, 12, 30, 48, 86 };
 
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
