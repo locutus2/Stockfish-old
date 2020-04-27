93e0c835a3cb3a24be41d68309870b71045b81e8
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-27 13:04:58 +0200

    Use constant factor 3/4 for endgame connected bonus. Bench: 4771902

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -138,10 +138,10 @@ namespace {
         // Score this pawn
         if (support | phalanx)
         {
-            int v =  Connected[r] * (4 + 2 * bool(phalanx) + (passed && !blocked) - 2 * bool(opposed) - bool(blocked)) / 2
+            int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
-            score += make_score(v, v * (r - 2) / 4);
+            score += make_score(v, v * 3 / 4);
         }
 
         else if (!neighbours)
