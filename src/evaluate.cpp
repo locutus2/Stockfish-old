25348bc128ab490b3aeeec022136e21c1da042eb
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-04-30 22:21:20 +0200

    Try 25% of tuned knight mobility tweaks with compensation. Bench: 4931686

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -88,7 +88,7 @@ namespace {
 
 #define S(mg, eg) make_score(mg, eg)
 
-  constexpr int AN = 50, B0 = 111, B1 = 25;
+  constexpr int AN = 25, B0 = 111, B1 = 25;
 
   #define WN(o, n) ((o) + ((n) - (o)) * AN / 100)
 
