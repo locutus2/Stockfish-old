2b9641eecaf421e97d26dfa981b70d70386f0ed4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-07 22:06:48 +0200

    Less king danger if castle is possible (double effect) with half compensation. Bench: 4851682

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -462,8 +462,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 -  80 * bool(pos.castling_rights(Us))
-                 +  77;
+                 -  40 * bool(pos.castling_rights(Us))
+                 +  57;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
