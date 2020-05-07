ac04e219774152647115bdc361dfe903a9b51261
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-07 21:54:18 +0200

    Less king danger if castle is possible (quadruple effect) with half compensation. Bench: 4769141

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -462,8 +462,8 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 - 160 * bool(pos.castling_rights(Us))
-                 +  37;
+                 -  80 * bool(pos.castling_rights(Us))
+                 +  77;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
