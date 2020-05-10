dd709ff16a75796821d4621bdff7748ad23f0652
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-10 10:11:10 +0200

    Rank dependant midgame connected bonus factor: r/5. Bench: 4615336

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -140,7 +140,7 @@ namespace {
             int v =  Connected[r] * (4 + 2 * bool(phalanx) - 2 * bool(opposed) - bool(blocked)) / 2
                    + 21 * popcount(support);
 
-            score += make_score(v * r / 7, v * (r - 2) / 4);
+            score += make_score(v * r / 5, v * (r - 2) / 4);
         }
 
         else if (!neighbours)
