848914a1278050a4a15bfeb2695145df3c902bde
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-11 23:17:44 +0200

    Don't do 1/32 of history prunings. Bench: 4766391

--- src/search.cpp
+++ src/search.cpp
@@ -1025,7 +1025,8 @@ moves_loop: // When in check, search starts from here
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                  && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
+                  && (thisThread->nodes & 31))
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
