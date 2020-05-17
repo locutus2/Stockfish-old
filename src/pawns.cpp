1bade608aab8a4c6735541567ee3dcdb46af5e08
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-17 18:38:17 +0200

    Detect h4 in this position 8/1k6/6p1/8/7P/6P1/1K6/8 w - - 0 1 as candiate passer (see forum https://groups.google.com/forum/?fromgroups=#!topic/fishcooking/JUy66g42QOQ). Bench: 4794323

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -82,6 +82,7 @@ namespace {
     Bitboard theirPawns = pos.pieces(Them, PAWN);
 
     Bitboard doubleAttackThem = pawn_double_attacks_bb<Them>(theirPawns);
+	Bitboard attacksThem = pawn_attacks_bb<Them>(theirPawns);
 
     e->passedPawns[Us] = 0;
     e->kingSquares[Us] = SQ_NONE;
@@ -123,7 +124,7 @@ namespace {
         //     (Refined in Evaluation::passed)
         passed =   !(stoppers ^ lever)
                 || (   !(stoppers ^ leverPush)
-                    && popcount(phalanx) >= popcount(leverPush))
+                    && popcount(phalanx | (shift<Up>(support) & ~(theirPawns | attacksThem))) >= popcount(leverPush))
                 || (   stoppers == blocked && r >= RANK_5
                     && (shift<Up>(support) & ~(theirPawns | doubleAttackThem)));
 
