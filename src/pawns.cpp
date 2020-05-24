bc485e5f0352609b44db38821536d01a53b4e527
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-24 23:02:11 +0200

    New tuned values for weak unopposed pawns, Bench: 4576727

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -31,13 +31,16 @@ namespace {
   #define V Value
   #define S(mg, eg) make_score(mg, eg)
 
+  constexpr int A = 100;
+  #define W(o,n) ((o) + ((n) - (o)) * A / 100)
+
   // Pawn penalties
   constexpr Score Backward      = S( 9, 24);
   constexpr Score BlockedStorm  = S(82, 82);
   constexpr Score Doubled       = S(11, 56);
   constexpr Score Isolated      = S( 5, 15);
   constexpr Score WeakLever     = S( 0, 56);
-  constexpr Score WeakUnopposed = S(13, 27);
+  constexpr Score WeakUnopposed = S(W(13,0), W(27,18));
 
   // Connected pawn bonus
   constexpr int Connected[RANK_NB] = { 0, 7, 8, 12, 29, 48, 86 };
