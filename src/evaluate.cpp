7b2cc3ee58fcbe2b19ac596a10a664b027edb0c2
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-27 00:35:18 +0200

    Now set unopposed bishop bonus to center average around old boshop pair bonus. Bench: 4585521

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -150,7 +150,7 @@ namespace {
   constexpr Score ThreatByPawnPush    = S( 48, 39);
   constexpr Score ThreatBySafePawn    = S(173, 94);
   constexpr Score TrappedRook         = S( 55, 13);
-  constexpr Score UnopposedBishop     = S( 33, 13);
+  constexpr Score UnopposedBishop     = S( 60, 60);
   constexpr Score WeakQueen           = S( 51, 14);
   constexpr Score WeakQueenProtection = S( 15,  0);
 
