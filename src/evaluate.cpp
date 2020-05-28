3973a6fb162c1b1749f142c56f5ac03065b7cc4e
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-28 19:31:18 +0200

    More king danger if pinned piece is undefended (+50% effect). Bench: 4633933

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -465,7 +465,7 @@ namespace {
                  + 148 * popcount(unsafeChecks)
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
-                 + 100 * undefendedPin
+                 + 150 * undefendedPin
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
