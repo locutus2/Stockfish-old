57bcf6c739c227b555af53aa6acb531cb0568da3
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-28 20:49:06 +0200

    Use endgame instead of midgame mobility in king danger. Bench: 4715822

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -460,7 +460,7 @@ namespace {
                  +  98 * popcount(pos.blockers_for_king(Us))
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
-                 +       mg_value(mobility[Them] - mobility[Us])
+                 +       eg_value(mobility[Them] - mobility[Us])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
