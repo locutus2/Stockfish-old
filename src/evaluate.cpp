eb3ae5f034a1c611d205dc2f28e4cee549ef0eb8
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-05-30 12:31:07 +0200

    Experiment: big decrease (-74) of king danger. Bench: 4482266

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -465,7 +465,7 @@ namespace {
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
                  -   4 * kingFlankDefense
-                 +  37;
+                 -  37;
 
     // Transform the kingDanger units into a Score, and subtract it from the evaluation
     if (kingDanger > 100)
