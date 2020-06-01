d41b7f4dfb6d9277a446452ac80338d271c70bfa
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-01 19:20:14 +0200

    Ignore own pawns which are not blocked. Bench: 4805677

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -293,7 +293,7 @@ namespace {
         else if (Pt == ROOK && (file_bb(s) & kingRing[Them]))
             score += RookOnKingRing;
 
-        else if (Pt == BISHOP && (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) & kingRing[Them]))
+        else if (Pt == BISHOP && (attacks_bb<BISHOP>(s, pos.pieces(Them, PAWN) | (pos.pieces(Us, PAWN) & shift<Down>(pos.pieces()))) & kingRing[Them]))
             score += BishopOnKingRing;
 
         int mob = popcount(b & mobilityArea[Us]);
