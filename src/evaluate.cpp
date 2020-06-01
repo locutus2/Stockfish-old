654685208a3906e17a17e106eb00a891b03e46ef
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-01 11:13:23 +0200

    Increase bonus for bishops aligned to the king ring. for mid- and end game: (32,5). Test against my passed bishopsOnKR patch. Bench: 5192651

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -128,7 +128,7 @@ namespace {
 
   // Assorted bonuses and penalties
   constexpr Score BishopPawns         = S(  3,  7);
-  constexpr Score BishopOnKingRing    = S( 48,  0);
+  constexpr Score BishopOnKingRing    = S( 32,  5);
   constexpr Score BishopXRayPawns     = S(  4,  5);
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
