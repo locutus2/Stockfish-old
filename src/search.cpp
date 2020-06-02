89bf6df060c221fad861a2441a06dab1c8c4a1cd
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-02 08:55:36 +0200

    No LMR for pawn moves at root. Bench: 4557013

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
-          && (!rootNode || thisThread->best_move_count(move) == 0)
+          && (!rootNode || (type_of(movedPiece) != PAWN && thisThread->best_move_count(move) == 0))
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
