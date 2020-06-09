44fa257d166cc67a535ef84a03ee6b02c46cfd56
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-09 08:26:16 +0200

    Less cut node reduction if tt predicts a fail high. Bench: 5067977

--- src/search.cpp
+++ src/search.cpp
@@ -1213,9 +1213,9 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
-              // Increase reduction for cut node or tt predicted fail high
-              if (cutNode || ttFailHigh)
-                  r += 2;
+              // Increase reduction for cut nodes (~10 Elo)
+              if (cutNode)
+                  r += 2 - ttFailHigh;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
