8386c5a357a24b50aefa40600f09d14e1b121f12
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-11 22:45:28 +0200

    The comment in the code says the penalty get smaller when bishop is outside the pawn chain but implementation does the opposite! So implement it now like it described. Bench: 4595253

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -324,7 +324,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
+                                     * (bool(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles));
 
                 // Penalty for all enemy pawns x-rayed
                 score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s) & pos.pieces(Them, PAWN));
