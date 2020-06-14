146976de36af59c81ea43276f4e1c24a7e75b1d4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-14 23:40:27 +0200

    No kill update if in check and already a first killer exists. Bench: 4278529

--- src/search.cpp
+++ src/search.cpp
@@ -1704,7 +1704,7 @@ moves_loop: // When in check, search starts from here
 
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, int depth) {
 
-    if (ss->killers[0] != move)
+    if (ss->killers[0] != move && (!ss->inCheck || !ss->killers[0]))
     {
         ss->killers[1] = ss->killers[0];
         ss->killers[0] = move;
