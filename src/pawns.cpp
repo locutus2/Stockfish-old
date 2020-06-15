7bfebd28f8d8f782cbb1b745cac153188b01c2a2
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-15 20:16:02 +0200

    Don't use pawns which attacked by opponent pawns for blocked count. Bench: 4532344

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -87,7 +87,7 @@ namespace {
     e->passedPawns[Us] = 0;
     e->kingSquares[Us] = SQ_NONE;
     e->pawnAttacks[Us] = e->pawnAttacksSpan[Us] = pawn_attacks_bb<Us>(ourPawns);
-    e->blockedCount += popcount(shift<Up>(ourPawns) & (theirPawns | doubleAttackThem));
+    e->blockedCount += popcount(shift<Up>(ourPawns & ~pawn_attacks_bb<Them>(theirPawns)) & (theirPawns | doubleAttackThem));
 
     // Loop through all pawns of the current color and score each pawn
     while ((s = *pl++) != SQ_NONE)
