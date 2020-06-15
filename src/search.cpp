dad094fe1d60ed3565efe421e9589feee4a45b21
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-15 13:05:48 +0200

    Index counter moves table also by prior capture state. Bench: 4648573

--- src/search.cpp
+++ src/search.cpp
@@ -929,7 +929,7 @@ moves_loop: // When in check, search starts from here
                                           nullptr                   , (ss-4)->continuationHistory,
                                           nullptr                   , (ss-6)->continuationHistory };
 
-    Move countermove = thisThread->counterMoves[pos.piece_on(prevSq)][prevSq];
+    Move countermove = thisThread->counterMoves[priorCapture][pos.piece_on(prevSq)][prevSq];
 
     MovePicker mp(pos, ttMove, depth, &thisThread->mainHistory,
                                       &thisThread->lowPlyHistory,
@@ -1721,7 +1721,8 @@ moves_loop: // When in check, search starts from here
     if (is_ok((ss-1)->currentMove))
     {
         Square prevSq = to_sq((ss-1)->currentMove);
-        thisThread->counterMoves[pos.piece_on(prevSq)][prevSq] = move;
+        bool priorCapture = pos.captured_piece();
+        thisThread->counterMoves[priorCapture][pos.piece_on(prevSq)][prevSq] = move;
     }
 
     if (depth > 11 && ss->ply < MAX_LPH)
