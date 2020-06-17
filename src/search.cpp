d95349bc487d4cb2ab76ae556a682a0ff4b918b8
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-17 22:39:02 +0200

    Tweak aspiration window after evasl quantization to hit multiples of 16 as boundaries. First delta=16, try opposie and decrease additive constant to 4. Bench: 4926403

--- src/search.cpp
+++ src/search.cpp
@@ -467,7 +467,7 @@ void Thread::search() {
                   break;
               }
 
-              delta += delta / 4 + 6;
+              delta += delta / 4 + 4;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
