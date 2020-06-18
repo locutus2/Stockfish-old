e4262e4bce639d9f973a1b0f73e3db706c4173fc
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-18 21:42:03 +0200

    Simpler version: Additional bonus (10,3) for supported pawns. Bench: 4962750

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -163,7 +163,7 @@ namespace {
                      + WeakLever * more_than_one(lever);
 
         else
-            score -= make_score(10, 3);
+            score += make_score(10, 3);
     }
 
     return score;
