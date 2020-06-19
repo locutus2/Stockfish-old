0abbe22c83aeeebf280146d239c8fcfb58a39de1
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-19 15:54:46 +0200

    50% more effect: Additional penalty (19,3) for phalanx pawns for which the push square is defended by opponent pawns. Bench: 4245960

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -143,7 +143,7 @@ namespace {
             score += make_score(v, v * (r - 2) / 4);
 
             if (phalanx && leverPush)
-                score -= make_score(6, 1);
+                score -= make_score(19, 3);
         }
 
         else if (!neighbours)
