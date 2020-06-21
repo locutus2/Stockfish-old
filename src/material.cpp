e3cd7a3e6c61739f672b476995ca3be8bc276ce3
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-21 10:18:56 +0200

    Tweak queen vs 3 minors imbalance. Bench: 4357234

--- src/material.cpp
+++ src/material.cpp
@@ -46,10 +46,10 @@ namespace {
     // pair pawn knight bishop rook queen
     {   0                               }, // Bishop pair
     {  36,    0                         }, // Pawn
-    {   9,   63,   0                    }, // Knight      OUR PIECES
-    {  59,   65,  42,     0             }, // Bishop
-    {  46,   39,  24,   -24,    0       }, // Rook
-    {  97,  100, -42,   137,  268,    0 }  // Queen
+    {   9,   63,    0                   }, // Knight      OUR PIECES
+    {  59,   65,   42,    0             }, // Bishop
+    {  46,   39,   24,  -24,    0       }, // Rook
+    {  97,  100, -142,   37,  268,    0 }  // Queen
   };
 
   // Endgame evaluation and scaling functions are accessed directly and not through
