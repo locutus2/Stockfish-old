6a6c9552d99896851c45763c23afe2ab12ee240c
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-26 11:41:33 +0200

    Try slightly faster aging for best move count (weight=7). Bench: 4334258

--- src/search.cpp
+++ src/search.cpp
@@ -466,7 +466,7 @@ void Thread::search() {
               }
               else
               {
-                  rootMoves[pvIdx].bestMoveCount += 4;
+                  rootMoves[pvIdx].bestMoveCount += 7;
                   break;
               }
 
