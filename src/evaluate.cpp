d073a13f5c827451d5a56c1a57d8eb4a5f754bb0
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-28 13:50:42 +0200

    Lower scaling factor 20 for drawish rook endgames. Use as additional condition that weak side king is near his pawns. Bench: 4528486

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -788,7 +788,7 @@ namespace {
                 && pos.count<PAWN>(strongSide) - pos.count<PAWN>(~strongSide) <= 1
                 && bool(KingSide & pos.pieces(strongSide, PAWN)) != bool(QueenSide & pos.pieces(strongSide, PAWN))
                 && (attacks_bb<KING>(pos.square<KING>(~strongSide)) & pos.pieces(~strongSide, PAWN)))
-            sf = 24;
+            sf = 20;
         else
             sf = std::min(sf, 36 + 7 * pos.count<PAWN>(strongSide));
     }
