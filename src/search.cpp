a5cf8903d359034483f8e046fd34de2b8cf8d400
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-07-01 00:14:48 +0200

    Do check extension only for ply < depth. Bench: 4156868

--- src/search.cpp
+++ src/search.cpp
@@ -1091,7 +1091,8 @@ moves_loop: // When in check, search starts from here
 
       // Check extension (~2 Elo)
       else if (    givesCheck
-               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move)))
+               && (pos.is_discovery_check_on_king(~us, move) || pos.see_ge(move))
+               && ss->ply < depth)
           extension = 1;
 
       // Passed pawn extension
