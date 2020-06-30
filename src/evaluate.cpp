e055f8d853ef853787f4a909d8d8c8b19dc05828
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-06-30 08:43:32 +0200

    For opposite bishops double 50 move rule down scaling. Bench: 4573131

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -879,7 +879,8 @@ namespace {
     v = (pos.side_to_move() == WHITE ? v : -v) + Tempo;
 
     // Damp down the evaluation linearly when shuffling
-    v = v * (100 - pos.rule50_count()) / 100;
+    int rule50 = pos.rule50_count() * (1 + pos.opposite_bishops());
+    v = v * std::max(100 - rule50, 0) / 100;
 
     return v;
   }
