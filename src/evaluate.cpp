dd6e761c852027f6850253c776fba48fdf91658d
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-07-12 08:47:16 +0200

    Replace conditional_more_than_two with more_than_one and exclude D/E file from queen/King side. Bench: 4526728

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,8 +314,8 @@ namespace {
             if (   Pt == KNIGHT
                 && bb & s & ~CenterFiles
                 && !(b & pos.pieces(Them) & ~pos.pieces(PAWN))
-                && !conditional_more_than_two(
-                      pos.pieces(Them) & ~pos.pieces(PAWN) & (s & QueenSide ? QueenSide : KingSide)))
+                && !more_than_one(  pos.pieces(Them) & ~pos.pieces(PAWN)
+                                  & (s & QueenSide ? QueenSide ^ FileDBB : KingSide ^ FileEBB)))
                 score += BadOutpost;
             else if (bb & s)
                 score += Outpost[Pt == BISHOP];
