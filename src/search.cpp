e4a7e23622b9afe2533b2c04392f1f57083b2aa6
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-07-13 08:58:30 +0200

    Allow passed pawn extension also if not killer and opponent has atmost two rooks. Bench: 4777620

--- src/search.cpp
+++ src/search.cpp
@@ -1114,7 +1114,7 @@ moves_loop: // When in check, search starts from here
           extension = 1;
 
       // Passed pawn extension
-      else if (   (formerPv || move == ss->killers[0])
+      else if (   (move == ss->killers[0] || pos.non_pawn_material(~us) <= 2 * RookValueMg)
                && pos.advanced_pawn_push(move)
                && pos.pawn_passed(us, to_sq(move)))
           extension = 1;
