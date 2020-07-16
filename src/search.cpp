f6a48e2cfd499efc9d602e5dde892139a0a68c69
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-07-16 20:59:03 +0200

    Update stats of first failed quiet move directly but use only half stat bonus. Bench: 3336359

--- src/search.cpp
+++ src/search.cpp
@@ -1353,7 +1353,7 @@ moves_loop: // When in check, search starts from here
           else if (!captureOrPromotion && quietCount < 64)
           {
               if (quietCount == 0)
-                  update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), -stat_bonus(depth));
+                  update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), -stat_bonus(depth) / 2);
 
               quietsSearched[quietCount++] = move;
           }
