c189f4cc06431c248cce906fa475d149a8c77c1c
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-07-28 16:01:53 +0200

    Less reduction at non-check cut nodes for second move. Bench: 3967910

--- src/search.cpp
+++ src/search.cpp
@@ -1173,8 +1173,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          // Decrease reduction at non-check cut nodes for second move at low depths
-          if (cutNode && depth <= 8 && moveCount <= 2 && !ss->inCheck)
+          // Decrease reduction at non-check cut nodes for second move
+          if (cutNode && moveCount <= 2 && !ss->inCheck)
               r--;
 
           // Decrease reduction if the ttHit running average is large
