06fd486beb242ee690d1f700267e42a7b9e794f7
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-02 17:45:13 +0200

    50% more WeakQueenProtection bonus if really only the queen defends a weak piece and no bonus otherwise. Bench: 4633458

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -157,7 +157,7 @@ namespace {
   constexpr Score ThreatByPawnPush    = S( 48, 39);
   constexpr Score ThreatBySafePawn    = S(173, 94);
   constexpr Score TrappedRook         = S( 55, 13);
-  constexpr Score WeakQueenProtection = S(  7,  0);
+  constexpr Score WeakQueenProtection = S( 21,  0);
   constexpr Score WeakQueen           = S( 56, 15);
 
 
@@ -548,8 +548,7 @@ namespace {
         score += Hanging * popcount(weak & b);
 
         // Additional bonus if weak piece is only protected by a queen
-        score += WeakQueenProtection * ( 2 * popcount(weak & attackedBy[Them][QUEEN])
-                                        +    popcount(weak & attackedBy[Them][QUEEN] & ~attackedBy2[Them]));
+        score += WeakQueenProtection * popcount(weak & attackedBy[Them][QUEEN] & ~attackedBy2[Them]);
     }
 
     // Bonus for restricting their piece moves
