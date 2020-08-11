091f33acef8f0b669c497368d92120fa096f91aa
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-11 14:56:41 +0200

    Use for each eight thread classic eval. Bench: 4244812

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -941,7 +941,7 @@ make_v:
 
 Value Eval::evaluate(const Position& pos) {
 
-  if (Eval::useNNUE && pos.this_thread()->id() % 2 == 0)
+  if (Eval::useNNUE && pos.this_thread()->id() % 8 == 0)
   {
       Value v = eg_value(pos.psq_score());
       // Take NNUE eval only on balanced positions
