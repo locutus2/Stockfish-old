21ce8fc817abda796dc5ce1e2ce4cc88816e3b47
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-13 23:45:55 +0200

    More futility pruning if king move. Bench: 3974341

--- src/search.cpp
+++ src/search.cpp
@@ -1029,15 +1029,13 @@ moves_loop: // When in check, search starts from here
               && !givesCheck)
           {
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 4 + (   (ss-1)->statScore > 0
-                                     || (ss-1)->moveCount == 1
-                                     || (type_of(movedPiece) == KING && !ss->inCheck))
+              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
-              if (   lmrDepth < 7
+              if (   lmrDepth < 7 + (type_of(movedPiece) == KING)
                   && !ss->inCheck
                   && ss->staticEval + 283 + 170 * lmrDepth <= alpha
                   &&  (*contHist[0])[movedPiece][to_sq(move)]
