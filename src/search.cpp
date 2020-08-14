5d050cd8657fc61f738176c5a493f788d66c2bd1
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-14 11:48:35 +0200

    Prune king moves if lmrDepth < 1, not in check and eval <= alpha-200. Bench: 3980661

--- src/search.cpp
+++ src/search.cpp
@@ -1046,7 +1046,7 @@ moves_loop: // When in check, search starts from here
 
               if (   lmrDepth < 1
                   && type_of(movedPiece) == KING
-                  && priorCapture
+                  && eval <= alpha - 200
                   && !ss->inCheck)
                   continue;
 
