e8292485795ea774a55d39a2fcbad91387e8bd03
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-23 11:17:26 +0200

    More reduction after prior capture and last move count = 0. That means we are after a probcut move. Bench: 3611076

--- src/search.cpp
+++ src/search.cpp
@@ -1164,6 +1164,10 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
+          // Increase reduction after prior capture if last move count == 0 (so last move is a probcut move)
+          if (priorCapture && (ss-1)->moveCount == 0)
+              r++;
+
           // Decrease reduction at non-check cut nodes for second move at low depths
           if (   cutNode
               && depth <= 10
