8c789568205a4332b3da4d3543704094e9188609
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-24 20:32:14 +0200

    Reintroduce IID with stronger conditions (based on stats). Bench: 3987388

--- src/search.cpp
+++ src/search.cpp
@@ -941,7 +941,7 @@ namespace {
     }
 
     // Step 11. Internal iterative deepening (~1 Elo)
-    if (depth >= 7 && !ttHit && !ttMove)
+    if (depth >= 7 && !ttMove && !ttHit && cutNode && eval <= alpha)
     {
         search<NT>(pos, ss, alpha, beta, depth - 7, cutNode);
 
