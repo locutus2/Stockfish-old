3f09f5dd7f851ac206a4f06a8f508defede6d2d0
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-08-26 19:03:36 +0200

    Use classic eval only if pawn count > 0. Bench: 3717747

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -941,7 +941,7 @@ make_v:
 Value Eval::evaluate(const Position& pos) {
 
   bool classical = !Eval::useNNUE
-                || (   pos.count<PAWN>() <= 14
+                || (   pos.count<PAWN>() > 0
                     && abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count()));
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
