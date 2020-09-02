8cc2182f7ea6591a06077d4c6c9e209372523e8b
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-03 01:02:15 +0200

    Skip LMR at root randomly for 1/16 of the moves. Bench: 4043781

--- src/search.cpp
+++ src/search.cpp
@@ -1160,6 +1160,7 @@ moves_loop: // When in check, search starts from here
       // re-searched at full depth.
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode + 2 * (PvNode && abs(bestValue) < 2)
+          && (!rootNode || !(thisThread->nodes & 0xF))
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
