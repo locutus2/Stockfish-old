d3c68d44a448d63d35660a5dfdca74039d723cc7
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-03 08:43:44 +0200

    Skip pruning at PV nodes if depth < 8. Bench: 4071468

--- src/search.cpp
+++ src/search.cpp
@@ -1015,6 +1015,7 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
+          && (!PvNode || depth >= 8)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
