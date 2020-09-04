ea8bb430f0e959dab871cc7d0aaa06c180c163a5
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-04 17:30:23 +0200

    Update also killers from two plies before is last move was no capture and both positions are not in check. Bench: 3804988

--- src/search.cpp
+++ src/search.cpp
@@ -1751,6 +1751,12 @@ moves_loop: // When in check, search starts from here
         ss->killers[0] = move;
     }
 
+    if (!pos.captured_piece() && !(ss-2)->inCheck && !ss->inCheck && (ss-2)->killers[0] != move)
+    {
+        (ss-2)->killers[1] = (ss-2)->killers[0];
+        (ss-2)->killers[0] = move;
+    }
+
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
