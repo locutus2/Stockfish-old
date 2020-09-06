43cd5f0bbf08fb28efef19fc7a1be52ca300cb22
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-06 20:15:53 +0200

    Now prune at leaf nodes only randomly 15/16 of moves. Bench: 3856875

--- src/search.cpp
+++ src/search.cpp
@@ -1015,7 +1015,7 @@ moves_loop: // When in check, search starts from here
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
           && pos.non_pawn_material(us)
-          && (depth > 1 || ((thisThread->nodes ^ move) & 1))
+          && (depth > 1 || ((thisThread->nodes ^ move) & 0xF))
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
