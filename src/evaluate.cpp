498f13a68f355c40da53ef4fe9093a5d1c97a5cf
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-06 12:41:50 +0200

    Tweak random usage of classic eval by using position key for randomization. So always for the same position the classic or nnue eval is used. Bench: 3738528

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1018,7 +1018,7 @@ Value Eval::evaluate(const Position& pos) {
   bool useClassical = abs(eg_value(pos.psq_score())) * 16 > NNUEThreshold1 * (16 + pos.rule50_count());
   bool classical = !Eval::useNNUE
                 ||  useClassical
-                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 8 && !(pos.this_thread()->nodes & 0xF));
+                || (abs(eg_value(pos.psq_score())) > PawnValueMg / 8 && !(pos.key() & 0xF));
   Value v = classical ? Evaluation<NO_TRACE>(pos).value()
                       : NNUE::evaluate(pos) * 5 / 4 + Tempo;
 
