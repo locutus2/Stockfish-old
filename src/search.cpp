740a2b046f176412edcc211979dec2e4d7877ccf
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-14 18:54:01 +0200

    More reduction of quiet moves if average eval <= alpha-100. Bench: 3693311

--- src/search.cpp
+++ src/search.cpp
@@ -1196,7 +1196,7 @@ moves_loop: // When in check, search starts from here
           if (!captureOrPromotion)
           {
               // Increase reduction if average eval in search tree is low
-              if (evalSumCount > 0 && evalSum / evalSumCount <= alpha)
+              if (evalSumCount > 0 && evalSum / evalSumCount <= alpha - 100)
                   r++;
 
               // Increase reduction if ttMove is a capture (~5 Elo)
