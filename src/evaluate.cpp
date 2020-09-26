fb8c1a19beab8e3db5e4947d8211150bafebed62
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-09-26 09:25:58 +0200

    Tweak recently added opposite bishop hybrid condition. Trigger it also if piece count is between 7 to 11. Bench: 4115548

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1040,7 +1040,7 @@ Value Eval::evaluate(const Position& pos) {
           || (   abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
               && !(pos.this_thread()->nodes & 0xB)
               && (   pos.opposite_bishops()
-                  || (pos.count<PAWN>() >= 4 && pos.count<PAWN>() <= 7)))))
+                  || (pos.count<ALL_PIECES>() >= 7 && pos.count<ALL_PIECES>() <= 11)))))
           v = adjusted_NNUE();
   }
 
