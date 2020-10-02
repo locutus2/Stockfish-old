7d82d88f32567544af755db1f9a8d3c8e93105e1
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-10-02 18:31:51 +0200

    Less reduction at former PV nodes if current ply is greater than selective depth. Bench: 3968544

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrease reduction if plies are greate than selective depth
-          if (ss->ply > thisThread->selDepth)
+          if (formerPv && ss->ply > thisThread->selDepth)
               r--;
 
           // Decrease reduction if the ttHit running average is large
