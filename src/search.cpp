32c50184e923d02024fcfee5ddc4644759cfdbf4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-10-07 22:01:53 +0200

    People complain of sometimes the very long thinkings of Stockfish in TCEC about a move in comparison to the past. One reason is the recently increase of bestMoveInstability factor to 2. Here i replace the factor with a formula dependend on log(#threads). This decays the factor with more threads aproaching 1 (8 threads => factor 1.5). No functional change for single core. Bench: 4016121

--- src/search.cpp
+++ src/search.cpp
@@ -517,7 +517,7 @@ void Thread::search() {
               totBestMoveChanges += th->bestMoveChanges;
               th->bestMoveChanges = 0;
           }
-          double bestMoveInstability = 1 + 2 * totBestMoveChanges / Threads.size();
+          double bestMoveInstability = 1 + (1 + 1 / (1 + 0.5 * std::log(Threads.size()))) * totBestMoveChanges / Threads.size();
 
           double totalTime = rootMoves.size() == 1 ? 0 :
                              Time.optimum() * fallingEval * reduction * bestMoveInstability;
