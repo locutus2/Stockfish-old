fbea167212b9fd18e786d7af1ba7fc939e1e7b3e
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-10-08 12:47:04 +0200

    Extend ttPv search tree only if really no best move was found. Bench: 3740596

--- src/search.cpp
+++ src/search.cpp
@@ -1366,11 +1366,11 @@ moves_loop: // When in check, search starts from here
 
     // If no good move is found and the previous position was ttPv, then the previous
     // opponent move is probably good and the new position is added to the search tree.
-    if (bestValue <= alpha)
+    if (!bestMove)
         ss->ttPv = ss->ttPv || ((ss-1)->ttPv && depth > 3);
     // Otherwise, a counter move has been found and if the position is the last leaf
     // in the search tree, remove the position from the search tree.
-    else if (!PvNode && depth > 3)
+    else if (depth > 3)
         ss->ttPv = ss->ttPv && (ss+1)->ttPv;
 
     if (!excludedMove && !(rootNode && thisThread->pvIdx))
