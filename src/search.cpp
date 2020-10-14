7591f19b2719f4d202b824c808d6cdc79f83c56a
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-10-14 23:02:32 +0200

    Less reduction if move matches any move of same color in current PV (check only at PV nodes). Bench: 3815706

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrese reduction if move matches any move of same color in current PV
-          if (findPvMove(thisThread, us, move))
+          if (PvNode && findPvMove(thisThread, us, move))
               r--;
 
           // Decrease reduction if the ttHit running average is large
