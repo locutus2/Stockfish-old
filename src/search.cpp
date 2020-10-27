1ff3a6546f14304beeb6c7f8e7ba5b82462273a4
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-10-27 07:29:04 +0100

    Decrease depth limit for history pruning if in check. Bench: 3857189

--- src/search.cpp
+++ src/search.cpp
@@ -1023,8 +1023,8 @@ moves_loop: // When in check, search starts from here
               && !givesCheck)
           {
               // Countermoves based pruning (~20 Elo)
-              if (   lmrDepth < 3 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
-                                  + (eval <= alpha + 400)
+              if (   lmrDepth < 4 + ((ss-1)->statScore > 0 || (ss-1)->moveCount == 1)
+                                  - ss->inCheck
                   && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
