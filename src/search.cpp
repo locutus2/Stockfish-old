70905eb83e1fecf5e61cb002937385fbc5924692
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-11-02 19:37:32 +0100

    Increase quadratic stat bonus term (with compensation for depth 1). Bench: 3610728

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return d > 13 ? 29 : 16 * d * d + 134 * d - 133;
+    return d > 13 ? 29 : 18 * d * d + 134 * d - 135;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
