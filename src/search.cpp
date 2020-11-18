0bb08d025de24fe4207f9b80ce2dccbae6fd9999
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-11-18 10:44:49 +0100

    Tweak stat score reduction formula dependent of move gives check (try opposite).  Bench: 3685229

--- src/search.cpp
+++ src/search.cpp
@@ -1218,7 +1218,7 @@ moves_loop: // When in check, search starts from here
                   r++;
 
               // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-              r -= (ss->statScore + 1500 * givesCheck - 99) / 14884;
+              r -= (ss->statScore - 1500 * givesCheck + 99) / 14884;
           }
           else
           {
