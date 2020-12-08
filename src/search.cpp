68c389f07f6ad8d2948f1d5efd48b6f1adc14be5
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-08 15:04:49 +0100

    Update static history only if eval diff at least 10 cp. Bench: 4072701

--- src/search.cpp
+++ src/search.cpp
@@ -818,8 +818,8 @@ namespace {
     // Update static history for previous move
     if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture)
     {
-        int bonus = ss->staticEval > -(ss-1)->staticEval + 2 * Tempo ? -stat_bonus(depth) * 2 :
-                    ss->staticEval < -(ss-1)->staticEval + 2 * Tempo ?  stat_bonus(depth) * 2 :
+        int bonus = ss->staticEval > -(ss-1)->staticEval + 2 * Tempo + 20 ? -stat_bonus(depth) :
+                    ss->staticEval < -(ss-1)->staticEval + 2 * Tempo - 20 ?  stat_bonus(depth) :
                     0;
         thisThread->staticHistory[~us][from_to((ss-1)->currentMove)] << bonus;
     }
