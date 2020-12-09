fd258fe0d9bc8907a150e0728abb819666013a26
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-09 17:49:22 +0100

    Extend stat bonus formula for depth 14. Depth 1 value are the same and max bonus keeps nearly the same. Bench: 3862258

--- src/search.cpp
+++ src/search.cpp
@@ -82,7 +82,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return d > 26 ? 29 : 8 * d * d + 134 * d - 125;
+    return d > 14 ? 29 : 15 * d * d + 119 * d - 117;
   }
 
   // Add a small random component to draw evaluations to avoid 3fold-blindness
