9002da40966678fd8be6a39b9329cfc218f70bc1
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-18 19:26:51 +0100

    Increase saturation speed by 50% for continuation history (keep maximum the same). Test against pb's passed simpStatHistFollowPR. Bench: 4078911

--- src/movepick.h
+++ src/movepick.h
@@ -97,10 +97,10 @@ typedef Stats<int16_t, 10692, 1, 1, MAX_LPH, int(SQUARE_NB) * int(SQUARE_NB)> Lo
 typedef Stats<Move, NOT_USED, NOT_USED, NOT_USED, PIECE_NB, SQUARE_NB> CounterMoveHistory;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-typedef Stats<int16_t, 10692, 3, 2, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
+typedef Stats<int16_t, 10692, 1, 1, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
-typedef Stats<int16_t, 29952, 1, 1, PIECE_NB, SQUARE_NB> PieceToHistory;
+typedef Stats<int16_t, 29952, 3, 2, PIECE_NB, SQUARE_NB> PieceToHistory;
 
 /// ContinuationHistory is the combined history of a given pair of moves, usually
 /// the current one given a previous one. The nested history table is based on
