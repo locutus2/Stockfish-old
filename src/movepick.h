bb886958638cf87c0f3ee2c857cf7810a7a46396
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-18 19:23:52 +0100

    Increase saturation speed by 50% for capture history (keep maximum the same). Test against pb's passed simpStatHistFollowPR. Bench: 4288382

--- src/movepick.h
+++ src/movepick.h
@@ -90,14 +90,14 @@ typedef Stats<int16_t, 10692, 1, 1, COLOR_NB, int(SQUARE_NB) * int(SQUARE_NB)> B
 /// and quiet moves which are/were in the PV (ttPv). It is cleared with each new
 /// search and filled during iterative deepening.
 constexpr int MAX_LPH = 4;
-typedef Stats<int16_t, 10692, 3, 2, MAX_LPH, int(SQUARE_NB) * int(SQUARE_NB)> LowPlyHistory;
+typedef Stats<int16_t, 10692, 1, 1, MAX_LPH, int(SQUARE_NB) * int(SQUARE_NB)> LowPlyHistory;
 
 /// CounterMoveHistory stores counter moves indexed by [piece][to] of the previous
 /// move, see www.chessprogramming.org/Countermove_Heuristic
 typedef Stats<Move, NOT_USED, NOT_USED, NOT_USED, PIECE_NB, SQUARE_NB> CounterMoveHistory;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-typedef Stats<int16_t, 10692, 1, 1, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
+typedef Stats<int16_t, 10692, 3, 2, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB> CapturePieceToHistory;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 typedef Stats<int16_t, 29952, 1, 1, PIECE_NB, SQUARE_NB> PieceToHistory;
