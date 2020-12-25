f3a50ed32c571d14dc50b8dce5be05fd95bfa953
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-25 13:29:20 +0100

    Try opposite: Use NNUE eval if psq score and classic eval differs not significantly (threshold = 27 = 5% quantile). Bench: 4025243

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1073,7 +1073,7 @@ Value Eval::evaluate(const Position& pos) {
       // small probability if the classical eval is less than the threshold.
       if (   largePsq && !strongClassical
           && (   abs(v) * 16 < NNUEThreshold2 * r50
-              || abs(v - eg_value(pos.psq_score()) * (pos.side_to_move() == WHITE ? 1 : -1)) > 1276
+              || abs(v - eg_value(pos.psq_score()) * (pos.side_to_move() == WHITE ? 1 : -1)) < 27
               || (   pos.opposite_bishops()
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
                   && !(pos.this_thread()->nodes & 0xB))))
