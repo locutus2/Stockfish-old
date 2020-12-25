619b3c1d4f0e516852c59a48ae5600ff9eca3246
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-25 09:51:01 +0100

    Use NNUE eval if psq score and classic eval differs significantly (threshold = 4700). Bench: 4220552

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1057,7 +1057,7 @@ Value Eval::evaluate(const Position& pos) {
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
-      Value psq = Value(max(abs(eg_value(pos.psq_score())), abs(mg_value(pos.psq_score()))));
+      Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
       bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
@@ -1073,6 +1073,7 @@ Value Eval::evaluate(const Position& pos) {
       // small probability if the classical eval is less than the threshold.
       if (   largePsq && !strongClassical
           && (   abs(v) * 16 < NNUEThreshold2 * r50
+              || abs(v - eg_value(pos.psq_score())) > 4700
               || (   pos.opposite_bishops()
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
                   && !(pos.this_thread()->nodes & 0xB))))
