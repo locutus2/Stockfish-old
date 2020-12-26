518075feb8ca5706af9254064f06013d014551ed
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-26 13:34:20 +0100

    Add additional index to capture history: state if target square is attacked by opponent pawn or attacked by opponent minor (if not attacked by pawn). Bench: 3823499

--- src/movepick.h
+++ src/movepick.h
@@ -97,7 +97,7 @@ typedef Stats<int16_t, 10692, MAX_LPH, int(SQUARE_NB) * int(SQUARE_NB)> LowPlyHi
 typedef Stats<Move, NOT_USED, PIECE_NB, SQUARE_NB> CounterMoveHistory;
 
 /// CapturePieceToHistory is addressed by a move's [piece][to][captured piece type]
-typedef Stats<int16_t, 10692, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB, 2> CapturePieceToHistory;
+typedef Stats<int16_t, 10692, PIECE_NB, SQUARE_NB, PIECE_TYPE_NB, 3> CapturePieceToHistory;
 
 /// PieceToHistory is like ButterflyHistory but is addressed by a move's [piece][to]
 typedef Stats<int16_t, 29952, PIECE_NB, SQUARE_NB> PieceToHistory;
@@ -108,8 +108,9 @@ typedef Stats<int16_t, 29952, PIECE_NB, SQUARE_NB> PieceToHistory;
 typedef Stats<PieceToHistory, NOT_USED, PIECE_NB, SQUARE_NB> ContinuationHistory;
 
 inline int capture_index(const Position& pos, Move m) {
-  return bool(  pawn_attacks_bb(pos.side_to_move(), to_sq(m))
-              & pos.pieces(~pos.side_to_move(), PAWN));
+  return pawn_attacks_bb(pos.side_to_move(), to_sq(m)) & pos.pieces(~pos.side_to_move(), PAWN)     ? 1 :
+            (attacks_bb<KNIGHT>(to_sq(m), pos.pieces()) & pos.pieces(~pos.side_to_move(), KNIGHT))
+         || (attacks_bb<BISHOP>(to_sq(m), pos.pieces()) & pos.pieces(~pos.side_to_move(), BISHOP)) ? 2 : 0;
 }
 
 /// MovePicker class is used to pick one pseudo legal move at a time from the
