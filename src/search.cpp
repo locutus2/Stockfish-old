48a46d11f920bc95dc70c1ea6756d53ba323c60d
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-28 14:16:18 +0100

    Don't decrease beta after fail low. Bench: 4370088

--- src/search.cpp
+++ src/search.cpp
@@ -451,7 +451,6 @@ void Thread::search() {
               // re-search, otherwise exit the loop.
               if (bestValue <= alpha)
               {
-                  beta = (alpha + beta) / 2;
                   alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                   failedHighCnt = 0;
