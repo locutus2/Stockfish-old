31f15245a840eb6d660b8d005c96f5183990700b
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-30 13:05:32 +0100

    Do LMR also for captures with bad capture history (threshold = -8472 = 10% quantile). Bench: 3995257

--- src/search.cpp
+++ src/search.cpp
@@ -1173,7 +1173,7 @@ moves_loop: // When in check, search starts from here
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
               || (!PvNode && !formerPv)
-              || thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < -9851
+              || thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < -8472
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
