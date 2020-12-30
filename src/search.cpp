a47b5414a379b89ecc7663573020dadfa6fe3979
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-30 17:58:27 +0100

    Use alpha += delta / 12 after fail high. Bench: 3929520

--- src/search.cpp
+++ src/search.cpp
@@ -460,7 +460,7 @@ void Thread::search() {
               }
               else if (bestValue >= beta)
               {
-                  alpha += delta / 8;
+                  alpha += delta / 12;
                   beta = std::min(bestValue + delta, VALUE_INFINITE);
                   ++failedHighCnt;
               }
