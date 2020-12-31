4e3016e9fd7a9dc1f8a6b94ec1904a3679789de8
Stefan Geschwentner <stgeschwentner@gmail.com>
2020-12-31 09:30:45 +0100

    Do no LMR for captures with good capture history at cut nodes (threshold = 7280 = 60% quantile). Test against master with PRs 3281 + 3283. Bench: 3910453

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,8 @@ moves_loop: // When in check, search starts from here
           && (  !captureOrPromotion
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              || (((!PvNode && !formerPv) || cutNode) && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
+              || (cutNode && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 7280)
+              || (!PvNode && !formerPv && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
       {
           Depth r = reduction(improving, depth, moveCount);
