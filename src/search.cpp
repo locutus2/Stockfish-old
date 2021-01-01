3612f6d76b5becd25b305e501c4742aa164883f6
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-01 08:55:52 +0100

    Do no LMR for captures with good capture history with low ttHitAverage (threshold = 8732 = 80% quantile). Bench: 4197887

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,8 @@ moves_loop: // When in check, search starts from here
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
               || (!PvNode && !formerPv && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
-              || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
+              || (   thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024
+                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 8732)))
       {
           Depth r = reduction(improving, depth, moveCount);
 
