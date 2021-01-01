982a7d47bccc1ffc8f4bedb1226c92aad8be6ec1
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-01 10:09:11 +0100

    Do no LMR for captures with good capture history when moveCountPruning = true (threshold = 4712 = 60% quantile). Bench: 3874041

--- src/search.cpp
+++ src/search.cpp
@@ -1156,9 +1156,9 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
-              || moveCountPruning
-              || (   ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 7120)
+              || (   moveCountPruning
+                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4712)
+              || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
               || (!PvNode && !formerPv && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 4506)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024))
