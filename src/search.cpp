9918ede302a8c637538f941f1dd43c8d2c6a9514
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-03 10:31:06 +0100

    Analog to stat score reduction give for captures a dynamic calculated reduction based on capture history: (capHist - 4357) / 6015. Bench: 3927159

--- src/search.cpp
+++ src/search.cpp
@@ -1232,9 +1232,11 @@ moves_loop: // When in check, search starts from here
           {
               // Unless giving check, this capture is likely bad
               if (   !givesCheck
-                  && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha
-                  && thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 10287)
+                  && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+              // Decrease/increase reduction for moves with a good/bad capture history
+              r -= (thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 4357) / 6015;
           }
 
           Depth d = std::clamp(newDepth - r, 1, newDepth);
