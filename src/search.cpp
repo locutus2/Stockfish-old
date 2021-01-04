efd1d1db8e34732187b5e77823f437c896f4dbe9
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-04 15:43:06 +0100

    Asymmetric initial deltas: deltaPlus=14, deltaMinus=20. Bench: 4291994

--- src/search.cpp
+++ src/search.cpp
@@ -404,8 +404,8 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               Value prev = rootMoves[pvIdx].previousScore;
-              deltaPlus = Value(15);
-              deltaMinus = Value(19);
+              deltaPlus = Value(14);
+              deltaMinus = Value(20);
               alpha = std::max(prev - deltaMinus,-VALUE_INFINITE);
               beta  = std::min(prev + deltaPlus, VALUE_INFINITE);
 
