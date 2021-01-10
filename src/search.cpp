69ab80d718283e15d3a21688e087b4c087e8c050
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-10 14:44:52 +0100

    Reduce even less in ttHit rule if !ss->ttPv && (ss-1)->moveCount > 13. Bench: 3947049

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
-              r--;
+              r -= 1 + (!ss->ttPv && (ss-1)->moveCount > 13);
 
           // Increase reduction if other threads are searching this position
           if (th.marked())
