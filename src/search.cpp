b5fcdbab15806600044b3cfb290158f11cfea383
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-14 09:26:53 +0100

    Do multiPV = 2 for a iteration if bestMoveChanges > 10. Bench: 4970372

--- src/search.cpp
+++ src/search.cpp
@@ -387,7 +387,7 @@ void Thread::search() {
       if (!Threads.increaseDepth)
          searchAgainCounter++;
 
-      if (bestMoveChanges > 12)
+      if (bestMoveChanges > 10)
           multiPV = std::min(std::max(originalMultiPV, (size_t)2), rootMoves.size());
       else
           multiPV = std::min(originalMultiPV, rootMoves.size());
