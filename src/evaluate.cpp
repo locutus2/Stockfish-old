6d973c0b7d18f903e1808876d47c8172beaba753
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-20 12:26:09 +0100

    Reduce NNUE eval by 1/(8+material/1024) if opposite colored bishops. Bench: 4419576

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1053,7 +1053,10 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + PawnValueMg * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (679 + mat / 32) / 1024 + Tempo;
+         Value val = NNUE::evaluate(pos) * (679 + mat / 32) / 1024 + Tempo;
+         if (pos.opposite_bishops())
+             val -= val / (8 + mat / 1024);
+         return val;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
