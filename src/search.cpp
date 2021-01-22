6d260c7031179ec8f1b8cc151d9d53425a50cd37
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-01-22 07:37:45 +0100

    No LMR at root but for compensation reduce more at ply 1. Also check previous move count > 3 and not PV node. Bench: 4990211

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Increase reduction at ply 1
-          if(ss->ply == 1 && (ss-1)->moveCount > 3)
+          if(!PvNode && ss->ply == 1 && (ss-1)->moveCount > 3)
               r++;
 
           // Decrease reduction if the ttHit running average is large
