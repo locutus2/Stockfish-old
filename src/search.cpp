e1c0dde242fccb4a75fcb9abbbeb965ad887150a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-06 20:21:41 +0100

    Allow futility pruning only for depth < 5 in sungula search. Bench: 4007896

--- src/search.cpp
+++ src/search.cpp
@@ -831,7 +831,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 9
+        &&  depth < 9 - 4 * bool(excludedMove)
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
@@ -894,7 +894,6 @@ namespace {
     if (   !PvNode
         &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
-        && !excludedMove
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
         // because probCut search has depth set to depth - 4 but we also do a move before it
