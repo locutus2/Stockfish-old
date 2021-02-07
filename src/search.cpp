284e7eff3c2c8faef423f6005eb8de9869813299
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-07 20:24:57 +0100

    Add quiet futility pruned moves to bad quiets array so negative history update can occur. Bench: 4482264

--- src/search.cpp
+++ src/search.cpp
@@ -1068,15 +1068,15 @@ moves_loop: // When in check, search starts from here
                     + (*contHist[1])[movedPiece][to_sq(move)]
                     + (*contHist[3])[movedPiece][to_sq(move)]
                     + (*contHist[5])[movedPiece][to_sq(move)] / 2 < 26394)
-                  continue;
-
-              // Prune moves with negative SEE (~20 Elo)
-              if (!pos.see_ge(move, Value(-(30 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
               {
                   if (quietCount < 64)
                       quietsSearched[quietCount++] = move;
                   continue;
               }
+
+              // Prune moves with negative SEE (~20 Elo)
+              if (!pos.see_ge(move, Value(-(30 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
+                  continue;
           }
       }
 
