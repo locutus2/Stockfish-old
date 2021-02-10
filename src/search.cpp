1ad26a96b6f6c01fd44351828f74aaccd9441141
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-10 21:20:25 +0100

    Do LMR always with a search depth lower than full search depth. Bench: 3805012

--- src/search.cpp
+++ src/search.cpp
@@ -1242,11 +1242,11 @@ moves_loop: // When in check, search starts from here
                   r -= ss->statScore / 14382;
           }
 
-          Depth d = std::clamp(newDepth - r, 0, newDepth);
+          Depth d = std::clamp(newDepth - r, 1, newDepth - 1);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          doFullDepthSearch = value > alpha && d != newDepth;
+          doFullDepthSearch = value > alpha;
 
           didLMR = true;
       }
