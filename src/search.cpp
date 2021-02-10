8b0ed3d2948b3836e3c45c6788259950948c4a0f
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-10 21:38:57 +0100

    Allow for checking moves LMR directly go into quience search. Bench: 4401936

--- src/search.cpp
+++ src/search.cpp
@@ -1242,11 +1242,11 @@ moves_loop: // When in check, search starts from here
                   r -= ss->statScore / 14382;
           }
 
-          Depth d = std::clamp(newDepth - r, 1, newDepth - 1);
+          Depth d = std::clamp(newDepth - r, Depth(!givesCheck), newDepth);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
-          doFullDepthSearch = value > alpha;
+          doFullDepthSearch = value > alpha && d != newDepth;
 
           didLMR = true;
       }
