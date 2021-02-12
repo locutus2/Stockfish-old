bb402a91f01ab757c16b7d11a34a2f0233415801
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-12 22:37:57 +0100

    Do only one ply more reduction if likelyFailLow. Bench: 3824319

--- src/search.cpp
+++ src/search.cpp
@@ -1027,7 +1027,8 @@ moves_loop: // When in check, search starts from here
 
       // Indicate PvNodes that will probably fail low if node was searched with non-PV search 
       // at depth equal or greater to current depth and result of this search was far below alpha
-      bool likelyFailLow =    ttMove
+      bool likelyFailLow =    PvNode 
+                           && ttMove 
                            && (tte->bound() & BOUND_UPPER) 
                            && ttValue < alpha + 200 + 100 * depth 
                            && tte->depth() >= depth;
@@ -1181,8 +1182,8 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV 
           // and node is not likely to fail low (~10 Elo)
-          if (ss->ttPv && !likelyFailLow)
-              r -= 2;
+          if (ss->ttPv)
+              r -= 2 - likelyFailLow;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
