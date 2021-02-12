a863ffa140eb35b583c0b04112ed1e9da855a13b
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-12 06:34:23 +0100

    Stat score offset: - 6025 + 1542 * improving. Bench: 4059786

--- src/search.cpp
+++ src/search.cpp
@@ -1232,7 +1232,7 @@ moves_loop: // When in check, search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 5437 + 1740 * ss->inCheck;
+                             - 6025 + 1542 * improving;
 
               // Decrease/increase reduction by comparing opponent's stat score (~10 Elo)
               if (ss->statScore >= -89 && (ss-1)->statScore < -116)
