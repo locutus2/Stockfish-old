033463d18e28805fff4d246a46c6ab2978e91f75
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-16 10:59:44 +0100

    Allow NMP also for (ss-1)->statScore >= 22661 if priorCapture && (ss-1)->moveCount > 1. Bench: 4045690

--- src/search.cpp
+++ src/search.cpp
@@ -839,9 +839,9 @@ namespace {
     // Step 8. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 22661
+        && ((ss-1)->statScore < 22661 || (priorCapture && (ss-1)->moveCount > 1))
         &&  eval >= beta
-        && (eval >= ss->staticEval || (cutNode && ttMove))
+        &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 24 * depth - 34 * improving + 162 * ss->ttPv + 159
         && !excludedMove
         &&  pos.non_pawn_material(us)
