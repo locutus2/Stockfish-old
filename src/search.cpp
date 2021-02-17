94e2e24570f1e1e1cd1584ca72c714532094107c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-17 21:59:32 +0100

    No history update for failed low singular search. Bench: 3657755

--- src/search.cpp
+++ src/search.cpp
@@ -1393,7 +1393,8 @@ moves_loop: // When in check, search starts from here
 
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
-             && !priorCapture)
+             && !priorCapture
+             && !excludedMove)
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
     if (PvNode)
