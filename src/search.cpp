a32e9e29702b1245a261f7e3ebb0dc4ceab1f0e4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-20 23:47:03 +0100

    Apply decreasing of depth at PV nodes only if tt hit. Bench: 3911798

--- src/search.cpp
+++ src/search.cpp
@@ -961,13 +961,14 @@ namespace {
          ss->ttPv = ttPv;
     }
 
-moves_loop: // When in check, search starts from here
-
     // Step 10. If the position is not in TT, decrease depth by 2
     if (   PvNode
         && depth >= 6
-        && !ttMove)
-        depth -= 2 - ss->inCheck;
+        && !ttMove
+        && ss->ttHit)
+        depth -= 2;
+
+moves_loop: // When in check, search starts from here
 
     ttCapture = ttMove && pos.capture_or_promotion(ttMove);
 
