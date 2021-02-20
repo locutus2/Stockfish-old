41e0c2dbdd0262312f5f6b6d723ac9fd4c5b5496
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-02-20 18:22:59 +0100

    Randomly (p=1/64) add 8192 to quiet move order score. Bench: 4155085

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -19,6 +19,7 @@
 #include <cassert>
 
 #include "movepick.h"
+#include "thread.h"
 
 namespace {
 
@@ -109,7 +110,8 @@ void MovePicker::score() {
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
+                   + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0)
+                   + ((pos.this_thread()->nodes ^ m) & 0x3F ? 0 : 8192);
 
       else // Type == EVASIONS
       {
