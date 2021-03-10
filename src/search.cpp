16e0bb43ef713b8dee179b14f93ac861d00184ab
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-10 18:11:29 +0100

    Increase lmrDepth threshold by one at cut nodes for capture history pruning. Bench: 4905009

--- src/search.cpp
+++ src/search.cpp
@@ -1071,8 +1071,7 @@ moves_loop: // When in check, search starts from here
           {
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
-                  && type_of(move) != PROMOTION
-                  && lmrDepth < 1
+                  && lmrDepth < 1 + cutNode
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
