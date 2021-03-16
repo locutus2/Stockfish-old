e7214846ec76efe670b85745df53d13b0290edcc
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-16 11:51:13 +0100

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1198,12 +1198,14 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
           
           CC = captureOrPromotion && 
-		  cutNode && 
+		  //cutNode && 
+                  //ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha &&
+                  (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678) &&
 		  !(
                  moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
-              //|| cutNode
-              || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
+              || cutNode
+              //|| (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024);
           V = (ss+1)->distanceFromPv;
 
