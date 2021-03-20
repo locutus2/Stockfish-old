f6bf2f414643a43f1852ba6e7db00491ffa9d814
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-20 16:21:09 +0100

    Set partial sorting limit to zero for depth 1 but formula is asymtotic to master formula for higher depth. Add 6000/(depth+1). Bench: 4586773

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, 3000 - 3000 * depth);
+          partial_insertion_sort(cur, endMoves, -3000 * depth + 6000 / (depth + 1));
       }
 
       ++stage;
