7bfd15d8fad79ccfa1d9204f9309c05228611087
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-21 12:10:34 +0100

    Half sort limit at cut nodes. Bench: 4799158

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts from here
                                       countermove,
                                       ss->killers,
                                       ss->ply,
-                                      PvNode);
+                                      !cutNode);
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
