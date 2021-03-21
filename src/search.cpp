61641396bbd6b347ea07ddc564860790ec6c47c7
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-21 22:43:23 +0100

    Do full sort at former PV nodes. Bench: 4370224

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts from here
                                       countermove,
                                       ss->killers,
                                       ss->ply,
-                                      PvNode || (formerPv && !cutNode));
+                                      formerPv);
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
