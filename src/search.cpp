2826a9900b1f2fd0868215dfb0a2a266b24be5ff
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-21 11:42:04 +0100

    Do a full sort at PV nodes or former PV which not to be expected a cut node. Bench: 4261835

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop: // When in check, search starts from here
                                       countermove,
                                       ss->killers,
                                       ss->ply,
-                                      ss->ttPv);
+                                      PvNode || (formerPv && !cutNode));
 
     value = bestValue;
     singularQuietLMR = moveCountPruning = false;
