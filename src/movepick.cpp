b399b9b343a234d2b159f208e3e22ac8eaeb0051
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-22 23:23:31 +0100

    Linear (without always sorting positive values). Bench: 4947043

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -64,7 +64,7 @@ namespace {
 void MovePicker::init() {
 
     for (int i = 0; i < 32; ++i)
-        Quantile[i] = MAX_QUANTILE * std::min(1.0, 0.1732 + 0.0388 * i + 0.0013 * i * i);
+        Quantile[i] = MAX_QUANTILE * std::min(1.0, 0.1261 + 0.0576 * i);
 }
 
 /// Constructors of the MovePicker class. As arguments we pass information
