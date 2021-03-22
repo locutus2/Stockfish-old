e800284a4a3bf7a3d59d09445eab03f1dd3a6af4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-22 22:43:13 +0100

    Try logistic with cubic term. Bench: 4425572

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -65,7 +65,7 @@ namespace {
 void MovePicker::init() {
 
     for (int i = 0; i < 32; ++i)
-        Quantile[i] = MAX_QUANTILE * std::min(1.0, std::max(0.0, 0.05 * (16 - i) / 15 + 1 / (1 + std::exp(1.617 - 0.24842 * i))));
+        Quantile[i] = MAX_QUANTILE / (1 + std::exp(1.7188 - 0.4038 * i + 0.0398 * i * i - 0.0025 * i * i * i));
 }
 
 /// Constructors of the MovePicker class. As arguments we pass information
