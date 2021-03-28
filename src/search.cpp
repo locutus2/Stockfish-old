451f8c19efb75b9d343a7e485e287e98afc9d34c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-03-28 11:55:04 +0200

    Do check extension only if cutNode or not formerPv or ttMove. Bench: 4707469

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop: // When in check, search starts from here
 
       // Check extension (~2 Elo)
       else if (    givesCheck
-               && (!formerPv || move == ttMove)
+               && (cutNode || !formerPv || move == ttMove)
                && (pos.is_discovered_check_on_king(~us, move) || pos.see_ge(move)))
           extension = 1;
 
