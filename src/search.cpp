ab19dceb7ddb5b3e04dbcf220659022357893f84
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-01 19:08:53 +0200

    No killer update if in check. Bench: 4935765

--- src/search.cpp
+++ src/search.cpp
@@ -1806,7 +1806,7 @@ moves_loop: // When in check, search starts from here
   void update_quiet_stats(const Position& pos, Stack* ss, Move move, int bonus, int depth) {
 
     // Update killers
-    if ((ss-1)->currentMove != MOVE_NULL && ss->killers[0] != move)
+    if (!ss->inCheck && ss->killers[0] != move)
     {
         ss->killers[1] = ss->killers[0];
         ss->killers[0] = move;
