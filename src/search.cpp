b048d468fb533160fa615d4b9a8b680fb5e29307
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-04 09:38:59 +0200

    Don't use non-check-capture reduction if recapture. Bench: 4635489

--- src/search.cpp
+++ src/search.cpp
@@ -1232,12 +1232,10 @@ moves_loop: // When in check, search starts from here
 
           if (captureOrPromotion)
           {
-              if (priorCapture && to_sq(move) == prevSq)
-                  r -= 2;
-
               // Increase reduction for non-checking captures likely to be bad
-              else if (   !givesCheck
-                       && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
+              if (   !givesCheck
+                  && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha
+                  && (!priorCapture || to_sq(move) != prevSq))
                   r++;
           }
           else
