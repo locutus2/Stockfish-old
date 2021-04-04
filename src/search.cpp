07c6bbb60171407abc806dc1a0115b92b013d4d5
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-04 08:32:17 +0200

    Less reduction if recapture. Bench: 4349850

--- src/search.cpp
+++ src/search.cpp
@@ -1232,6 +1232,9 @@ moves_loop: // When in check, search starts from here
 
           if (captureOrPromotion)
           {
+              if (priorCapture && to_sq(move) == prevSq)
+                  --r;
+
               // Increase reduction for non-checking captures likely to be bad
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
