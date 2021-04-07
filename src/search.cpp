571cecec989d77edbe76ee4e3af7e98f9623c65e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-07 07:55:53 +0200

    Remove trick which calculates static eval after null move from static eval of previous position. After introduction of NNUE this symmetry does not really hold anymore (especially considering úsing rotate instead of flip for mirroing). So calculate from scratch in this case also. Bench: 4437191

--- src/search.cpp
+++ src/search.cpp
@@ -805,12 +805,7 @@ namespace {
     }
     else
     {
-        // In case of null move search use previous static eval with a different sign
-        // and addition of two tempos
-        if ((ss-1)->currentMove != MOVE_NULL)
-            ss->staticEval = eval = evaluate(pos);
-        else
-            ss->staticEval = eval = -(ss-1)->staticEval + 2 * Tempo;
+        ss->staticEval = eval = evaluate(pos);
 
         // Save static evaluation into transposition table
         tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
@@ -1533,11 +1528,7 @@ moves_loop: // When in check, search starts from here
                 bestValue = ttValue;
         }
         else
-            // In case of null move search use previous static eval with a different sign
-            // and addition of two tempos
-            ss->staticEval = bestValue =
-            (ss-1)->currentMove != MOVE_NULL ? evaluate(pos)
-                                             : -(ss-1)->staticEval + 2 * Tempo;
+            ss->staticEval = bestValue = evaluate(pos);
 
         // Stand pat. Return immediately if static value is at least beta
         if (bestValue >= beta)
