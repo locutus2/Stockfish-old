a73bd1f27ad49de86c69d228008e5c6e9d8a27fe
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-09 07:53:08 +0200

    More reduction if capturing a queen and capturing piece is not a queen. Bench: 4179900

--- src/search.cpp
+++ src/search.cpp
@@ -1234,7 +1234,7 @@ moves_loop: // When in check, search starts from here
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
 
-              if (type_of(pos.captured_piece()) == QUEEN)
+              if (type_of(pos.captured_piece()) == QUEEN && type_of(movedPiece) != QUEEN)
                   r++;
           }
           else
