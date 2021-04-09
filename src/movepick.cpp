e35bcfde97930a2a703f1d5a7249874ed98f0113
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-09 07:33:37 +0200

    Replace 2*cmh[0] with cmh[0]+cmh[2] in move order score. Bench: 4334967

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -107,9 +107,9 @@ void MovePicker::score() {
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
-                   + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
+                   +     (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
-                   +     (*continuationHistory[2])[pos.moved_piece(m)][to_sq(m)] / 2
+                   +     (*continuationHistory[2])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
                    + (ply < MAX_LPH ? std::min(4, depth / 3) * (*lowPlyHistory)[ply][from_to(m)] : 0);
