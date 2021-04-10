221535b68fbe90333ca0fc4e4d2f9735f22e013a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-10 18:40:16 +0200

    Randomize best move change reduction rule. Bench: 4422949

--- src/search.cpp
+++ src/search.cpp
@@ -1206,13 +1206,14 @@ moves_loop: // When in check, search starts from here
           // and node is not likely to fail low. (~10 Elo)
           if (   ss->ttPv
               && !likelyFailLow)
-              r -= (thisThread->nodes & 2) + 1;
+              r -= 2;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if (   (rootNode || !PvNode)
               && thisThread->rootDepth > 10
-              && thisThread->bestMoveChanges <= 2)
-              r++;
+              && thisThread->bestMoveChanges <= 2
+              && (thisThread->nodes & 1))
+              r += 2;
 
           // More reductions for late moves if position was not in previous PV
           if (   moveCountPruning
