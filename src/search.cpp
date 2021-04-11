7db2f3d71a45ce0024759aa0c8406932d33d55d7
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-11 23:35:16 +0200

    Allow LMR for check moves only for depth >= 4. Bench: 4556696

--- src/search.cpp
+++ src/search.cpp
@@ -1183,7 +1183,7 @@ moves_loop: // When in check, search starts from here
       // We use various heuristics for the sons of a node after the first son has
       // been searched. In general we would like to reduce them, but there are many
       // cases where we extend a son if it has good chances to be "interesting".
-      if (    depth >= 3
+      if (    depth >= 3 + givesCheck
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
               || moveCountPruning
