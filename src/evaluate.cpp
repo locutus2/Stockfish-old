86e035c87ae6b54a64ee86a8643824abeb710e38
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-14 20:02:56 +0200

    Use classical eval for rook vs pawns. Bench: 4555433

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1113,7 +1113,7 @@ Value Eval::evaluate(const Position& pos) {
       // Use classical evaluation for really low piece endgames.
       // The most critical case is a bishop + A/H file pawn vs naked king draw.
       bool strongClassical =    pos.non_pawn_material() == BishopValueMg
-                            ||  pos.non_pawn_material() == KnightValueMg
+                            ||  pos.non_pawn_material() == RookValueMg
                             || (pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2);
 
       v = classical || strongClassical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
