34dff7648e76fdd371b5dfb194558b842b064120
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-18 17:24:26 +0200

    If prior capture then search for probcut two moves more. Bench: 4145522

--- src/search.cpp
+++ src/search.cpp
@@ -923,7 +923,7 @@ namespace {
         ss->ttPv = false;
 
         while (   (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 2 + 2 * cutNode + priorCapture)
+               && probCutCount < 2 + 2 * cutNode + 2 * priorCapture)
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
