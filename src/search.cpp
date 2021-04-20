ef709bf3e9c640fbb6f656541a6d3a1347f2ba74
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-20 09:35:10 +0200

    At root do more reductions for half of the moves. Bench: 4073638

--- src/search.cpp
+++ src/search.cpp
@@ -1189,8 +1189,8 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (rootNode && !(thisThread->nodes & 3))
-              r--;
+          if (rootNode && (thisThread->nodes & 1))
+              r++;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
