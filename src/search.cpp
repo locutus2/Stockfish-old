3f8b2713c3d762cb2c812d1a33c10f0a230965a6
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-21 22:28:39 +0200

    Less reduction for capture/promotion if in check at cut node. Bench: 4094311

--- src/search.cpp
+++ src/search.cpp
@@ -1228,6 +1228,9 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+              if (cutNode && ss->inCheck)
+                  r--;
           }
           else
           {
