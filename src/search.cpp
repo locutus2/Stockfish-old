1b40eb593da9649be20c4cac4ed7b9b7488a6f93
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-23 00:58:59 +0200

    More reduction at cut node if depth <= 3. Bench: 4888241

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (cutNode && depth <= 7)
+          if (cutNode && depth <= 3)
               r++;
 
           // Decrease reduction if the ttHit running average is large
