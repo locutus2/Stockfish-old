cac47ba5b011627507fc00773cda66979f321449
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-22 19:01:18 +0200

    Less reduction for caputure at first three ranks. Bench: 4590496

--- src/search.cpp
+++ src/search.cpp
@@ -1229,7 +1229,7 @@ moves_loop: // When in check, search starts from here
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
 
-              if (cutNode && ss->ttHit && (tte->bound() & BOUND_LOWER))
+              if (relative_rank(us, to_sq(move)) < RANK_4)
                   r--;
           }
           else
