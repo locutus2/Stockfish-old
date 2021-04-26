d0556da2821dc247574b928d7b9107b478892b76
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-26 19:30:14 +0200

    Inspired by the tune50mrdc tuning try a quadratic approach for the rule 50 counter in NNUE eval scaling: r * r / 25. Bench: 4738780

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1094,7 +1094,7 @@ Value Eval::evaluate(const Position& pos) {
          int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
          int scale =  580
                     + material / 32
-                    - 4 * pos.rule50_count();
+                    - pos.rule50_count() * pos.rule50_count() / 25;
 
          Value nnue = NNUE::evaluate(pos) * scale / 1024 + Tempo;
 
