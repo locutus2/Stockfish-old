d96c28b3dc1228cbe5129b2919e4b7f3b1730f94
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-04-27 06:27:33 +0200

    Double quadratic rule 50 count for NNUE eval. Bench: 4721158

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1092,9 +1092,10 @@ Value Eval::evaluate(const Position& pos) {
       auto  adjusted_NNUE = [&]()
       {
          int material = pos.non_pawn_material() + 4 * PawnValueMg * pos.count<PAWN>();
+         int r = pos.rule50_count() * (200 - pos.rule50_count()) / 100;
          int scale =  580
                     + material / 32
-                    - pos.rule50_count() * (300 - pos.rule50_count()) / 50;
+                    - r * (200 - r) / 25;
 
          Value nnue = NNUE::evaluate(pos) * scale / 1024 + Tempo;
 
