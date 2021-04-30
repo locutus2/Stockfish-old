307eda49abb6c0068b1db643667698c20e883068
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-01 00:32:16 +0200

    Increase bestMoveChanges by (move count + 2) / 4. Bench: 4926278

--- src/search.cpp
+++ src/search.cpp
@@ -1351,7 +1351,7 @@ moves_loop: // When in check, search starts from here
               // We record how often the best move has been changed in each
               // iteration. This information is used for time management and LMR
               if (moveCount > 1)
-                  thisThread->bestMoveChanges += moveCount / 2;
+                  thisThread->bestMoveChanges += (moveCount + 2) / 4;
           }
           else
               // All other moves but the PV are set to the lowest value: this
