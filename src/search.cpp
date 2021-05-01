8190ddbd39fc74bf37cd4b51bc964213acd04e0c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-01 13:05:26 +0200

    5% more aging of total best move changes. Bench: 4075325

--- src/search.cpp
+++ src/search.cpp
@@ -375,7 +375,7 @@ void Thread::search() {
   {
       // Age out PV variability metric
       if (mainThread)
-          totBestMoveChanges /= 3;
+          totBestMoveChanges /= 2.1;
 
       // Save the last iteration's scores before first PV line is searched and
       // all the move scores except the (new) PV are set to -VALUE_INFINITE.
