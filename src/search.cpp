77dc80ccc2d8dab80322543314320a5f79e06d5a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-02 17:57:03 +0200

    After removing check extensions try less reductions for checking moves. Bench: 3728081

--- src/search.cpp
+++ src/search.cpp
@@ -1187,6 +1187,9 @@ moves_loop: // When in check, search starts from here
       {
           Depth r = reduction(improving, depth, moveCount);
 
+          if (givesCheck)
+              r--;
+
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
