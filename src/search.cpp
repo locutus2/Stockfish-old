f4c1f2d6914a1558c23428e85d6f24375aea41f2
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-03 19:51:37 +0200

    test

--- src/search.cpp
+++ src/search.cpp
@@ -1194,7 +1194,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
 	  CC = givesCheck;
-	  C = PvNode;
+	  C = cutNode;
 
           // Decrease reduction if the ttHit running average is large
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
