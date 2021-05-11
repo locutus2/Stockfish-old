91b79e4d3113e7cf478080d0e82cd51e1669e7ef
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-11 18:17:01 +0200

    More reduction for quiet moves if singular search failed and value <= alpha. Bench: 3982010

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts from here
                   return beta;
           }
 
-          else if (pos.capture_or_promotion(ss->currentMove))
+          else if (value <= alpha)
               singularFailed = true;
       }
 
