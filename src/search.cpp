90c8b9684b41b0028c3df7e625f7c88eb1952d10
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-11 15:17:25 +0200

    More reduction for quiet moves if singular search failed. Bench: 3770670

--- src/search.cpp
+++ src/search.cpp
@@ -1228,14 +1228,14 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
-
-              if (singularFailed && depth < 12)
-                  r--;
           }
           else
           {
               // Increase reduction if ttMove is a capture (~5 Elo)
               if (ttCapture)
+                  r++;
+
+              if (singularFailed)
                   r++;
 
               // Increase reduction at root if failing high
