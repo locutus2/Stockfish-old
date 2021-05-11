da05baa92a313a198f7021c2fe3a777fc3d017f8
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-11 20:28:10 +0200

    Instead of extending reduce other moves more. Also do LMR for all captures if singular move found. Bench: 2002234

--- src/search.cpp
+++ src/search.cpp
@@ -1131,6 +1131,7 @@ moves_loop: // When in check, search starts from here
               || moveCountPruning
               || ss->staticEval + PieceValue[EG][pos.captured_piece()] <= alpha
               || cutNode
+              || extension
               || (!PvNode && !formerPv && captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] < 3678)
               || thisThread->ttHitAverage < 432 * TtHitAverageResolution * TtHitAverageWindow / 1024)
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
