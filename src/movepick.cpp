da774bb04f73038a454d0bb87c3b26c12788b82c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-13 19:40:31 +0200

    Do full sort for ply <= 6. Bench: 3373887

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, (depth - ply < 10 ? -3000 * depth : std::numeric_limits<int>::min()));
+          partial_insertion_sort(cur, endMoves, (ply > 6 ? -3000 * depth : std::numeric_limits<int>::min()));
       }
 
       ++stage;
