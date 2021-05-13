3deefb8cdf84cceb2b3ecc6cf653d67c0099fb0d
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-13 15:53:46 +0200

    Do full sort for depth >= 12. Bench: 3653222

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, (depth < 11 ? -3000 * depth : std::numeric_limits<int>::min()));
+          partial_insertion_sort(cur, endMoves, (depth < 12 ? -3000 * depth : std::numeric_limits<int>::min()));
       }
 
       ++stage;
