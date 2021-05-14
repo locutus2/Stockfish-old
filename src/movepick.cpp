12c031999c4f413612ac3996be97f5c64038e82c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-14 08:23:18 +0200

    Quadratic sort limit -300 * depth * depth. Bench: 3755533

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -300 * depth * depth);
       }
 
       ++stage;
