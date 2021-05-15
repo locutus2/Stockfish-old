f3daf793d8b10855de5a42e8fd4789ee91e0c5e2
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-15 23:00:24 +0200

    Experiment: add now also singular moves to tree. Bench: 4478424

--- src/search.cpp
+++ src/search.cpp
@@ -1185,6 +1185,9 @@ moves_loop: // When in check, search starts from here
               singularQuietLMR = !ttCapture;
               if (!PvNode && value < singularBeta - 140)
                   extension = 2;
+
+              if (ss->node)
+                  ss->node->add_move(move);
           }
 
           // Multi-cut pruning
@@ -1383,9 +1386,6 @@ moves_loop: // When in check, search starts from here
           {
               bestMove = move;
 
-              if(ss->node)
-                  ss->node->add_move(bestMove);
-
               if (PvNode && !rootNode) // Update pv even in fail-high case
                   update_pv(ss->pv, move, (ss+1)->pv);
 
@@ -1433,8 +1433,13 @@ moves_loop: // When in check, search starts from here
 
     // If there is a move which produces search value greater than alpha we update stats of searched moves
     else if (bestMove)
+    {
+        if (ss->node)
+            ss->node->add_move(bestMove);
+
         update_all_stats(pos, ss, bestMove, bestValue, beta, prevSq,
                          quietsSearched, quietCount, capturesSearched, captureCount, depth);
+    }
 
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
