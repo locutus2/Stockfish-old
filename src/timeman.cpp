1e817190b74c76b6e129c8cc15a544d5480afcda
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-15 13:10:33 +0200

    Use more time for early plies and less for later plies: std::pow(0.55 * ply + 16.0, 0.5). Bench: 4749514

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -77,7 +77,7 @@ void TimeManagement::init(Search::LimitsType& limits, Color us, int ply) {
   // game time for the current move, so also cap to 20% of available game time.
   if (limits.movestogo == 0)
   {
-      optScale = std::min(0.0084 + std::pow(1.5 * ply + 7.9, 0.45) * 0.0042,
+      optScale = std::min(0.0084 + std::pow(0.55 * ply + 16.0, 0.5) * 0.0042,
                            0.2 * limits.time[us] / double(timeLeft));
       maxScale = std::min(7.0, 4.0 + ply / 12.0);
   }
