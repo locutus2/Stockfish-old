36a0b65bfae067d5f930de6b0cb60e7aea16d389
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-17 01:58:03 +0200

    Remove previous time reduction. Bench: 4749514

--- src/search.cpp
+++ src/search.cpp
@@ -260,7 +260,7 @@ void Thread::search() {
   Move  lastBestMove = MOVE_NONE;
   Depth lastBestMoveDepth = 0;
   MainThread* mainThread = (this == Threads.main() ? Threads.main() : nullptr);
-  double timeReduction = 1, totBestMoveChanges = 0;
+  double totBestMoveChanges = 0;
   Color us = rootPos.side_to_move();
   int iterIdx = 0;
 
@@ -468,8 +468,7 @@ void Thread::search() {
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
           // If the bestMove is stable over several iterations, reduce time accordingly
-          timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.92 : 0.95;
-          double reduction = (1.47 + mainThread->previousTimeReduction) / (2.32 * timeReduction);
+          double timeReduction = lastBestMoveDepth + 9 < completedDepth ? 0.761 : 1.098;
 
           // Use part of the gained time from a previous stable move for the current move
           for (Thread* th : Threads)
@@ -479,7 +478,7 @@ void Thread::search() {
           }
           double bestMoveInstability = 1 + 2 * totBestMoveChanges / Threads.size();
 
-          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability;
+          double totalTime = Time.optimum() * fallingEval * timeReduction * bestMoveInstability;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
@@ -510,8 +509,6 @@ void Thread::search() {
 
   if (!mainThread)
       return;
-
-  mainThread->previousTimeReduction = timeReduction;
 
   // If skill level is enabled, swap best PV line with the sub-optimal one
   if (skill.enabled())
