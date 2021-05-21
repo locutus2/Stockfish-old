34ed661a0415a3e29bdfacb76e7f95f56ecd96b6
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-21 10:18:20 +0200

    Use nnue lazy mode only for buckets 4-7. Bench: 4015864

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -184,7 +184,7 @@ namespace Stockfish::Eval::NNUE {
           - psqtAccumulation[static_cast<int>(perspectives[1])][bucket]
         ) / 2;
 
-      if (abs(psqt) > LazyThreshold * OutputScale && bucket < 4)
+      if (abs(psqt) > LazyThreshold * OutputScale && bucket > 3)
         return { psqt, true };
 
   #if defined(USE_AVX512)
