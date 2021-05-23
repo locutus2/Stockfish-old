15808eab36736617748ed4a9f2961e39afe2f547
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-23 20:01:16 +0200

    Do ttCapture reduction for non-checking captures. Bench: 3865338

--- src/search.cpp
+++ src/search.cpp
@@ -1126,7 +1126,7 @@ moves_loop: // When in check, search starts from here
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r =  reduction(improving, depth, moveCount)
-                   + (ttCapture && !givesCheck);
+                   + (ttCapture && (!captureOrPromotion || !givesCheck));
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
