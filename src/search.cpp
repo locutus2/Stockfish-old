26136419168e17de109d8d16e61bc9c36a090032
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-23 11:59:09 +0200

    No LMR for capture checks at non-PV and non-former-PV nodes. Bench: 3334808

--- src/search.cpp
+++ src/search.cpp
@@ -1121,8 +1121,8 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
-              || (cutNode && !givesCheck)
-              || (!PvNode && !formerPv))
+              || cutNode
+              || (!PvNode && !formerPv && !givesCheck))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
