caf59f67c6a1628d1ce536e914408bf8bd4fa5b4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-25 14:01:07 +0200

    Ignore singularQuietLMR reduction if at cut node. Bench: 3777802

--- src/search.cpp
+++ src/search.cpp
@@ -1128,7 +1128,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
-          if (!cutNode && thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
+          if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
 
           // Decrease reduction if position is or has been on the PV
@@ -1148,7 +1148,7 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Decrease reduction if ttMove has been singularly extended (~1 Elo)
-          if (singularQuietLMR)
+          if (!cutNode && singularQuietLMR)
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
