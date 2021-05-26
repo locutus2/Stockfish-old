9e05372766cd550f4c3a8533ef42979bf8de2c24
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-26 08:16:12 +0200

    Do capture LMR at former PV nodes only if previous move is not the first move. Bench: 3912508

--- src/search.cpp
+++ src/search.cpp
@@ -1120,7 +1120,7 @@ moves_loop: // When in check, search starts from here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
-          && (!captureOrPromotion || !ss->ttPv)
+          && (!captureOrPromotion || (!PvNode && (!formerPv || (ss-1)->moveCount > 1)))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
           Depth r = reduction(improving, depth, moveCount);
