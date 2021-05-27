f8513d40ee7da2d635ba20353654c54a5d19a68a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-27 13:20:38 +0200

    Exclude captures at cur nodes from LMR if it captures last moved piece. Bench: 3866082

--- src/search.cpp
+++ src/search.cpp
@@ -1121,7 +1121,7 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
-              || (cutNode && to_sq(move) != prevSq)
+              || (cutNode && (ss-1)->moveCount > 1 && to_sq(move) != prevSq)
               || (!PvNode && !formerPv))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
