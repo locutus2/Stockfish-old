7295ae4361eec340118256221615fa2a521cecbc
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-05-27 10:52:30 +0200

    Exclude now captures at cut nodes also for the second previous move. Bench: 3960136

--- src/search.cpp
+++ src/search.cpp
@@ -1121,7 +1121,7 @@ moves_loop: // When in check, search starts from here
       if (    depth >= 3
           &&  moveCount > 1 + 2 * rootNode
           && (  !captureOrPromotion
-              || (cutNode && (ss-1)->moveCount > 1)
+              || (cutNode && (ss-1)->moveCount > 2)
               || (!PvNode && !formerPv))
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
