0cb312a1bfc60f69faf662e474f4e78ecc4d6616
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-01 10:53:48 +0200

    Do reduction for quiet moves at cut nodes only if depth <= 10. Bench: 4464940

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 + !captureOrPromotion * (1 + (depth > 10));
+              r += 1 + (!captureOrPromotion && depth <= 10);
 
           if (!captureOrPromotion)
           {
