cf837dd159153a1a8c02ea661ea958bd2048789e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-04 00:16:39 +0200

    Reduce prob cut depth at non-cut nodes (use at minimum depth 1). Bench: 4248601

--- src/search.cpp
+++ src/search.cpp
@@ -852,7 +852,7 @@ namespace {
     // If we have a good enough capture and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
-        &&  depth > 5 - cutNode
+        &&  depth > 4
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
@@ -875,7 +875,7 @@ namespace {
             if (move != excludedMove && pos.legal(move))
             {
                 assert(pos.capture_or_promotion(move));
-                assert(depth >= 6 - cutNode);
+                assert(depth >= 5);
 
                 captureOrPromotion = true;
                 probCutCount++;
@@ -893,7 +893,7 @@ namespace {
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
-                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 5 + cutNode, !cutNode);
+                    value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, std::max(depth - 5 + cutNode, 1), !cutNode);
 
                 pos.undo_move(move);
 
