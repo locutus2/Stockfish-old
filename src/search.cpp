88dfc59bb9bdd6b9bcf426f2bfd3b611718f6119
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-05 08:04:05 +0200

    Replace in probcut qsearch by search with depth-11. Bench: 4268331

--- src/search.cpp
+++ src/search.cpp
@@ -889,7 +889,7 @@ namespace {
                 pos.do_move(move, st);
 
                 // Perform a preliminary qsearch to verify that the move holds
-                value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 13, !cutNode);
+                value = -search<NonPV>(pos, ss+1, -probCutBeta, -probCutBeta+1, depth - 11, !cutNode);
 
                 // If the qsearch held, perform the regular search
                 if (value >= probCutBeta)
