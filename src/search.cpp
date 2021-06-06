726fb05aaa9aee5adbe469e750c4c9573d6edd85
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-06 20:30:36 +0200

    Use smaller futility margin if ttCapture. Bench: 4298187

--- src/search.cpp
+++ src/search.cpp
@@ -65,8 +65,8 @@ namespace {
   constexpr uint64_t TtHitAverageResolution = 1024;
 
   // Futility margin
-  Value futility_margin(Depth d, bool improving) {
-    return Value(214 * (d - improving));
+  Value futility_margin(Depth d, bool improving, bool ttCapture) {
+    return Value(214 * (d - improving - ttCapture));
   }
 
   // Reductions lookup table, initialized at startup
@@ -793,8 +793,8 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 9 - ttCapture
-        &&  eval - futility_margin(depth, improving) >= beta
+        &&  depth < 9
+        &&  eval - futility_margin(depth, improving, ttCapture) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
