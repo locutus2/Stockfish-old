e67c457dce86ffd15a1e2c6aa67f5cfe7279d531
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-07 08:24:29 +0200

    Do always verification search if previous position was check. Bench: 5047647

--- src/search.cpp
+++ src/search.cpp
@@ -799,7 +799,7 @@ namespace {
     // Step 8. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && ((ss-1)->statScore < 23767 || (ss-1)->inCheck)
+        && (ss-1)->statScore < 23767
         &&  eval >= beta
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 20 * depth - 22 * improving + 168 * ss->ttPv + 159
@@ -827,7 +827,7 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
+            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && !(ss-1)->inCheck))
                 return nullValue;
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
