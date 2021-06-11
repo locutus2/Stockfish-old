ab86931b2779062cd7b64e29d0b0354ec49558e0
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-11 20:17:03 +0200

    More reduction if r - rBase > 5. Bench: 5328237

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop: // When in check, search starts from here
                   r -= ss->statScore / 14721;
           }
 
-          if(r - rBase > 6)
+          if(r - rBase > 5)
               r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if
