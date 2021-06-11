fb1413318451ada8dcf4218b59631cd1eda7fb70
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-11 19:36:37 +0200

    Inspired by VoyagerOnes reduction experiments try something in the opposite direction. Even more reduction if additional reduction is already high. Bench: 5589905

--- src/search.cpp
+++ src/search.cpp
@@ -1130,7 +1130,7 @@ moves_loop: // When in check, search starts from here
               || !ss->ttPv)
           && (!PvNode || ss->ply > 1 || thisThread->id() % 4 != 3))
       {
-          Depth r = reduction(improving, depth, moveCount);
+          Depth rBase = reduction(improving, depth, moveCount), r = rBase;
 
           if (PvNode)
               r--;
@@ -1179,6 +1179,9 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / 14721;
           }
+
+          if(r - rBase > 6)
+              r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
