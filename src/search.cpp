fd55af1f4647eb5657e9a0c6612c76d52375a2da
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-14 13:54:23 +0200

    Try again using stat score for captures but increase stat reduction divisor by 50%. Bench: 4789515

--- src/search.cpp
+++ src/search.cpp
@@ -1175,10 +1175,11 @@ moves_loop: // When in check, search starts from here
 
           ss->statScore =  (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
-                         + (*contHist[3])[movedPiece][to_sq(move)];
+                         + (*contHist[3])[movedPiece][to_sq(move)]
+                         - 4923;
 
           if (captureOrPromotion)
-              ss->statScore += thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 10755;
+              ss->statScore += thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())];
 
           else
           {
@@ -1186,12 +1187,12 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
-              ss->statScore += thisThread->mainHistory[us][from_to(move)] - 4923;
+              ss->statScore += thisThread->mainHistory[us][from_to(move)];
           }
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           if (!ss->inCheck)
-              r -= ss->statScore / 14721;
+              r -= ss->statScore / (captureOrPromotion ? 22082 : 14721);
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
