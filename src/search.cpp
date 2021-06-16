8000f83a610e98056ae586bffd57b3c207fdcd70
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 19:02:04 +0200

    More reduction if root move is not on PV. Bench: 5007598

--- src/search.cpp
+++ src/search.cpp
@@ -1146,6 +1146,9 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 
+	  else if (!thisThread->rootPvMove)
+              r++;
+
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
