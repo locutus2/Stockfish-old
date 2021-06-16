88af8ba1cf0d6cdfc809e55df41beb8c6d176327
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 19:08:43 +0200

    More reduction for non-captures if root move is not on PV. Bench: 4855404

--- src/search.cpp
+++ src/search.cpp
@@ -1146,9 +1146,6 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 
-	  else if (!thisThread->rootPvMove)
-              r++;
-
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
@@ -1179,6 +1176,9 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
+              if (!PvNode && !thisThread->rootPvMove)
+                  r++;
+
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
