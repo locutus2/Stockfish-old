d5a1aa0a0bbbe46873f07e95e7762427e5c584e7
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 21:44:26 +0200

    More reduction for non-captures if root move is not on PV. Allow now also more reduction at root. Bench: 4309282

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
-              if (!PvNode && !thisThread->rootPvMove && !givesCheck)
+              if (!thisThread->rootPvMove)
                   r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
