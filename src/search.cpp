f527dfbb6621678eacbfd977a668c6ae3ee0d225
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 21:24:28 +0200

    More reduction for non-checking non-captures if root move is not on PV. Bench: 4786391

--- src/search.cpp
+++ src/search.cpp
@@ -1180,7 +1180,7 @@ moves_loop: // When in check, search starts from here
               if (ttCapture)
                   r++;
 
-              if (!PvNode && !thisThread->rootPvMove && (pos.key() & 1))
+              if (!PvNode && !thisThread->rootPvMove && !givesCheck)
                   r++;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
