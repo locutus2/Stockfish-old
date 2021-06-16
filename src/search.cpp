cfb7e9a661c077a628e11e4365323745170cb8b7
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 19:39:50 +0200

    Pandomly (p=50%) do more reduction if root move is not on PV. Bench: 4527075

--- src/search.cpp
+++ src/search.cpp
@@ -1146,6 +1146,9 @@ moves_loop: // When in check, search starts from here
           if (PvNode)
               r--;
 
+          else if (!thisThread->rootPvMove && (thisThread->nodes & 1))
+              r++;
+
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
               r--;
@@ -1190,9 +1193,6 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / 14721;
           }
-          else if (!PvNode && !thisThread->rootPvMove)
-              r++;
-
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
