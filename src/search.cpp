f1b5f71427fefd68339626e3dcd325d0b470e429
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-16 19:10:16 +0200

    More reduction for captures if root move is not on PV. Bench: 5173912

--- src/search.cpp
+++ src/search.cpp
@@ -1176,9 +1176,6 @@ moves_loop: // When in check, search starts from here
 
           if (!captureOrPromotion)
           {
-              if (!PvNode && !thisThread->rootPvMove)
-                  r++;
-
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
                   r++;
@@ -1193,6 +1190,9 @@ moves_loop: // When in check, search starts from here
               if (!ss->inCheck)
                   r -= ss->statScore / 14721;
           }
+          else if (!PvNode && !thisThread->rootPvMove)
+              r++;
+
 
           // In general we want to cap the LMR depth search at newDepth. But if
           // reductions are really negative and movecount is low, we allow this move
