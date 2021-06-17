b62365a13aa6d262ee080b630a52b58731b0283c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-17 13:41:28 +0200

    Try opposite: Double extension only if root move is not on PV. Bench: 4379396

--- src/search.cpp
+++ src/search.cpp
@@ -1082,7 +1082,7 @@ moves_loop: // When in check, search starts from here
               if (   !PvNode
                   && value < singularBeta - 93
                   && ss->doubleExtensions < 3
-                  && thisThread->rootPvMove)
+                  && !thisThread->rootPvMove)
               {
                   extension = 2;
                   doubleExtension = true;
