6823de7e1e2a601fc8bb57bd14da30ec74465f6c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-18 22:27:19 +0200

    Do more low best move changes reduction if root move not on PV. Bench: 4962868

--- src/search.cpp
+++ src/search.cpp
@@ -1159,7 +1159,7 @@ moves_loop: // When in check, search starts from here
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if (   (rootNode || !PvNode)
               && thisThread->bestMoveChanges <= 2)
-              r += 1 + thisThread->rootPvMove;
+              r += 2 - thisThread->rootPvMove;
 
           // Decrease reduction if opponent's move count is high (~1 Elo)
           if ((ss-1)->moveCount > 13)
