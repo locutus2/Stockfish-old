1773a93cde9dae11799e338d31a8a4be575b087d
candirufish <38038147+candirufish@users.noreply.github.com>
2021-06-27 23:14:11 +0200

    no clmr for km no cap 2

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r++;
+              r += 2;
 
           if (!captureOrPromotion)
           {
