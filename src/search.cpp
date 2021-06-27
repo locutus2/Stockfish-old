25ef923b4438c88631a8d9c50af9836b1aad9ab9
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-27 21:54:42 +0200

    Higher cmh[0] threshold if double extension. Bench: 4982430

--- src/search.cpp
+++ src/search.cpp
@@ -1017,7 +1017,7 @@ moves_loop: // When in check, search starts from here
           {
               // Continuation history based pruning (~20 Elo)
               if (   lmrDepth < 5
-                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold - 800 * improving
+                  && (*contHist[0])[movedPiece][to_sq(move)] < CounterMovePruneThreshold + 1000 * doubleExtension
                   && (*contHist[1])[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
                   continue;
 
