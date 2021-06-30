8038259804ab2855487ead6a88ff915a30883e8a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-06-30 14:47:22 +0200

    Less reduction at cut node if capture or in check or not ttCapture. Bench: 5071640

--- src/search.cpp
+++ src/search.cpp
@@ -1158,7 +1158,7 @@ moves_loop: // When in check, search starts from here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode)
-              r += 1 - captureOrPromotion + ttCapture;
+              r += 2 - captureOrPromotion - (captureOrPromotion || ss->inCheck || !ttCapture);
 
           if (!captureOrPromotion)
           {
