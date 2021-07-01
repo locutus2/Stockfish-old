d275a25420a5a10b261f6541ae95202e84738d33
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-01 15:09:54 +0200

    Retest on top of passed cklmr5b patch. Less reductions at cut nodes if major piece or king move gives check (version 2). Bench: 5315578

--- src/search.cpp
+++ src/search.cpp
@@ -1157,8 +1157,8 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += 2 - (givesCheck && type_of(movedPiece) >= ROOK);
+          if (cutNode)
+              r += 2 - 2 * (move == ss->killers[0]) - (givesCheck && type_of(movedPiece) >= ROOK);
 
           if (!captureOrPromotion)
           {
