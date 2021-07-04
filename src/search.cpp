aec4b2f1e6d4ea3ecf2d2beac314bebd219a9607
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-04 19:17:36 +0200

    Reduce killer at cut nodes if singularQuietLMR. Bench: 4951824

--- src/search.cpp
+++ src/search.cpp
@@ -1157,8 +1157,8 @@ moves_loop: // When in check, search starts from here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0])
-              r += 2 - (moveCount < 4);
+          if (cutNode && (move != ss->killers[0] || singularQuietLMR))
+              r += 2;
 
           if (!captureOrPromotion)
           {
