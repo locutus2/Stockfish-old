67f70663416bd603b0912852b328aa0c20d6617e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-09 13:33:08 +0200

    Less reduction for second move at cut nodes if depth >= 10. Bench: 5166608

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts from here
           if (cutNode && move != ss->killers[0])
               r += 2;
 
-          if (depth >= 10 && cutNode && !likelyFailLow && moveCount <= 2)
+          if (depth >= 10 && cutNode && moveCount <= 2)
               r--;
 
           if (!captureOrPromotion)
