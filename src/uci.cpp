113a31ac5d22f090e5b36852cee7c8a314ce4e4c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-11 16:05:18 +0200

    comment

--- src/uci.cpp
+++ src/uci.cpp
@@ -471,6 +471,63 @@ c34  bool(pos.count<ALL_PIECES>() & 16)
      *  BEST it=19100 hit=0.275892 support=0.0108136 T=3.78303e-43 msteps=1 => c0*!c2*!c4*!c7*!c11*!c12*!c19*!c20*c21*!c22*!c23*!c24*!c25*!c29*!c30
      *  BEST it=19600 hit=0.369648 support=0.00996203 T=3.08589e-44 msteps=1 => c0*c1*!c6*!c12*c13:wq
      * */
+     /*   SA55_1_w
+     *  c0   cutNode,
+     *  c1   PvNode || cutNode,  // PvNode = 00, cutNode = 01, allNode = 10
+     *  c2   captureOrPromotion,
+     *  c3   givesCheck,
+     *  c4   ss->inCheck,
+     *  c5   improving,
+     *  c6   likelyFailLow,
+     *  c7   ttCapture,
+     *  c8   more_than_one(pos.checkers()),
+     *  c9   doubleExtension,
+     *  c19  moveCountPruning, // CHANGED
+     *  c11  singularQuietLMR,
+     *  c12  ss->ttPv && !PvNode,
+     *  c13  move == ss->killers[0],
+     *  c14  move == ss->killers[1],
+     *  c15  move == countermove,
+     *  c16  bool(int(depth) & 1),
+     *  c17  bool(int(depth) & 2),
+     *  c18  bool(int(depth) & 4),
+     *  c19  bool(int(depth) & 8),
+     *  c20  bool(moveCount & 1),
+     *  c21  bool(moveCount & 2),
+     *  c22  bool(moveCount & 4),
+     *  c23  bool(moveCount & 8),
+     *  c24  bool(moveCount & 16),
+     *  c25  bool(moveCount & 32),
+     *  c26  bool(moveCount & 64),
+     *  c27  bool(type_of(movedPiece) & 1),
+     *  c28  bool(type_of(movedPiece) & 2),
+     *  c29  bool(type_of(movedPiece) & 4),
+     *  c30  bool(pos.count<ALL_PIECES>() & 1),
+     *  c31  bool(pos.count<ALL_PIECES>() & 2),
+     *  c32  bool(pos.count<ALL_PIECES>() & 4),
+     *  c33  bool(pos.count<ALL_PIECES>() & 8),
+     *  c34  bool(pos.count<ALL_PIECES>() & 16),
+     *  c35  bool(extension)
+        c36  ss->ttHit,
+        c37  bool(ttMove),
+        c38  bool(bestMove),
+        c39  eval >= beta,
+        c40  ss->staticEval >= beta,
+        c41  (ss-1)->inCheck,
+        c42  type_of(move) == PROMOTION,
+        c43  bool((ss-1)->moveCount & 1),
+        c44  bool((ss-1)->moveCount & 2),
+        c45  bool((ss-1)->moveCount & 4),
+        c46  bool((ss-1)->moveCount & 8),
+        c47  bool((ss-1)->moveCount & 16),
+        c48  bool((ss-1)->moveCount & 32),
+        c49  thisThread->mainHistory[us][from_to(move)] > 0,
+        c50  (*contHist[0])[movedPiece][to_sq(move)] > 0,
+        c51  (*contHist[1])[movedPiece][to_sq(move)] > 0,
+        c52  (*contHist[3])[movedPiece][to_sq(move)] > 0,
+        c53  (*contHist[5])[movedPiece][to_sq(move)] > 0,
+        c54  thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] > 0
+		    */
     FUNC best, tmp;
     double bestVal = 0;
     double curVal = 0;
