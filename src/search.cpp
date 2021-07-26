1c0a4030f99406d856fc10f1902a755dd98b8a37
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-26 18:43:23 +0200

    Replace for capture/promotion in stat score calculation main with capture history (but offset by -4000 to maintain same average). Bench: 5307705

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts from here
           if (ttCapture)
               r++;
 
-          ss->statScore =  (captureOrPromotion ? thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] * 5 / 4
+          ss->statScore =  (captureOrPromotion ? thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 4000
                                                : thisThread->mainHistory[us][from_to(move)])
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
