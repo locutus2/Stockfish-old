86ef678d59a21c84109c1bdb3ab51f7bdf6402f9
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-07-28 11:44:36 +0200

    Replace stat score offset with -5430 * cutNode - 3032. Bench: 5582574

--- src/search.cpp
+++ src/search.cpp
@@ -1167,7 +1167,8 @@ moves_loop: // When in check, search starts from here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
-                         - 4923;
+                         - 5430 * cutNode
+                         - 3032;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
