e1653f13edd200d6bcbf0f5c55d9f138c0af065a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-04 19:54:42 +0200

    Use lmr depth < 6 with new thresholds. Test against my passed cmhp patch. Bench: 5303941

--- src/search.cpp
+++ src/search.cpp
@@ -1016,9 +1016,9 @@ moves_loop: // When in check, search starts here
           else
           {
               // Continuation history based pruning (~20 Elo)
-              if (   lmrDepth < 5
-                  && (*contHist[0])[movedPiece][to_sq(move)] < 25 - 25 * depth * depth
-                  && (*contHist[1])[movedPiece][to_sq(move)] < 25 - 25 * depth * depth)
+              if (   lmrDepth < 6
+                  && (*contHist[0])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth
+                  && (*contHist[1])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
