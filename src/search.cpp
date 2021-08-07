d8dd57c41358ec9ea53fc44001d6abdbfc36d1fe
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-07 19:18:50 +0200

    Use different thresholds. cmh0 = 27 - 27 * depth * depth, cmh1 = 18 - 18 * depth * depth. Bench: 4908143

--- src/search.cpp
+++ src/search.cpp
@@ -1019,8 +1019,8 @@ moves_loop: // When in check, search starts here
           {
               // Continuation history based pruning (~20 Elo)
               if (   lmrDepth < 5
-                  && (*contHist[0])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth
-                  && (*contHist[1])[movedPiece][to_sq(move)] < 23 - 23 * depth * depth)
+                  && (*contHist[0])[movedPiece][to_sq(move)] < 27 - 27 * depth * depth
+                  && (*contHist[1])[movedPiece][to_sq(move)] < 18 - 18 * depth * depth)
                   continue;
 
               // Futility pruning: parent node (~5 Elo)
