79a88d8e25645a506982478b980e66e0c03f7ce4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-20 19:09:08 +0200

    Add small random value [-3,-1,1,3] to eval. Test against master with PR 3666 and 3667. Bench: 5427368

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1115,7 +1115,7 @@ Value Eval::evaluate(const Position& pos) {
   // Damp down the evaluation linearly when shuffling
   v = v * (100 - pos.rule50_count()) / 100;
 
-  v += 2 * (pos.this_thread()->nodes & 1) - 1;
+  v += 2 * (pos.this_thread()->nodes & 3) - 3;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
