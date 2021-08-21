235851cb7fbaff55d2e0668a2c1093592eeca30e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-21 14:44:41 +0200

    Randomly (p=1/8) do less reduction at cut nodes. Test against master with PR 3666 and 3667. Bench: 5307799

--- src/search.cpp
+++ src/search.cpp
@@ -1156,7 +1156,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 1 + bool(thisThread->nodes & 0xF);
+              r += 1 + bool(thisThread->nodes & 7);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
