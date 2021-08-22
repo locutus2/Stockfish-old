305c3fea76553487e5e794fc3a9c3b1436892443
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-22 17:20:22 +0200

    Retest with new default book (old LTC failed vellow with 93k games). If capture/promotion add capture history - 682 to stat score. Bench: 5348680

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,9 @@ moves_loop: // When in check, search starts here
                          + (*contHist[3])[movedPiece][to_sq(move)]
                          - 4923;
 
+          if (captureOrPromotion)
+              ss->statScore += thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())] - 682;
+
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
