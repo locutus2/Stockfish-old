6fc8d80fecb18e913be1b78a7a47850c5f336e2c
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-08-22 21:16:03 +0200

    Retest with new default book (old LTC failed yellow with 115k games). Stat score offset: - 4973 + 870 * ss->inCheck (offset is modified to reflect the change of master inbetween). Bench: 5166635

--- src/search.cpp
+++ src/search.cpp
@@ -1166,7 +1166,7 @@ moves_loop: // When in check, search starts here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
-                         - 4923;
+                         - 4973 + 870 * ss->inCheck;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
