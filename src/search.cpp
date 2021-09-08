3b4b451d5ae6acf48d1489d13a216807df2169e5
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-08 17:31:58 +0200

    Update also main history after LMR triggered full search. Bench: 6683759

--- src/search.cpp
+++ src/search.cpp
@@ -1208,6 +1208,7 @@ moves_loop: // When in check, search starts here
               int bonus = value > alpha ?  stat_bonus(newDepth)
                                         : -stat_bonus(newDepth);
 
+              thisThread->mainHistory[us][from_to(move)] << bonus;
               update_continuation_histories(ss, movedPiece, to_sq(move), bonus);
           }
       }
