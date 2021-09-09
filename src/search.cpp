9967ed5fa81594a21b34bcc32183d3cc5a3c1a52
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-09 21:00:23 +0200

    Do capture extension at PV and cut nodes only if previous move count > 1. Bench: 5414611

--- src/search.cpp
+++ src/search.cpp
@@ -1096,9 +1096,10 @@ moves_loop: // When in check, search starts here
       }
 
       // Capture extensions for PvNodes and cutNodes
-      else if (   (cutNode || (PvNode && !givesCheck))
+      else if (   (PvNode || cutNode)
                && captureOrPromotion 
-               && moveCount != 1)
+               && moveCount != 1
+               && (ss-1)->moveCount > 1)
           extension = 1;
 
       // Check extensions
