0dcb1e2c7848e1d82c17378098cad5db63563ef0
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-16 11:42:25 +0200

    Even less reduction at PV node if no best move found at odd plies. Bench: 6527581

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,7 @@ moves_loop: // When in check, search starts here
           Depth r = reduction(improving, depth, moveCount);
 
           if (PvNode)
-              r -= 1 + !(bestMove || (ss->ply & 1));
+              r -= 1 + (!bestMove && (ss->ply & 1));
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
