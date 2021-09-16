4819b37b9bd135e91855f26cdb7cf3289b8bb206
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-16 19:22:42 +0200

    Less cut node reduction if at ttPv node. Bench: 7256024

--- src/search.cpp
+++ src/search.cpp
@@ -1169,8 +1169,8 @@ moves_loop: // When in check, search starts here
               r--;
 
           // Increase reduction for cut nodes (~3 Elo)
-          if (cutNode && move != ss->killers[0] && !ss->ttPv)
-              r += 2;
+          if (cutNode && move != ss->killers[0])
+              r += 2 - ss->ttPv;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
