2254e1d1981fc1a0b55c18233da89c32fc541465
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-20 17:06:05 +0200

    At depth < 10 randomly (p=1/8) do less reduction for non-promotion pawn pushes. Test against Vizs passed ptwComboPR. Bench: 6255341

--- src/search.cpp
+++ src/search.cpp
@@ -1142,7 +1142,7 @@ moves_loop: // When in check, search starts here
       {
           Depth r = reduction(improving, depth, moveCount);
 
-          if (PvNode && !captureOrPromotion && type_of(movedPiece) == PAWN && !(thisThread->nodes & 7))
+          if (depth < 10 && !captureOrPromotion && type_of(movedPiece) == PAWN && !(thisThread->nodes & 7))
               r--;
 
           if (PvNode)
