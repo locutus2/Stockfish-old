be0ef957b233462aa546c7eb465085f062cf8127
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-21 18:34:24 +0200

    At expected cut nodes do more reduction for non-captures if ttCapture. Test against Vizs passed ptwComboPR. Bench: 5546634

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r += 1 + (cutNode && captureOrPromotion);
+              r += 1 + (cutNode && !captureOrPromotion);
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
