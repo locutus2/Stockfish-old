c322d1a4e27d1b2fcb59b0072e941d712e4c7445
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-23 20:42:07 +0200

    Collect total number of non first moves until the current position. Do capture history pruning only if non first moves count < 18. Bench: 5849531

--- src/search.cpp
+++ src/search.cpp
@@ -1014,7 +1014,7 @@ moves_loop: // When in check, search starts here
               // Capture history based pruning when the move doesn't give check
               if (   !givesCheck
                   && lmrDepth < 1
-                  && ss->nonFirstMoves < 14
+                  && ss->nonFirstMoves < 18
                   && captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 0)
                   continue;
 
