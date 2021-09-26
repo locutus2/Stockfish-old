4306165196466597bbf345233bae02deee86120a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-26 19:02:58 +0200

    Less reduction at cur nodes if in check. Bench: 5911164

--- src/search.cpp
+++ src/search.cpp
@@ -1221,7 +1221,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if (ss->inCheck && (cutNode || captureOrPromotion))
+          if (ss->inCheck && cutNode)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
