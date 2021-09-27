293833a7ef1c007fae89064bc4514ca9fd6fb29d
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-27 20:48:48 +0200

    Less reduction at cut nodes if linear decision function (10 factors) >= 5. Bench: 6058279

--- src/search.cpp
+++ src/search.cpp
@@ -1230,6 +1230,20 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
+          if (   cutNode
+              &&  4 * (move == ss->killers[0])
+                + 3 * (move == countermove)
+                + 2 * givesCheck
+                + 2 * ((ss-1)->currentMove == MOVE_NULL)
+                +     captureOrPromotion
+                +     (eval >= beta)
+                - 2 * (type_of(move) == PROMOTION)
+                -     bool(ttMove)
+                -     moveCountPruning
+                -     noLMRExtension
+                >= 5)
+              r--;
+
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions if
