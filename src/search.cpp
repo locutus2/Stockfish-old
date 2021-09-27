1c218607823093280a8fb3a9d6c5bf2d038ffed4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-27 13:09:53 +0200

    Less reduction for checking moves as ALL nodes if no tt hit. Bench: 5700659

--- src/search.cpp
+++ src/search.cpp
@@ -1221,7 +1221,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if (!PvNode && !cutNode && givesCheck)
+          if (!PvNode && !cutNode && givesCheck && !ss->ttHit)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
