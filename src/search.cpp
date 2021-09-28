20b45a6cc7449d0cf0901d779b45378301137808
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-09-28 19:18:46 +0200

    cap hist

--- src/search.cpp
+++ src/search.cpp
@@ -1315,7 +1315,7 @@ moves_loop: // When in check, search starts here
       //if(CC) 
 	     C = {
 			      PARAM(thisThread->captureHistory[movedPiece][to_sq(move)][type_of(pos.captured_piece())]) /*21*/,
-		      PARAM(depth) /*21*/,
+	//	      PARAM(depth) /*21*/,
 		     PARAM(cutNode) /*0*/, 
 	                      PARAM(PvNode) /*1*/, 
 			      PARAM(ss->inCheck) /*2*/, 
@@ -1445,7 +1445,7 @@ moves_loop: // When in check, search starts here
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
-          CC = captureOrPromotion;
+          CC = captureOrPromotion && (thisThread->nodes & 1);
 
           // Range reductions (~3 Elo)
           if (ss->staticEval - value < 30 && depth > 7)
