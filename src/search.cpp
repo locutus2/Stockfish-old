07edb6fa3de729d8a785f513c40ba56c89b0ffa9
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-05 07:32:20 +0200

    Tweak reintroduced thread reduction term: 0.6 * log(#th). Bench: 5714575

--- src/search.cpp
+++ src/search.cpp
@@ -173,7 +173,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((21.9 + std::log((Threads.size() + 1) / 2.0)) * std::log(i));
+      Reductions[i] = int((21.9 + 0.6 * std::log(Threads.size())) * std::log(i));
 }
 
 
