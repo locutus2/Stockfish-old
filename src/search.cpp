50c83c48c7575d84cd3a2d5198c969a182b1c454
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-06 16:42:03 +0200

    Increase cap in new stat formula upto depth 9. Bench: 5346575

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((5 * d + 202) * d - 187 , 2000);
+    return std::min((6 * d + 229) * d - 215 , 2330);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
