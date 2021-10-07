9298ffe64ab7213a5ee18d432091634eb6a4e023
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-07 13:25:14 +0200

    Increase stat formula so that cap is (nearly) reached at depth 7. Bench: 5691489

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((6 * d + 229) * d - 215 , 1231);
+    return std::min((7 * d + 272) * d - 256 , 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
