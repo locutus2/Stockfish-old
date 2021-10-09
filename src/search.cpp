81f539df864ca289466c05d93256da5322d620da
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-09 23:09:10 +0200

    At PV nodes do a full PV search if moveCount == 1 or if no best move and in check at root was found. Bench: 6083472

--- src/search.cpp
+++ src/search.cpp
@@ -1255,7 +1255,7 @@ moves_loop: // When in check, search starts here
       }
       else
       {
-          doFullDepthSearch = !PvNode || (moveCount > 1 && (!rootNode || bestMove || move != ss->killers[1]));
+          doFullDepthSearch = !PvNode || (moveCount > 1 && (!rootNode || bestMove || !ss->inCheck));
           didLMR = false;
       }
 
@@ -1277,7 +1277,7 @@ moves_loop: // When in check, search starts here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (rootNode && !bestMove && move == ss->killers[1]) || (value > alpha && (rootNode || value < beta))))
+      if (PvNode && (moveCount == 1 || (rootNode && !bestMove && ss->inCheck) || (value > alpha && (rootNode || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
