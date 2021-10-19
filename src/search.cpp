5e81de543fda2c5aad97138fdd0a1389a82db62b
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-19 21:05:05 +0200

    Simplify deeper by removing r>= -1 case. Test against Bms passed Test116. Bench: 7049024

--- src/search.cpp
+++ src/search.cpp
@@ -1214,8 +1214,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1                   ? 0
-                       : moveCount <= 3 && r <= -3 ? 2
+          int deeper =   moveCount <= 3 && r <= -3 ? 2
                        : moveCount <= 5            ? 1
                        : PvNode && depth > 6       ? 1
                        :                             0;
