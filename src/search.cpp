bd02d1fd78f89a1fab47f63342931d408de10e0a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-20 22:25:36 +0200

    Use deeper = 0 for captures/promotions. Bench: 6368710

--- src/search.cpp
+++ src/search.cpp
@@ -1211,10 +1211,10 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
+                       : captureOrPromotion        ? 0
                        : moveCount <= 3 && r <= -3 ? 2
                        : moveCount <= 5            ? 1
                        : PvNode && depth > 6       ? 1
-                       : captureOrPromotion        ? 1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
