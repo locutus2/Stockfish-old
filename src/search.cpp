8f050d39393883d064a5d53665b5a7bbce55f473
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-21 17:05:24 +0200

    Remove moveCount <= 3. Bench: 6479239

--- src/search.cpp
+++ src/search.cpp
@@ -1211,7 +1211,6 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1             ? 0
-                       : moveCount <= 4      ? 2
                        : moveCount <= 5      ? 1
                        : PvNode && depth > 6 ? 1
                        :                       0;
