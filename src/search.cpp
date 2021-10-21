5b15343a730fbf189a5df9d023ff1de0f9d90708
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-21 16:50:15 +0200

    Use moveCount <= 2 instead of <= 3. Bench: 5838886

--- src/search.cpp
+++ src/search.cpp
@@ -1211,8 +1211,8 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1             ? 0
-                       : moveCount <= 3      ? 2
-                       : moveCount <= 4      ? 1
+                       : moveCount <= 2      ? 2
+                       : moveCount <= 5      ? 1
                        : PvNode && depth > 6 ? 1
                        :                       0;
 
