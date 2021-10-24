6f5a2c4fec3cf9de8fd0cc213a67b3c636464260
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-24 16:46:21 +0200

    Less reduction (one ply for each 4 pieces (excluding pawns) traded since the root position) at non-PV nodes if prior capture. Bench: 6487712

--- src/search.cpp
+++ src/search.cpp
@@ -294,7 +294,7 @@ void Thread::search() {
 
   ss->pv = pv;
 
-  rootPieceCount = rootPos.count<ALL_PIECES>();
+  rootPieceCount = rootPos.count<ALL_PIECES>() - rootPos.count<PAWN>();
   bestValue = delta = alpha = -VALUE_INFINITE;
   beta = VALUE_INFINITE;
 
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
 
           if (   !PvNode
               && priorCapture)
-              r -= (thisThread->rootPieceCount - pos.count<ALL_PIECES>()) / 4;
+              r -= (thisThread->rootPieceCount - pos.count<ALL_PIECES>() + pos.count<PAWN>()) / 4;
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
