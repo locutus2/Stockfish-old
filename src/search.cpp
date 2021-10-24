df6195e4edcd3532e4ba148271fb7fa7e16af8a4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-24 16:18:19 +0200

    Less reduction at non-PV nodes if prior capture. Bench: 7418288

--- src/search.cpp
+++ src/search.cpp
@@ -1171,6 +1171,10 @@ moves_loop: // When in check, search starts here
               && bestMoveCount <= 3)
               r--;
 
+          if (   !PvNode
+              && priorCapture)
+              r--;
+
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
