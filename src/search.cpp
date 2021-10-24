330d4883958f4374fccb27a42dadb978873e7225
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-24 22:14:32 +0200

    Two plies more reduction if in LMR research. Bench: 6692228

--- src/search.cpp
+++ src/search.cpp
@@ -816,7 +816,6 @@ namespace {
     if (   !PvNode
         &&  depth < 9
         &&  eval - futility_margin(depth, improving) >= beta
-        && !ss->LMRresearch
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
 
@@ -1198,6 +1197,9 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
+
+          if (ss->LMRresearch)
+              r += 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
