caaef54ce05491199f246806ce3c733b41f2270d
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-24 18:47:25 +0200

    Do full depth research at PV nodes only if d < newDepth - 1. Bench: 7535505

--- src/search.cpp
+++ src/search.cpp
@@ -1225,7 +1225,7 @@ moves_loop: // When in check, search starts here
               rangeReduction++;
 
           // If the son is reduced and fails high it will be re-searched at full depth
-          doFullDepthSearch = value > alpha && d < newDepth - 1;
+          doFullDepthSearch = value > alpha && d < newDepth - PvNode;
           didLMR = true;
       }
       else
