202f797f7743e1a155a09fd8e8e4489725c59d98
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-27 20:06:02 +0200

    Use deeper = 1 if improving. Bench: 6725503

--- src/search.cpp
+++ src/search.cpp
@@ -1210,9 +1210,9 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1             ? 0
-                       : cutNode && moveCount == 5 ? 1
                        : moveCount <= 5      ? 2
                        : PvNode && depth > 6 ? 1
+                       : improving           ? 1
                        :                       0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
