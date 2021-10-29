d1b95a2428e10547ad34c555c9aca1cb6c1f8226
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-29 02:47:02 +0200

    Use a exponetial average for root score with a high weight of 63/64 for current score. Bench: 5297142

--- src/search.cpp
+++ src/search.cpp
@@ -1283,7 +1283,7 @@ moves_loop: // When in check, search starts here
 
           //rm.score = rm.updates ? (value * depth + rm.score * rm.updates) / (rm.updates + depth) : value;
           //rm.updates += depth;
-          rm.score = rm.score != -VALUE_INFINITE ? (31 * value + rm.score) / 32 : value;
+          rm.score = rm.score != -VALUE_INFINITE ? (63 * value + rm.score) / 64 : value;
           value = rm.score;
 
           // PV move or new best move?
