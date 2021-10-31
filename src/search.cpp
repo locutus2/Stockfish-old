ce08a86cc73b3b8c3b5331dec8c996376791104f
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-31 14:34:46 +0100

    If deeper = 2 reduce by the extension. Bench: 7002671

--- src/search.cpp
+++ src/search.cpp
@@ -1210,10 +1210,9 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : moveCount <= 5            ? 2
+                       : moveCount <= 5            ? 2 - extension
                        : PvNode && depth > 6       ? 1
                        : cutNode && moveCount <= 7 ? 1
-                       : cutNode && !improving     ? 1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
