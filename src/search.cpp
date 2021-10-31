886b76dd8e11ba987ee2108cdbdc74a3435ac415
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-31 15:33:27 +0100

    Reduce deeper by extension (minimum zero). Bench: 6363095

--- src/search.cpp
+++ src/search.cpp
@@ -1210,11 +1210,11 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : extension                 ? 0
                        : moveCount <= 5            ? 2
                        : PvNode && depth > 6       ? 1
                        : cutNode && moveCount <= 7 ? 1
                        :                             0;
+          deeper = std::max(deeper - extension, 0);
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
 
