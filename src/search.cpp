27569665ffb7cbc1d1ae6d7af193ac774f92772e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-10-31 13:45:05 +0100

    Use deeper = 1 at cut nodes also for moveCount <= 9. Test against passed deeper5_PR. Bench: 7249829

--- src/search.cpp
+++ src/search.cpp
@@ -1210,9 +1210,9 @@ moves_loop: // When in check, search starts here
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
           int deeper =   r >= -1                   ? 0
-                       : moveCount <= 5            ? 2 - (!PvNode && !cutNode && moveCount >= 5)
+                       : moveCount <= 5            ? 2
                        : PvNode && depth > 6       ? 1
-                       : cutNode && moveCount <= 7 ? 1
+                       : cutNode && moveCount <= 9 ? 1
                        :                             0;
 
           Depth d = std::clamp(newDepth - r, 1, newDepth + deeper);
