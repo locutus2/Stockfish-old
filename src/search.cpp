c851e28823b9d03e409d1d70d3db1f2b275e1c24
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-01 02:30:35 +0100

    At root use depth-weighted average score if value > alpha. Also do an additional aging of old values. Bench: 5917206

--- src/search.cpp
+++ src/search.cpp
@@ -1277,7 +1277,7 @@ moves_loop: // When in check, search starts here
           if (value > alpha)
           {
               rm.averageScore = value = (depth * value + rm.scoreWeight * rm.averageScore) / (depth + rm.scoreWeight);
-              rm.scoreWeight = (rm.scoreWeight + depth) / 2;
+              rm.scoreWeight = rm.scoreWeight / 2 + depth;
           }
 
           // PV move or new best move?
