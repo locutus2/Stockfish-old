ee146df642533c28ee14838fbf67567cd49ce6fe
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-04 20:11:15 +0100

    Use average instead of previous score as center of aspiration window but also decrease delta to 13. Test against Vizs passed lowDeltaRedPR. Bench: 6378405

--- src/search.cpp
+++ src/search.cpp
@@ -376,8 +376,8 @@ void Thread::search() {
           // Reset aspiration window starting size
           if (rootDepth >= 4)
           {
-              Value prev = (rootMoves[pvIdx].averageScore + rootMoves[pvIdx].previousScore) / 2;
-              delta = Value(17) + int(prev) * prev / 16384;
+              Value prev = rootMoves[pvIdx].averageScore;
+              delta = Value(13) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
