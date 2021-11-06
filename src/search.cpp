ae00b1942b068401f4c6875280bd06824cd6d1a6
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-06 01:22:15 +0100

    Use for even root depth previous score. Bench: 6725998

--- src/search.cpp
+++ src/search.cpp
@@ -376,8 +376,8 @@ void Thread::search() {
           // Reset aspiration window starting size
           if (rootDepth >= 4)
           {
-              Value prev = rootDepth & 1 ? rootMoves[pvIdx].previousScore
-                                         : rootMoves[pvIdx].averageScore;
+              Value prev = rootDepth & 1 ? rootMoves[pvIdx].averageScore
+                                         : rootMoves[pvIdx].previousScore;
               delta = Value(17) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
               beta  = std::min(prev + delta, VALUE_INFINITE);
