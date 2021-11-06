18c7a5d69614480f839c6998f91262ab715e9a38
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-06 08:47:15 +0100

    Do at each second thread for first two moves at root node PV searches. Bench: 6719976

--- src/search.cpp
+++ src/search.cpp
@@ -1232,7 +1232,7 @@ moves_loop: // When in check, search starts here
       }
       else
       {
-          doFullDepthSearch = !PvNode || moveCount > 1;
+          doFullDepthSearch = !PvNode || moveCount > 1 + (rootNode && (thisThread->id() & 1));
           didLMR = false;
       }
 
@@ -1254,7 +1254,7 @@ moves_loop: // When in check, search starts here
       // For PV nodes only, do a full PV search on the first move or after a fail
       // high (in the latter case search only if value < beta), otherwise let the
       // parent node fail low with value <= alpha and try another move.
-      if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+      if (PvNode && (moveCount <= 1 + (rootNode && (thisThread->id() & 1))|| (value > alpha && (rootNode || value < beta))))
       {
           (ss+1)->pv = pv;
           (ss+1)->pv[0] = MOVE_NONE;
