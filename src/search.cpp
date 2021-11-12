f20100da4e307ee39723c9240056d4709d8a64d4
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-12 19:18:55 +0100

    Use depth weighted average of average and previous score for window. Take 9 (use ratio 7:3). Bench: 6924824

--- src/search.cpp
+++ src/search.cpp
@@ -376,8 +376,8 @@ void Thread::search() {
           // Reset aspiration window starting size
           if (rootDepth >= 4)
           {
-              Value prev = (  rootMoves[pvIdx].averageScore  * (13 * rootDepth + 12)
-                            + rootMoves[pvIdx].previousScore * ( 3 * rootDepth - 12)) / (16 * rootDepth) ;
+              Value prev = (  rootMoves[pvIdx].averageScore  * (7 * rootDepth + 12)
+                            + rootMoves[pvIdx].previousScore * (3 * rootDepth - 12)) / (10 * rootDepth) ;
               delta = Value(17) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
               beta  = std::min(prev + delta, VALUE_INFINITE);
