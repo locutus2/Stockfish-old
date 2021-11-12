800f1715117bcaa18dbd56346c9d582e51c638e9
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-12 21:18:53 +0100

    Use maximum of average and previous score as center of window. Bench: 6700664

--- src/search.cpp
+++ src/search.cpp
@@ -376,8 +376,7 @@ void Thread::search() {
           // Reset aspiration window starting size
           if (rootDepth >= 4)
           {
-              Value prev = std::min(rootMoves[pvIdx].averageScore,
-                                    (rootMoves[pvIdx].averageScore + rootMoves[pvIdx].previousScore) / 2);
+              Value prev = std::max(rootMoves[pvIdx].averageScore, rootMoves[pvIdx].previousScore);
               delta = Value(17) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
               beta  = std::min(prev + delta, VALUE_INFINITE);
