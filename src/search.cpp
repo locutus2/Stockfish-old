39f62e298065fd2881a86dbcb599990b8faf3537
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-20 14:30:42 +0100

    Add to average a sigmoid of the difference of previous and average score (Only for negative difference, C=400). Bench: 6803596

--- src/search.cpp
+++ src/search.cpp
@@ -377,7 +377,7 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               int diff = rootMoves[pvIdx].previousScore - rootMoves[pvIdx].averageScore;
-              Value prev = rootMoves[pvIdx].averageScore + diff * std::abs(diff) / (std::abs(diff) + 400);
+              Value prev = rootMoves[pvIdx].averageScore + std::min(diff, 0) * std::abs(diff) / (std::abs(diff) + 400);
 
               delta = Value(17) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
