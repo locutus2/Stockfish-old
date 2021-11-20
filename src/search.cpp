ea70765250940c7879d1543eb31d4efd939fa089
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-20 07:51:04 +0100

    If previous - average score > 200 use previous else average score. Bench: 6715511

--- src/search.cpp
+++ src/search.cpp
@@ -377,8 +377,8 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               int diff = rootMoves[pvIdx].previousScore - rootMoves[pvIdx].averageScore;
-              Value prev = diff < -200 ? rootMoves[pvIdx].previousScore
-                                       : rootMoves[pvIdx].averageScore;
+              Value prev = diff > 200 ? rootMoves[pvIdx].previousScore
+                                      : rootMoves[pvIdx].averageScore;
 
               delta = Value(17) + int(prev) * prev / 16384;
               alpha = std::max(prev - delta,-VALUE_INFINITE);
