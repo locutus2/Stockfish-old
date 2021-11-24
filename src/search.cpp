bc1fb072c1287950f204ef93bb46fed00661a7c6
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-11-24 16:53:03 +0100

    Double effect: Randomly do on average two plies less reduction for each variation. Bench: 8242896

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
       {
           Depth r = reduction(improving, depth, moveCount, rangeReduction > 2);
 
-          if(!(thisThread->nodes % thisThread->rootDepth))
+          if(!(thisThread->nodes % ((ss->ply + depth) / 2)))
               r--;
 
           // Decrease reduction at some PvNodes (~2 Elo)
