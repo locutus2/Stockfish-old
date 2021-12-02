4101d2803ec7c5e20540f9b150f28a6a54f2de6e
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-02 22:52:46 +0100

    init cmh table with -1000. Bench: 5251527

--- src/thread.cpp
+++ src/thread.cpp
@@ -67,7 +67,7 @@ void Thread::clear() {
       {
           for (auto& to : continuationHistory[inCheck][c])
                 for (auto& h : to)
-                      h->fill(-143);
+                      h->fill(-1000);
           continuationHistory[inCheck][c][NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
       }
 }
