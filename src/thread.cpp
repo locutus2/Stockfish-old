4a048ca7b5443a0fa0bd2aac59f4c6c8ffc539cf
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-03 18:24:28 +0100

    Init cmh table with -143 (use also for special initialization). Bench: 4148962

--- src/thread.cpp
+++ src/thread.cpp
@@ -67,8 +67,8 @@ void Thread::clear() {
       {
           for (auto& to : continuationHistory[inCheck][c])
                 for (auto& h : to)
-                      h->fill(-71);
-          continuationHistory[inCheck][c][NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
+                      h->fill(-143);
+          continuationHistory[inCheck][c][NO_PIECE][0]->fill(-143);
       }
 }
 
