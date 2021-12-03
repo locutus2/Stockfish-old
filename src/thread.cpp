c369b7f39ac483d2bc641a956defce782f10dd99
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-03 19:23:43 +0100

    Try opposite: init cmh table with 10000. Bench: 7797787

--- src/thread.cpp
+++ src/thread.cpp
@@ -67,7 +67,7 @@ void Thread::clear() {
       {
           for (auto& to : continuationHistory[inCheck][c])
                 for (auto& h : to)
-                      h->fill(1000);
+                      h->fill(10000);
           continuationHistory[inCheck][c][NO_PIECE][0]->fill(Search::CounterMovePruneThreshold - 1);
       }
 }
