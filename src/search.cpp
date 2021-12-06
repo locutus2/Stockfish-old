ff4655ada46e34e3e497d5239d2b0b5d39ed836b
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-06 18:04:25 +0100

    Shift allowed falling eval factor range up by 0.1. Bench: 4724181

--- src/search.cpp
+++ src/search.cpp
@@ -493,7 +493,7 @@ void Thread::search() {
           double fallingEval = (142 + 6 * (mainThread->bestPreviousScore - bestValue)
                                     + 6 * (mainThread->bestPreviousAverageScore - bestValue)
                                     + 6 * (mainThread->iterValue[iterIdx] - bestValue)) / 825.0;
-          fallingEval = std::clamp(fallingEval, 0.5, 1.5);
+          fallingEval = std::clamp(fallingEval, 0.6, 1.6);
 
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 9 < completedDepth ? 1.92 : 0.95;
