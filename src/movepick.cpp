dc8ab7cb8f3a14cf674f4643298d184129a9a9a2
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-13 13:28:47 +0100

    Sort limit -3000 * (depth + 2) + 6000 / depth. Bench: 4837894

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -202,7 +202,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * (depth + 2) + 24000 / (depth + 3));
+          partial_insertion_sort(cur, endMoves, -3000 * (depth + 2) + 6000 / depth);
       }
 
       ++stage;
