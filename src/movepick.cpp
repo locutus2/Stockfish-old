03fb975a54309cbebd90bc77c7433c0182311b30
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-14 22:43:57 +0100

    Sort limit max(0, 4 * depth - rootDepth). Bench: 4378811

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -19,6 +19,7 @@
 #include <cassert>
 
 #include "movepick.h"
+#include "thread.h"
 
 namespace Stockfish {
 
@@ -202,7 +203,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * depth);
+          partial_insertion_sort(cur, endMoves, -3000 * std::max(0, 4 * depth - pos.this_thread()->rootDepth));
       }
 
       ++stage;
