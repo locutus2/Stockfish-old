4a709a7881838efee46936fd6dd814aa5dfd634a
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-16 20:53:37 +0100

    Decrease delta by 2 * number of fail highs. Bench: 4775590

--- src/search.cpp
+++ src/search.cpp
@@ -444,7 +444,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 5 - failedHighCnt;
+              delta += delta / 4 + 5 - 2 * failedHighCnt;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
