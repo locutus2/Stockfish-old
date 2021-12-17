c6f28a5a84907587e3511993146105b3e839670b
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-17 17:47:37 +0100

    Increase delta by 2 * number of fail lows. Bench: 4582419

--- src/search.cpp
+++ src/search.cpp
@@ -447,7 +447,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 4 + failedLowCnt;
+              delta += delta / 4 + 5 + 2 * failedLowCnt;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
