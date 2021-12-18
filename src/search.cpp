027692de33ae0b7c4748097c2fc12a0412c82e24
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-18 19:22:37 +0100

    Increase delta by number of researches. Compensate by decrease offset by 2. Bench: 4837081

--- src/search.cpp
+++ src/search.cpp
@@ -444,7 +444,7 @@ void Thread::search() {
               else
                   break;
 
-              delta += delta / 4 + 4 + researches;
+              delta += delta / 4 + 3 + researches;
 
               assert(alpha >= -VALUE_INFINITE && beta <= VALUE_INFINITE);
           }
