6f177dceaf9e8e7091fb190f829c6ea563e5b111
Stefan Geschwentner <stgeschwentner@gmail.com>
2021-12-19 12:09:45 +0100

    Decrease initial delta by 2 if last best move not a capture. Bench: 5196011

--- src/search.cpp
+++ src/search.cpp
@@ -377,7 +377,7 @@ void Thread::search() {
           if (rootDepth >= 4)
           {
               Value prev = rootMoves[pvIdx].averageScore;
-              delta = Value(17) + int(prev) * prev / 16384 - 7 * bool(rootPos.checkers());
+              delta = Value(17) + int(prev) * prev / 16384 - 2 * !rootPos.capture(rootMoves[pvIdx].pv[0]);
               alpha = std::max(prev - delta,-VALUE_INFINITE);
               beta  = std::min(prev + delta, VALUE_INFINITE);
 
