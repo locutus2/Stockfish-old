461945ee359af8439e622e4963bca6a251b53590
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-02 22:34:24 +0100

    More reduction if !PvNode && !cutNode && !priorCapture && (ss-1)->moveCount != 1. Bench: 3923426

--- src/search.cpp
+++ src/search.cpp
@@ -1182,7 +1182,7 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if(likelyFailLow && !priorCapture && (ss-1)->moveCount != 1)
+          if(!PvNode && !cutNode && !priorCapture && (ss-1)->moveCount != 1)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
