e276390ee30d18b63463199b5ec46de7914b71fd
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-04 22:02:35 +0100

    Tweak reduction array by using log(i+1). For compensation subtract 8. Bench: 5702646

--- src/search.cpp
+++ src/search.cpp
@@ -181,7 +181,7 @@ namespace {
 void Search::init() {
 
   for (int i = 1; i < MAX_MOVES; ++i)
-      Reductions[i] = int((21.9 + std::log(Threads.size()) / 2) * std::log(i + 1) - 7);
+      Reductions[i] = int((21.9 + std::log(Threads.size()) / 2) * std::log(i + 1) - 8);
 }
 
 
