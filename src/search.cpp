a310ec7ec422d97c0129d6007755a730b029924f
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-05 01:14:57 +0100

    Increase depth reduction factor by 4. Bench: 4566296

--- src/search.cpp
+++ src/search.cpp
@@ -70,7 +70,7 @@ namespace {
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
   Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
-    int r = Reductions[d] * (Reductions[mn] + 4);
+    int r = (Reductions[d] + 4) * Reductions[mn];
     return (r + 1358 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 904);
   }
 
