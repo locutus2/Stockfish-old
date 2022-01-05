4af5d807b18aa77bcfb49227b84d0df68a045377
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-05 19:36:27 +0100

    More reduction for a non-capture rook move which ends on a square which has to left and right an own pawn. Bench: 4760551

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,11 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (   type_of(movedPiece) == ROOK
+              && !captureOrPromotion
+              && more_than_one(pos.pieces(us, PAWN) & rank_bb(to_sq(move)) & adjacent_files_bb(to_sq(move))))
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
