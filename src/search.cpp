e3ebcc9ab7aea342a5194b555d7042f3548f846d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-06 12:38:46 +0100

    Tweak non-quiet SEE pruning threshold: (-218 - cmh0/512) * depth. Bench: 4794874

--- src/search.cpp
+++ src/search.cpp
@@ -1040,7 +1040,7 @@ moves_loop: // When in check, search starts here
                   continue;
 
               // SEE based pruning (~9 Elo)
-              if (!pos.see_ge(move, Value(-218 - (*contHist[0])[movedPiece][to_sq(move)] / 1024) * depth))
+              if (!pos.see_ge(move, Value(-218 - (*contHist[0])[movedPiece][to_sq(move)] / 512) * depth))
                   continue;
           }
           else
