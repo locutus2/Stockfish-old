d7518990d134778b7d278b47b758197dcdbbd843
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-12 00:20:42 +0100

    If already a best move found then reduce moves which moves the same piece more. Bench: 4689194

--- src/search.cpp
+++ src/search.cpp
@@ -1182,6 +1182,9 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (PvNode && bestMove && from_sq(move) == from_sq(bestMove))
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
