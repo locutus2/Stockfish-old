ca91a05a3d897c0dbe550e5802f17412e88fb0e1
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-14 13:32:49 +0100

    Add in stat bonus to depth complexity / 128. Bench: 5109353

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,8 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d, int complexity) {
-    return std::min((6 * d + 229) * d - 215 + complexity / 4, 2000);
+    d += complexity / 128;
+    return std::min((6 * d + 229) * d - 215, 2000);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
