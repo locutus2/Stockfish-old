b62b6174bdb23cb0255971dbdf190bb55e548e95
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-14 21:08:17 +0100

    Add additional term min(complexity,500) to stat bonus. Bench: 4607559

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d, int complexity) {
-    return std::min((6 * d + 229) * d - 215 + complexity * d / 4, 2000);
+    return std::min((6 * d + 229) * d - 215, 2000) + std::min(complexity, 500);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
