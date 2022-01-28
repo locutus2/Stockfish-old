8274cfbe75f3b294d03a140cbf71060ac9eab3fb
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-28 20:19:21 +0100

    More reduction if stat score < -45000. Bench: 4872673

--- src/search.cpp
+++ src/search.cpp
@@ -1164,8 +1164,8 @@ moves_loop: // When in check, search starts here
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 14721;
 
-          if (ss->statScore > 29000)
-              r--;
+          if (ss->statScore < -45000)
+              r++;
 
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
