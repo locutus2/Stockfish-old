b857562ea9b456462a17440d95d140bf119a1598
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-29 14:43:35 +0100

    More tt capture reduction at PV nodes if best move is tt move and ply < depth. Bench: 4289105

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r += 1 + (PvNode && bestMove == ttMove && (thisThread->nodes & 1));
+              r += 1 + (PvNode && bestMove == ttMove && ss->ply < depth);
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
