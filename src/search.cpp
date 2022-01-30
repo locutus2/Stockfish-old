e81ce87bca102633c043ecfb24ea6c3d0e31c48d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-30 21:56:32 +0100

    Less reduction at cut nodes if ply >= depth. Bench: 4635325

--- src/search.cpp
+++ src/search.cpp
@@ -1149,7 +1149,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2;
+              r += 1 + (ss->ply < depth);
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
