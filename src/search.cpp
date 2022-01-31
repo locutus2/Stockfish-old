06019105347890dbaef315afdb35b8a3c4d665c2
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-01-31 09:27:46 +0100

    More reduction for tt capture if ply < 4. Bench: 4414057

--- src/search.cpp
+++ src/search.cpp
@@ -1153,7 +1153,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
-              r += 1 + (ss->ply < 3);
+              r += 1 + (ss->ply < 4);
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
