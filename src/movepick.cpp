7d307635867830ce42c525fd3a7e70848279bf17
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-07 08:24:40 +0100

    Decrease quiet sort limit by 12 * average complexity. Bench: 4375891

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -207,7 +207,7 @@ top:
           endMoves = generate<QUIETS>(pos, cur);
 
           score<QUIETS>();
-          partial_insertion_sort(cur, endMoves, -3000 * (depth + pos.this_thread()->complexityAverage.value() / 256));
+          partial_insertion_sort(cur, endMoves, -3000 * depth - 12 * pos.this_thread()->complexityAverage.value());
       }
 
       ++stage;
