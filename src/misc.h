3cc049374f3ecf14fb51dd2511faf7b1f54324d2
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-08 20:14:10 +0100

    Update complexity average also with search complexity (eval/search ration 2:1). Bench: 4461945

--- src/misc.h
+++ src/misc.h
@@ -94,24 +94,35 @@ class RunningAverage {
       RunningAverage() {}
 
       // Reset the running average to rational value p / q
-      void set(int64_t p, int64_t q)
-        { average = p * PERIOD * RESOLUTION / q; }
+      void set(int64_t p, int64_t q = 1, int64_t w = 1)
+      {
+          average = p * PERIOD * RESOLUTION / q;
+	  weight  = w * PERIOD * RESOLUTION;
+      }
 
       // Update average with value v
-      void update(int64_t v)
-        { average = RESOLUTION * v + (PERIOD - 1) * average / PERIOD; }
+      void update(int64_t v, int64_t w = 1)
+      {
+          average = RESOLUTION * w * v + (PERIOD - 1) * average / PERIOD;
+          weight  = RESOLUTION * w + (PERIOD - 1) * weight / PERIOD;
+      }
 
       // Test if average is strictly greater than rational a / b
       bool is_greater(int64_t a, int64_t b)
-        { return b * average > a * PERIOD * RESOLUTION ; }
+      {
+          return b * average > a * weight;
+      }
 
       int64_t value()
-        { return average / (PERIOD * RESOLUTION); }
+      {
+          return average / weight;
+      }
 
   private :
       static constexpr int64_t PERIOD     = 4096;
       static constexpr int64_t RESOLUTION = 1024;
       int64_t average;
+      int64_t weight;
 };
 
 template <typename T, std::size_t MaxSize>
