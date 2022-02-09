9088c9d27194aefb5dcc09327365dffcc38f5998
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-09 20:47:28 +0100

    Scale eval now with the sum of remaining moves and game phase. Bench: 4224464

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1111,7 +1111,8 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (208 - pos.rule50_count()) / 208;
+  Phase gamePhase = Material::probe(pos)->game_phase();
+  v = v * (100 - pos.rule50_count() + gamePhase) / (100 + PHASE_MIDGAME);
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
