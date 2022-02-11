1a58115b955061bbf6da0f067bf408aeeecc6e01
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-11 20:47:42 +0100

    More reduction if NMP failed. Bench: 4526198

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (failedNMP)
-              r--;
+              r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
