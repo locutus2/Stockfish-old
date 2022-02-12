ac5ff5b23b1f308aaca13621f3155bcdb9269bb3
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-12 20:45:30 +0100

    No pruning for threat counter move if different from other good moves. Bench: 4424718

--- src/search.cpp
+++ src/search.cpp
@@ -1003,6 +1003,11 @@ moves_loop: // When in check, search starts here
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
           && pos.non_pawn_material(us)
+          && !(   move == threatCounterMove
+               && move != ttMove
+               && move != ss->killers[0]
+               && move != ss->killers[1]
+               && move != countermove)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
@@ -1169,13 +1174,6 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
-
-          if (   move == threatCounterMove
-              && move != ttMove
-              && move != ss->killers[0]
-              && move != ss->killers[1]
-              && move != countermove)
-              r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
