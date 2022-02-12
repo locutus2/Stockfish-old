00bbc1496e404ea0c30da6ad4519b6ee6708888a
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-12 10:57:22 +0100

    Set deeper for r >= -1 only zero if not threat counter move. Bench: 4475082

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,6 @@ moves_loop: // When in check, search starts here
       // cases where we extend a son if it has good chances to be "interesting".
       if (    depth >= 2
           &&  moveCount > 1 + rootNode
-          &&  move != counterThreatMove
           && (   !ss->ttPv
               || !captureOrPromotion
               || (cutNode && (ss-1)->moveCount > 1)))
@@ -1183,7 +1182,7 @@ moves_loop: // When in check, search starts here
           // In general we want to cap the LMR depth search at newDepth. But if reductions
           // are really negative and movecount is low, we allow this move to be searched
           // deeper than the first move (this may lead to hidden double extensions).
-          int deeper =   r >= -1                   ? 0
+          int deeper =   r >= -1 && move != counterThreatMove ? 0
                        : moveCount <= 5            ? 2
                        : PvNode && depth > 4       ? 1
                        : cutNode && moveCount <= 5 ? 1
