0080edd472c798aa6e1fd030b44802be6f0a6765
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-12 07:15:49 +0100

    Less pruning (increase lmrDepth) if NMP failed. Bench: 5610237

--- src/search.cpp
+++ src/search.cpp
@@ -1008,7 +1008,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - failedNMP - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = std::max(newDepth + failedNMP - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
 
           if (   captureOrPromotion
               || givesCheck)
