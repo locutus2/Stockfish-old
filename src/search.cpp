dcf103dfb3f92c11cb5202796b23dd786103f623
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-02-14 06:11:19 +0100

    No reduction for counter capture at CUT nodes. Bench: 4524500

--- src/search.cpp
+++ src/search.cpp
@@ -1140,7 +1140,11 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + rootNode
           && (   !ss->ttPv
               || !captureOrPromotion
-              || (cutNode && (ss-1)->moveCount > 1)))
+              || (cutNode && (ss-1)->moveCount > 1))
+          && (   move != counterCapture
+              || !captureOrPromotion
+              || !cutNode))
+
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
@@ -1166,9 +1170,6 @@ moves_loop: // When in check, search starts here
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
               r++;
-
-          if (!PvNode && move == counterCapture && captureOrPromotion)
-              r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
