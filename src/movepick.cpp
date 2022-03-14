7b02fe29a18b3891cfcda39664a7234905ae6ad9
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-14 01:08:27 +0100

    Sort quiet moves and bad captures together. Offset -20000. Bench: 7177190

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -207,7 +207,7 @@ top:
       if (!skipQuiets)
       {
           for (auto& m : *this)
-              m.value -= 60000;
+              m.value -= 20000;
 
           cur = endBadCaptures;
           endMoves = generate<QUIETS>(pos, cur);
