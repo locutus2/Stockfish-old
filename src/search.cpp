6624ce68562f42fc45fef02adc7a9bc9aa0f7f54
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-14 21:13:19 +0100

    Always verification search if (ss-1)->inCheck. Bench: 7606234

--- src/search.cpp
+++ src/search.cpp
@@ -825,7 +825,7 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14))
+            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && !(ss-1)->inCheck))
                 return nullValue;
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
