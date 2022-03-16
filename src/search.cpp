f847e84df975b65c289e67cdf1564788a75ba475
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-16 12:24:18 +0100

    Do ttPv tree growing only if depth + root depth > 18. Test against my passed ttpvPR. Bench: 6542341

--- src/search.cpp
+++ src/search.cpp
@@ -1357,7 +1357,7 @@ moves_loop: // When in check, search starts here
 
     // If no good move is found and the previous position was ttPv, then the previous
     // opponent move is probably good and the new position is added to the search tree.
-    if (bestValue <= alpha && depth + thisThread->rootDepth > 14)
+    if (bestValue <= alpha && depth + thisThread->rootDepth > 18)
         ss->ttPv = ss->ttPv || (ss-1)->ttPv;
 
     // Write gathered information in transposition table
