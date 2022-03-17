86923c05a5022dcea5f0e66ad2a17a8c19beea97
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-17 14:09:39 +0100

    Always verification search if own material <= 2 * knight. Bench: 6737048

--- src/search.cpp
+++ src/search.cpp
@@ -825,7 +825,7 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && cutNode))
+            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && pos.non_pawn_material(us) > 2 * KnightValueMg))
                 return nullValue;
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
