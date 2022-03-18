c0a14e73d12278c0ecc77f1d86d443d52de43b17
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-18 07:53:06 +0100

    Always verification search if own material <= bishop. Bench: 7117708

--- src/search.cpp
+++ src/search.cpp
@@ -825,7 +825,7 @@ namespace {
             if (nullValue >= VALUE_TB_WIN_IN_MAX_PLY)
                 nullValue = beta;
 
-            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && pos.non_pawn_material(us) > RookValueMg))
+            if (thisThread->nmpMinPly || (abs(beta) < VALUE_KNOWN_WIN && depth < 14 && pos.non_pawn_material(us) > BishopValueMg))
                 return nullValue;
 
             assert(!thisThread->nmpMinPly); // Recursive verification is not allowed
