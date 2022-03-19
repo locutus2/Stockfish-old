a159e01ba73dfec83205f6a3577d0978cecd9978
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-19 18:03:24 +0100

    No NMP if own material <= 2 * knight. Bench: 7767103

--- src/search.cpp
+++ src/search.cpp
@@ -802,8 +802,7 @@ namespace {
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 198 + complexity / 28
         && !excludedMove
-        &&  pos.non_pawn_material(us)
-        && (!ss->ttPv || cutNode)
+        &&  pos.non_pawn_material(us) > 2 * KnightValueMg
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
     {
         assert(eval - beta >= 0);
