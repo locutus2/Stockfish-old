d3a1318ebe2481171f4a3df2c3c2a6cc1860e449
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-25 19:41:34 +0100

    Allow NMP at non-CUT nodes only if depth < ply + 6. Bench: 6667737

--- src/search.cpp
+++ src/search.cpp
@@ -803,7 +803,7 @@ namespace {
         &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 198 + complexity / 28
         && !excludedMove
         &&  pos.non_pawn_material(us)
-        && (cutNode || depth < ss->ply + 4)
+        && (cutNode || depth < ss->ply + 6)
         && (ss->ply >= thisThread->nmpMinPly || us != thisThread->nmpColor))
     {
         assert(eval - beta >= 0);
