ac2ce55ec5d7788d8e4fedde3c267b514221f89c
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-03-26 15:37:16 +0100

    Allow NMP at non-CUT nodes only if eval >= beta + 12 * depth. Bench: 6443341

--- src/search.cpp
+++ src/search.cpp
@@ -798,7 +798,7 @@ namespace {
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 14695
-        &&  eval >= beta + 6 * !cutNode * depth
+        &&  eval >= beta + 12 * !cutNode * depth
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 15 * depth - improvement / 15 + 198 + complexity / 28
         && !excludedMove
