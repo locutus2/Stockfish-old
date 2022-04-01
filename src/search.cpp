2a09e7e0ee169cc3009a23f0807309179c9b56e8
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-01 14:33:44 +0200

    Less reduction for discovered checks. Bench: 6538113

--- src/search.cpp
+++ src/search.cpp
@@ -1167,6 +1167,9 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
+          if (givesCheck && !(pos.checkers() & to_sq(move)))
+              r--;
+
           // Decrease reduction at PvNodes if bestvalue
           // is vastly different from static evaluation
           if (PvNode && !ss->inCheck && abs(ss->staticEval - bestValue) > 250)
