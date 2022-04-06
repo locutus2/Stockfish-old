3767f54f3f7eec41da4727a5eb609c8a3184b4b7
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-06 20:26:09 +0200

    Give order bonus 120000/(depth+1) for real discovered checks (exclude king). Bench: 6708666

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -145,7 +145,7 @@ void MovePicker::score() {
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[5])[pos.moved_piece(m)][to_sq(m)]
-                   +     (discoveredChecks & from_sq(m) ? 60000 / depth : 0)
+                   +     (discoveredChecks & from_sq(m) ? 120000 / (depth + 1) : 0)
                    +     (threatened & from_sq(m) ?
                            (type_of(pos.moved_piece(m)) == QUEEN && !(to_sq(m) & threatenedByRook)  ? 50000
                           : type_of(pos.moved_piece(m)) == ROOK  && !(to_sq(m) & threatenedByMinor) ? 25000
