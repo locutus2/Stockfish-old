1e52fa2237c2f7967efa1a504b15273be6627d92
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-16 14:06:29 +0200

    Maintain static eval average and use for adjusting time used. Bench: 6964954

--- src/search.cpp
+++ src/search.cpp
@@ -304,6 +304,7 @@ void Thread::search() {
   multiPV = std::min(multiPV, rootMoves.size());
 
   complexityAverage.set(202, 1);
+  evalAverage.set(0, 1);
 
   trend         = SCORE_ZERO;
   optimism[ us] = Value(39);
@@ -470,8 +471,9 @@ void Thread::search() {
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
+          double staticEvalDiff = std::clamp(1 + (int(bestValue) - evalAverage.value()) / 6080.0, 0.8, 1.2);
 
-          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition;
+          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * staticEvalDiff;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
@@ -773,6 +775,7 @@ namespace {
     complexity = abs(ss->staticEval - (us == WHITE ? eg_value(pos.psq_score()) : -eg_value(pos.psq_score())));
 
     thisThread->complexityAverage.update(complexity);
+    thisThread->evalAverage.update(ss->staticEval);
 
     // Step 7. Razoring.
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
--- src/thread.h
+++ src/thread.h
@@ -61,6 +61,7 @@ public:
   Material::Table materialTable;
   size_t pvIdx, pvLast;
   RunningAverage complexityAverage;
+  RunningAverage evalAverage;
   std::atomic<uint64_t> nodes, tbHits, bestMoveChanges;
   int selDepth, nmpMinPly;
   Color nmpColor;
