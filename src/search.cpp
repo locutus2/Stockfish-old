556c2cd6dcd5ef88c3cd901e8e9cece303436adf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-17 00:39:59 +0200

    More reduction at PV nodes if eval < best value. Bench: 5597658

--- src/search.cpp
+++ src/search.cpp
@@ -1168,8 +1168,8 @@ moves_loop: // When in check, search starts here
           if (ttCapture)
               r++;
 
-          if (eval != VALUE_NONE)
-              r += 2 * (eval <= bestValue) - 1;
+          if (PvNode && eval < bestValue)
+              r++;
 
           // Decrease reduction at PvNodes if bestvalue
           // is vastly different from static evaluation
