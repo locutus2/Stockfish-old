72d861fb19830dfcaa3a6f45e5572054013844b3
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-17 15:26:37 +0200

    Maintain static eval average and use for adjusting time used absolute difference. Bench: 6964954

--- src/search.cpp
+++ src/search.cpp
@@ -471,9 +471,9 @@ void Thread::search() {
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
-          double staticEvalDiff = std::clamp(1 + (evalAverage.value() - int(bestValue) + 60) / 6080.0, 0.8, 1.2);
+          double evalDiff = std::clamp(1 + (std::abs(evalAverage.value() - int(bestValue)) - 324) / 5180.0, 0.8, 1.2);
 
-          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * staticEvalDiff;
+          double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * evalDiff;
 
           // Cap used time in case of a single legal move for a better viewer experience in tournaments
           // yielding correct scores and sufficiently fast moves.
