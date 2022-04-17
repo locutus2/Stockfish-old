a55ec37c9c618e39477a8fafa68d91ea329e484e
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-17 09:48:56 +0200

    Raise alpha at PV nodes to beta - 2 * root depth. Bench: 5517683

--- src/search.cpp
+++ src/search.cpp
@@ -1295,7 +1295,7 @@ moves_loop: // When in check, search starts here
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
               {
                   if (beta < VALUE_INFINITE)
-                      alpha = std::max(value, beta - 3 * depth);
+                      alpha = std::max(value, beta - 2 * thisThread->rootDepth);
                   else
                       alpha = value;
                   bestMoveCount++;
