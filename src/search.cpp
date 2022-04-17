18a51cf8b59372effd55ee1e7552e0d01f86752d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-17 15:22:27 +0200

    Fixed: Maintain static eval average and use for adjusting time used (try opposite direction). Subtract mean. Bench: 6964954

--- src/search.cpp
+++ src/search.cpp
@@ -471,7 +471,7 @@ void Thread::search() {
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
-          double staticEvalDiff = std::clamp(1 + (evalAverage.value() - int(bestValue) + 60) / 6080.0, 0.2, 1.2);
+          double staticEvalDiff = std::clamp(1 + (evalAverage.value() - int(bestValue) + 60) / 6080.0, 0.8, 1.2);
 
           double totalTime = Time.optimum() * fallingEval * reduction * bestMoveInstability * complexPosition * staticEvalDiff;
 
