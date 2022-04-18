336e92b9aa43fdaf2ad6c112722b589cffbed8a5
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-18 08:51:48 +0200

    Scale stat bonus with (complexity + 1792) / 2048. Also avoid negative bonus for depth 1. Bench: 8199062

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d, int complexity) {
-    return std::min(((6 * d + 202) * d - 207) * (complexity + 1500) / 2048, 2145);
+    return std::min(((9 * d + 303) * d - 311) * (complexity + 1792) / 2048, 2145);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
