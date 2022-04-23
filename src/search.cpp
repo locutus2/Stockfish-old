9c120b96f8972a32622a1f2ce71a09ab6d386614
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-23 23:52:31 +0200

    Raise alpha to maximum of search value and depth weighted mean of initial window bounds (less slope). Bench: 6493852

--- src/search.cpp
+++ src/search.cpp
@@ -1299,7 +1299,7 @@ moves_loop: // When in check, search starts here
               if (PvNode && value < beta) // Update alpha! Always alpha < beta
               {
                   if (beta < VALUE_INFINITE && initialAlpha > -VALUE_INFINITE)
-                      alpha = std::max(value, (16 * (beta - 1) + depth * initialAlpha) / (16 + depth));
+                      alpha = std::max(value, (32 * (beta - 1) + (16 + depth) * initialAlpha) / (48 + depth));
                   else
                       alpha = value;
               }
