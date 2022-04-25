a87ce6ef70f20a31c3d089bdaa8b90477a3cb5ca
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-25 09:26:55 +0200

    Tweak best move instability time factor. Take 2. Bench: 7729968

--- src/search.cpp
+++ src/search.cpp
@@ -466,7 +466,7 @@ void Thread::search() {
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 10 < completedDepth ? 1.63 : 0.73;
           double reduction = (1.56 + mainThread->previousTimeReduction) / (2.20 * timeReduction);
-          double bestMoveInstability = 1.025 + std::max(0.956, 2.15 - 9.46 / rootDepth)
+          double bestMoveInstability = 1.127 + std::max(1.05, 2.36 - 10.4 / rootDepth)
                                               * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
