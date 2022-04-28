7310cd3383547cc693601301024bbd87b8f1657d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-28 21:31:21 +0200

    More reduction at PV nodes with best move if tt value < best value and depth > ply. Bench: 7105309

--- src/search.cpp
+++ src/search.cpp
@@ -1176,7 +1176,7 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 
-          if (rootNode && bestMove && ttValue < bestValue)
+          if (PvNode && bestMove && ttValue < bestValue && depth > ss->ply)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
