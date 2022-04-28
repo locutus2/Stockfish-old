f3b818da28928e221a11f5c4ff55807a0dd9a9cb
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-28 20:07:03 +0200

    More reduction at PV nodes with best move if tt value < best value and non-capture. Bench: 8145115

--- src/search.cpp
+++ src/search.cpp
@@ -1176,6 +1176,9 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / ( 3 + depth );
 
+          if (PvNode && bestMove && ttValue < bestValue && !capture)
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
