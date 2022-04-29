66872a6f136f8e7606bef6499925677f90265abb
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-04-29 06:17:48 +0200

    5% more variance of best move instability time factor (simplified version). Test against my passed tm_PR patch. Bench: 7729968

--- src/search.cpp
+++ src/search.cpp
@@ -466,8 +466,7 @@ void Thread::search() {
           // If the bestMove is stable over several iterations, reduce time accordingly
           timeReduction = lastBestMoveDepth + 10 < completedDepth ? 1.63 : 0.73;
           double reduction = (1.56 + mainThread->previousTimeReduction) / (2.20 * timeReduction);
-          double bestMoveInstability = 1.024 + std::max(1.05, 2.3625 - 10.4 / rootDepth)
-                                              * totBestMoveChanges / Threads.size();
+          double bestMoveInstability = 0.92 + 1.79 * totBestMoveChanges / Threads.size();
           int complexity = mainThread->complexityAverage.value();
           double complexPosition = std::clamp(1.0 + (complexity - 326) / 1618.1, 0.5, 1.5);
 
