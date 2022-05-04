e3d68ca83cdae6406ce5a37bfa5770c00495883a
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-04 13:48:29 +0200

    Do less CUT nodes reduction if parent of parent node is not a CUT node. Bench: 7277582

--- src/search.cpp
+++ src/search.cpp
@@ -1162,7 +1162,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2 + (ss-2)->cutNode;
+              r += 1 + (ss-2)->cutNode;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
