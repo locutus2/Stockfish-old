fa5087a7f0831366e7ef56def23ee7ca2ca0ea1d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-04 10:14:04 +0200

    Do LMR for captures also if parent of parent node is a CUT node. Bench: 6548179

--- src/search.cpp
+++ src/search.cpp
@@ -1146,7 +1146,7 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || ((cutNode || (ss-1)->cutNode) && (ss-1)->moveCount > 1)))
+              || ((cutNode || (ss-2)->cutNode) && (ss-1)->moveCount > 1)))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
