c36c9c5bfec584787340315c4749909d43c2e90d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-09 07:32:33 +0200

    More reduction at CUT node if improving. Bench: 7113671

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction for cut nodes (~3 Elo)
           if (cutNode && move != ss->killers[0])
-              r += 2;
+              r += 2 + improving;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
           if (ttCapture)
