e3e5d59091464ac1a159925363b6dcc1dea9ca17
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-13 00:39:12 +0200

    Calculate Single Conspiracy Numbers (SCN). More reduction if SCN > 20. Bench: 6617552

--- src/search.cpp
+++ src/search.cpp
@@ -1237,7 +1237,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (!excludedMove && ss->SCN > 10)
+          if (!excludedMove && ss->SCN > 20)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
