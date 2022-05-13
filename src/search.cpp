3795cee4b153cabc2628f821552d4f5e9b22d1b4
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-13 23:14:39 +0200

    Calculate Single Conspiracy Numbers (SCN). Less reduction at PV nodes if SCN = 1. Bench: 7046720

--- src/search.cpp
+++ src/search.cpp
@@ -1239,7 +1239,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (cutNode && !excludedMove && ss->SCN == 1)
+          if (PvNode && !excludedMove && ss->SCN == 1)
               r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
