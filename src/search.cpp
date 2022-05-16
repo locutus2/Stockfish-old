b5f2e0800fcaaa5a122cda052eee2b91da1c1664
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-16 23:59:41 +0200

    No check extension for capture if move count < 4. Bench: 6520276

--- src/search.cpp
+++ src/search.cpp
@@ -1110,7 +1110,8 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
-                   && abs(ss->staticEval) > 71)
+                   && abs(ss->staticEval) > 71
+                   && (!capture || moveCount >= 4))
               extension = 1;
 
           // Quiet ttMove extensions (~0 Elo)
