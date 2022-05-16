4b3dafef93efe80b497e299331660243403000bc
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-16 23:55:33 +0200

    More reduction for extended capture if move cunt < 4. Bench: 6332512

--- src/search.cpp
+++ src/search.cpp
@@ -1183,6 +1183,9 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
+          if (capture && moveCount < 4 && extension > 0)
+              r++;
+
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
