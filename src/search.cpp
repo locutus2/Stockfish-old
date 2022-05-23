2e287eb16d48b83e53498a8d3a4362b105c2f70b
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-23 20:37:16 +0200

    history pruning

--- src/search.cpp
+++ src/search.cpp
@@ -1045,19 +1045,7 @@ moves_loop: // When in check, search starts here
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 5
                   && history < -3875 * (depth - 1))
-                  continue;
-
-              history += thisThread->mainHistory[us][from_to(move)];
-
-              // Futility pruning: parent node (~9 Elo)
-              if (   !ss->inCheck
-                  && lmrDepth < 11
-                  && ss->staticEval + 122 + 138 * lmrDepth + history / 60 <= alpha)
-                  continue;
-
-              // Prune moves with negative SEE (~3 Elo)
-              if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
-          //        continue;
+              //    continue;
               {
               if(LCS_PRUNE)
               {
@@ -1116,6 +1104,18 @@ moves_loop: // When in check, search starts here
               }
               else continue;
               }
+
+              history += thisThread->mainHistory[us][from_to(move)];
+
+              // Futility pruning: parent node (~9 Elo)
+              if (   !ss->inCheck
+                  && lmrDepth < 11
+                  && ss->staticEval + 122 + 138 * lmrDepth + history / 60 <= alpha)
+                  continue;
+
+              // Prune moves with negative SEE (~3 Elo)
+              if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
+                  continue;
           }
       }
 
