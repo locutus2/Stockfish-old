3dceba92c0010d8fe0cd7f50e8a75cab72b9ebf5
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-25 23:21:59 +0200

    No pruning for killers from two plies earlier. Bench: 7355296

--- src/search.cpp
+++ src/search.cpp
@@ -1004,6 +1004,8 @@ moves_loop: // When in check, search starts here
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
           && pos.non_pawn_material(us)
+          && move != (ss-2)->killers[0]
+          && move != (ss-2)->killers[1]
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
