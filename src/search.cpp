c02aa17eb077683bb700ca83b13e91f37e843197
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-05-25 13:52:18 +0200

    No pruning for excluded move from two plies earlier if in check state there is the same. Bench: 6974473

--- src/search.cpp
+++ src/search.cpp
@@ -1004,7 +1004,7 @@ moves_loop: // When in check, search starts here
       // Step 14. Pruning at shallow depth (~98 Elo). Depth conditions are important for mate finding.
       if (  !rootNode
           && pos.non_pawn_material(us)
-          && (priorCapture || (ss-2)->excludedMove != move)
+          && ((ss-2)->excludedMove != move || ss->inCheck != (ss-2)->inCheck)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
           // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~7 Elo)
