9cbf454127f13fe47d9a637d988db3fa0c6debfa
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-07 12:12:29 +0200

    Verify quiet history pruning with a quience search <= alpha. Bench: 5570678

--- src/search.cpp
+++ src/search.cpp
@@ -1035,22 +1035,41 @@ moves_loop: // When in check, search starts here
                             + (*contHist[1])[movedPiece][to_sq(move)]
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
-              // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 5
-                  && history < -3875 * (depth - 1))
-                  continue;
-
-              history += thisThread->mainHistory[us][from_to(move)];
-
               // Futility pruning: parent node (~9 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 11
-                  && ss->staticEval + 122 + 138 * lmrDepth + history / 60 <= alpha)
+                  && ss->staticEval + 122 + 138 * lmrDepth + (history + thisThread->mainHistory[us][from_to(move)]) / 60 <= alpha)
                   continue;
 
               // Prune moves with negative SEE (~3 Elo)
               if (!pos.see_ge(move, Value(-25 * lmrDepth * lmrDepth - 20 * lmrDepth)))
                   continue;
+
+              // Continuation history based pruning (~2 Elo)
+              if (   lmrDepth < 5
+                  && history < -3875 * (depth - 1))
+              {
+                  ss->doubleExtensions = (ss-1)->doubleExtensions;
+                  ss->currentMove = move;
+                  ss->continuationHistory = &thisThread->continuationHistory[ss->inCheck]
+                                                                            [capture]
+                                                                            [movedPiece]
+                                                                            [to_sq(move)];
+
+                  if (PvNode)
+                  {
+                      (ss+1)->pv = pv;
+                      (ss+1)->pv[0] = MOVE_NONE;
+                  }
+
+                  pos.do_move(move, st, givesCheck);
+                  value = -qsearch<PvNode ? PV : NonPV>(pos, ss+1, -beta, -alpha);
+                  pos.undo_move(move);
+
+                  if (value <= alpha)
+                      continue;
+              }
+
           }
       }
 
