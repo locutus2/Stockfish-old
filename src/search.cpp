1853c18eed85b755bedde8baa9ba8d91592e5321
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-11 23:30:48 +0200

    Allow quiet history pruning for lmr depth = 5 if move count >= 4. Bench: 5725528

--- src/search.cpp
+++ src/search.cpp
@@ -1036,7 +1036,7 @@ moves_loop: // When in check, search starts here
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 5 + (depth >= 8)
+              if (   lmrDepth < 5 + (moveCount >= 4)
                   && history < -3875 * (depth - 1))
                   continue;
 
