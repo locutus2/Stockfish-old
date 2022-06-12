4efb9e36ae41734619bef23b43c49251e692ad33
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-12 10:04:28 +0200

    Allow quiet history pruning for lmr depth = 5 if depth >= 9 and (ss+1)->cutoffCnt >= 4 and distance to own king >= 2. Bench: 6160298

--- src/search.cpp
+++ src/search.cpp
@@ -1036,7 +1036,9 @@ moves_loop: // When in check, search starts here
                             + (*contHist[3])[movedPiece][to_sq(move)];
 
               // Continuation history based pruning (~2 Elo)
-              if (   lmrDepth < 5 + 2 * (depth >= 8)
+              if (   lmrDepth < 5 + (   depth >= 9
+                                     && (ss+1)->cutoffCnt >= 4
+                                     && distance(pos.square<KING>(us),to_sq(move)) >= 2)
                   && history < -3875 * (depth - 1))
                   continue;
 
