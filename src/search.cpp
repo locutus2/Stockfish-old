3d3fb1f2b703ada0bf16da93ac49ff1f151c822d
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-16 10:11:32 +0200

    Less reduction at PV nodes if no tt move and not in check. Bench: 6193374

--- src/search.cpp
+++ src/search.cpp
@@ -1168,6 +1168,9 @@ moves_loop: // When in check, search starts here
           if (PvNode)
               r -= 1 + 15 / (3 + depth);
 
+          if (PvNode && !ttMove && !ss->inCheck)
+              r--;
+
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
