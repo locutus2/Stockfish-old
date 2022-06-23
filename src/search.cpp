03e56aac7a3058d2b7fe42a758b350e5015a2986
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-24 01:23:43 +0200

    Update continuation history for quiet best move in window (if not tt move) but use depth-1 for stat bonus. Bench: 5229608

--- src/search.cpp
+++ src/search.cpp
@@ -1286,8 +1286,8 @@ moves_loop: // When in check, search starts here
               {
                   alpha = value;
 
-                  if(!capture && move != ttMove && depth > 3)
-                      update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(depth - 3));
+                  if(!capture && move != ttMove && depth > 1)
+                      update_continuation_histories(ss, movedPiece, to_sq(move), stat_bonus(depth - 1));
 
                   // Reduce other moves if we have found at least one score improvement
                   if (   depth > 2
