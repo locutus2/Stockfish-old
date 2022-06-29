a0a31b794ac006c142874436b8a90e577513035e
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-29 20:17:01 +0200

    Randomly (p=0.5) do more reduction at root if best move found. Bench: 5420042

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (rootNode && thisThread->nodes & 1)
+          if (rootNode && bestMove && thisThread->nodes & 1)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
