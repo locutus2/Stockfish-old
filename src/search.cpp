b4afcfbeb9542084f890de8177579122f50d85ea
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-29 23:54:35 +0200

    More reduction at PV nodes if best move found and equals tt move. Bench: 5378649

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (PvNode && bestMove && bestMove == ttMove && thisThread->nodes & 1)
+          if (PvNode && bestMove && bestMove == ttMove)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
