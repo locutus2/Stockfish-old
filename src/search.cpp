2cc10a8f6964912af3c414cdd9715f5d4ffa0ed1
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-01 00:18:09 +0200

    More reduction at PV nodes if best move found for each second thread. Bench: 5332964

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (PvNode && bestMove && bestMove == ttMove && thisThread->id() % 2 == 1)
+          if (PvNode && bestMove && thisThread->id() % 2 == 1)
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
