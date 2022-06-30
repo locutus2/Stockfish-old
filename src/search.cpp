8cf26285171cf1e1b9110258818b7d87481f9b28
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-06-30 07:00:04 +0200

    Randomly (p=0.25) do more reduction at PV nodes if best move found and equals tt move. Bench: 5306804

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
           if ((ss+1)->cutoffCnt > 3 && !PvNode)
               r++;
 
-          if (PvNode && bestMove && bestMove == ttMove)
+          if (PvNode && bestMove && bestMove == ttMove && !(thisThread->nodes & 3))
               r++;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
