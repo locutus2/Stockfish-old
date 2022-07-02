59993ddaccb39a5f7e8827bfd419fdad33f3fcbf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-02 08:53:45 +0200

    Two plies more reduction at PV nodes if best move found. Bench: 4485576

--- src/search.cpp
+++ src/search.cpp
@@ -1170,7 +1170,7 @@ moves_loop: // When in check, search starts here
               r++;
 
           if (PvNode && bestMove)
-              r++;
+              r += 2;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
