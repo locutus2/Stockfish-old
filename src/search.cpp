653e8e69ec4d1b1105f1a0ac2ea983af986b5d87
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-03 16:29:45 +0200

    Test only prior capture term after 300K tuning. Bench: 5785133

--- src/search.cpp
+++ src/search.cpp
@@ -1173,6 +1173,7 @@ moves_loop: // When in check, search starts here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
+                         + 4301 * priorCapture
                          - 4334;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
