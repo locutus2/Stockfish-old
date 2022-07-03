4dcb170bc44b7bf4a6d287e4b477eca60c9e7cf4
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-04 00:19:40 +0200

    Test only excluded move term (with compensation) after 300K tuning. Bench: 5241902

--- src/search.cpp
+++ src/search.cpp
@@ -1174,7 +1174,7 @@ moves_loop: // When in check, search starts here
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
                          + 3387 * bool(excludedMove)
-                         - 5373;
+                         - 4549;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
