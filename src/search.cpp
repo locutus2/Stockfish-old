867192d0adabfb86130cc3a4d2e9f97c0505044a
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-04 16:03:30 +0200

    Test only ttPv term after 300K tuning. Bench: 6138518

--- src/search.cpp
+++ src/search.cpp
@@ -1173,8 +1173,8 @@ moves_loop: // When in check, search starts here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
-                         + 3387 * bool(excludedMove)
-                         - 4549;
+                         + 2825 * ss->ttPv
+                         - 4334;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
