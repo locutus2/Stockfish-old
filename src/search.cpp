3625ac6d4e75268af82f5d8ac33c49033acc2583
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-04 16:13:06 +0200

    Test only improving term after 300K tuning. Bench: 5462476

--- src/search.cpp
+++ src/search.cpp
@@ -1173,8 +1173,8 @@ moves_loop: // When in check, search starts here
                          + (*contHist[0])[movedPiece][to_sq(move)]
                          + (*contHist[1])[movedPiece][to_sq(move)]
                          + (*contHist[3])[movedPiece][to_sq(move)]
-                         + 2825 * ss->ttPv
-                         - 3858;
+                         - 2704 * improving
+                         - 3415;
 
           // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
           r -= ss->statScore / 15914;
