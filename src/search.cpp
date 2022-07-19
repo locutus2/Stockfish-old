21a9a1fca5f660557a866d849e8b5fe082607c8b
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-19 17:23:38 +0200

    Cutoff count reduction: 1 + depth / 12. Bench: 5556883

--- src/search.cpp
+++ src/search.cpp
@@ -1169,7 +1169,7 @@ moves_loop: // When in check, search starts here
 
           // Increase reduction if next ply has a lot of fail high else reset count to 0
           else if ((ss+1)->cutoffCnt > 3)
-              r += 1 + depth / 8;
+              r += 1 + depth / 12;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
