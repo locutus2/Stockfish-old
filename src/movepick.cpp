1a3a320b8dd7ae6057b4802614673b2f5c393b76
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-27 00:50:32 +0200

    Add second main history with doubled divisor and use in quiet move ordering (replace old history with new history / 2). Bench: 5213710

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -138,7 +138,7 @@ void MovePicker::score() {
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
-          m.value =      (*mainHistory2)[pos.side_to_move()][from_to(m)]
+          m.value =      (*mainHistory2)[pos.side_to_move()][from_to(m)] / 2
                    + 2 * (*continuationHistory[0])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[1])[pos.moved_piece(m)][to_sq(m)]
                    +     (*continuationHistory[3])[pos.moved_piece(m)][to_sq(m)]
