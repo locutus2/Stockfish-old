c847724c3417b001f723b28800bfe2eb14e78a06
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-30 00:15:58 +0200

    33% less stat bonus for slower convergence. Bench: 6074243

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((11 * d + 330) * d - 380 , 2622);
+    return std::min((6 * d + 180) * d - 207 , 1430);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
