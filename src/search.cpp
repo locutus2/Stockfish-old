e038b4ab8f87bbec7c96093ce21a06807fc0ff50
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-30 17:41:21 +0200

    Combine: 11% less stat bonus for slower convergence and Half divisor of main history (for compensation double all usages of main history). Bench: 5535437

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((9 * d + 270) * d - 311 , 2145);
+    return std::min((8 * d + 240) * d - 276 , 1907);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
