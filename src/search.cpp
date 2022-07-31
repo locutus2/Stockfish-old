cd55b20dbf55af9828c4eaaa3aae2ade9311e1f1
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-07-31 11:17:31 +0200

    No LMR if last moved piece is captured. Bench: 5596591

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,8 @@ moves_loop: // When in check, search starts here
           &&  moveCount > 1 + (PvNode && ss->ply <= 1)
           && (   !ss->ttPv
               || !capture
-              || (cutNode && (ss-1)->moveCount > 1)))
+              || (cutNode && (ss-1)->moveCount > 1))
+          && (!capture || to_sq(move) != prevSq))
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
