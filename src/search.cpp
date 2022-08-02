059a35111a24e5c64636c6fa1aa07202cc211abf
Stefan Geschwentner <stgeschwentner@gmail.com>
2022-08-02 16:33:36 +0200

    11% less stat bonus for slower convergence. But decrease threshold so depth 7 and higher are above the threshold. Bench: 5459098

--- src/search.cpp
+++ src/search.cpp
@@ -81,7 +81,7 @@ namespace {
 
   // History and stats update bonus, based on depth
   int stat_bonus(Depth d) {
-    return std::min((8 * d + 240) * d - 276 , 2689);
+    return std::min((8 * d + 240) * d - 276 , 1542);
   }
 
   // Add a small random component to draw evaluations to avoid 3-fold blindness
